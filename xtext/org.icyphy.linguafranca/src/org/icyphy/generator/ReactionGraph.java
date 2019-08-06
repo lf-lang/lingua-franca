560d085efc88e9cacccff493d2d979f96af413bd
mniknami <mniknami>
2019-08-06 08:02:38 -0700

    Don't clear out dependsOnPorts; we need it for propagating data

--- xtext/org.icyphy.linguafranca/src/org/icyphy/generator/ReactionGraph.java
+++ xtext/org.icyphy.linguafranca/src/org/icyphy/generator/ReactionGraph.java
@@ -335,7 +335,7 @@ public class ReactionGraph {
                 port.collapseDependencies(visited);
                 dependsOnReactions.addAll(port.dependsOnReactions);
             }
-            dependsOnPorts.clear();
+            if (false) { dependsOnPorts.clear(); }
         }
     }
     
@@ -390,7 +390,7 @@ public class ReactionGraph {
                 port.collapseDependencies(visited);
                 dependsOnReactions.addAll(port.dependsOnReactions);
             }
-            dependsOnPorts.clear();
+            if (false) { dependsOnPorts.clear(); }
             if (dependsOnReactions.isEmpty()) {
                 _independentReactions.add(this);
             }
