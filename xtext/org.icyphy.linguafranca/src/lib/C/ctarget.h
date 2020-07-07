2fe515eaf2fbb05bcc60d119ced6616b4af2cb74
Soroush Bateni <soroosh129@gmail.com>
2020-07-07 00:53:45 -0500

    More comments

--- xtext/org.icyphy.linguafranca/src/lib/C/ctarget.h
+++ xtext/org.icyphy.linguafranca/src/lib/C/ctarget.h
@@ -1,3 +1,42 @@
+/**
+ * @file
+ * @author Edward A. Lee (eal@berkeley.edu)
+ *
+ * @section LICENSE
+Copyright (c) 2020, The University of California at Berkeley.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ * @section DESCRIPTION
+ * Target-specific runtime functions for the C target language.
+ * This API layer can be used in conjunction with:
+ *     target C;
+ * 
+ * Note for target language developers. Every target language has a runtime translation
+ * layer that works with the common core, as well as a source generator. 
+ * This file should act as a template for future runtime developement for target languages.
+ * For source generation, see xtext/org.icyphy.linguafranca/src/org/icyphy/generator/CGenerator.xtend.
+ */
+
+
 #ifndef CTARGET_H
 #define CTARGET_H
 
