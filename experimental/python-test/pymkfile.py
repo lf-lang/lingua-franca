7115fd025d8ff7c61b9cc20b69807443c51398f2
Soroush Bateni <soroosh129@gmail.com>
2020-07-13 11:20:24 -0500

    Comments

--- experimental/python-test/pymkfile.py
+++ experimental/python-test/pymkfile.py
@@ -2,6 +2,9 @@
 # This script is taken from
 # https://www6.software.ibm.com/developerworks/education/l-pythonscript/l-pythonscript-ltr.pdf
 # with some modifications.
+# 
+# This script can be used to generate a Makefile for C/C++ code that uses the Python C APIs.
+
 
 import distutils.sysconfig
 import string, sys
@@ -19,13 +22,20 @@ all: $(PROGRAMS)
 
 """
 
+# Add the Python library
 configopts['pythonlib'] = '-L' + distutils.sysconfig.get_config_var('LIBPL') \
 + '/' + \
 distutils.sysconfig.get_config_var('LIBRARY')
+
+
 configopts['pythoninc'] = ''
 configopts['pylibs'] = ''
+
+# The required include directories
 for dir in distutils.sysconfig.get_config_var('INCLDIRSTOMAKE').split():
     configopts['pythoninc'] += '-I%s ' % (dir,)
+
+# The required library directories
 for dir in distutils.sysconfig.get_config_var('LIBDIR').split():
     configopts['pylibs'] += '-L%s ' % (dir,)
 
@@ -36,13 +46,16 @@ configopts['pylibs'] += distutils.sysconfig.get_config_var('MODLIBS') \
         distutils.sysconfig.get_config_var('SYSLIBS')
 configopts['pyopt'] = distutils.sysconfig.get_config_var('OPT')
 
+# Add the program name to targets
 targets = ''
 for arg in sys.argv[1:]:
     targets += arg + ' '
 configopts['programs'] = targets
 
+# Fill the Makefile template
 print(maketemplate % configopts)
 
+# Generate the rest of the Makefile
 for arg in sys.argv[1:]:
     print("%s: %s.o\n\tgcc %s.o -shared $(LIBS) $(PYLIB) -o %s" \
         % (arg, arg, arg, arg))
