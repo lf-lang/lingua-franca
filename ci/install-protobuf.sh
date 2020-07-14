e5f8c43e75ff5c47a06b049d53d4fe1cb9fef2ed
Marten Lohstroh <marten@berkeley.edu>
2020-07-14 14:37:37 -0700

    Another attempt at fixing the Travis config.

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -3,6 +3,6 @@ set -ex
 echo -n "Installing protobufs......"
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
 tar -xzvf protobuf-all-3.12.3.tar.gz
-pushd protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install && popd
+protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install
 
 
