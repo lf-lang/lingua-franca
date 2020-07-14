c4852e37315ab36556abcb8e89188e0a30215655
Marten Lohstroh <marten@berkeley.edu>
2020-07-14 14:42:06 -0700

    --

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -3,6 +3,6 @@ set -ex
 echo -n "Installing protobufs......"
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
 tar -xzvf protobuf-all-3.12.3.tar.gz
-protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install
+cd protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install
 
 
