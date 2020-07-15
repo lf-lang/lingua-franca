55e7e721adca933de49290822f33a3704c08295d
Marten Lohstroh <marten@berkeley.edu>
2020-07-14 22:33:40 -0700

    Attempting to use mainstream library in combination with source-built protobuf-c.

--- ci/install-proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-echo -n "Installing protobufs......"
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
-tar -xzvf protobuf-all-3.12.3.tar.gz
-pushd protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install && popd
-
-
