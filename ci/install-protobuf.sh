562b4bbed37ab83a4d253e2b26b94c37ef1ec1da
Marten Lohstroh <marten@berkeley.edu>
2020-07-14 22:52:30 -0700

    Trying to build from source again.

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ex
 echo -n "Installing protobufs......"
-#wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
-#tar -xzvf protobuf-all-3.12.3.tar.gz
-#cd protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
+tar -xzvf protobuf-all-3.12.3.tar.gz
+cd protobuf-3.12.3 && ./configure --prefix=/usr && make && sudo make install
 
 wget https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.3/protobuf-c-1.3.3.tar.gz
 tar -xzvf protobuf-c-1.3.3.tar.gz
