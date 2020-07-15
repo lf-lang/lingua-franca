71958d37be97862e9fc85747aa136705a288e312
Marten Lohstroh <marten@berkeley.edu>
2020-07-14 23:19:23 -0700

    Fixed script.

--- .ci/install-protobuf.sh
+++ .ci/install-protobuf.sh
@@ -4,12 +4,12 @@ echo -n "Installing protobufs......"
 proto_version=protobuf-all-3.12.3
 proto_c_version=protobuf-c-1.3.3
 
-if [ -d "$proto_version" ]; then
+if [ ! -d "$proto_version" ]; then
     wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-all-3.12.3.tar.gz
     tar -xzvf protobuf-all-3.12.3.tar.gz
 fi
 cd $proto_version && ./configure --prefix=/usr && make && sudo make install
-if [ -d "$proto_c_version" ]; then
+if [ ! -d "$proto_c_version" ]; then
     wget https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.3/protobuf-c-1.3.3.tar.gz
     tar -xzvf protobuf-c-1.3.3.tar.gz
 fi
