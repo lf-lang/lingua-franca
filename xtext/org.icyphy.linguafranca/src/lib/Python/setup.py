35c789a9fe7d8d2fd2db539a0022b81e6a76632a
Soroush Bateni <soroosh129@gmail.com>
2020-09-03 22:50:34 -0500

    Added LinguaFrancaBase to PyPi

--- xtext/org.icyphy.linguafranca/src/lib/Python/setup.py
+++ xtext/org.icyphy.linguafranca/src/lib/Python/setup.py
@@ -5,5 +5,8 @@ setup(
     version = '1.0',
     description = 'Useful notations and helper functions used in Lingua Franca programs.',
     author= 'Soroush Bateni',
-    packages = ['LinguaFrancaBase']
+    author_email="soroosh129@gmail.com",
+    url="https://github.com/icyphy/lingua-franca",
+    packages = ['LinguaFrancaBase'],
+    python_requires='>=3.4',
 )
