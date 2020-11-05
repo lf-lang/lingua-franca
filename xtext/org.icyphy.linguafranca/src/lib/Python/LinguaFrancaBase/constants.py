1e97a5a2c68622f781de778e8f4f8a8f09679b5c
Soroush Bateni <soroosh129@gmail.com>
2020-11-05 08:46:25 -0600

    Added instant_t to LinguaFrancaBase for consistency with the C target.

--- xtext/org.icyphy.linguafranca/src/lib/Python/LinguaFrancaBase/constants.py
+++ xtext/org.icyphy.linguafranca/src/lib/Python/LinguaFrancaBase/constants.py
@@ -4,4 +4,9 @@ NEVER   = -0xFFFFFFFFFFFFFFFF
 FOREVER =  0x7FFFFFFFFFFFFFFF
 BILLION =  1000000000
 
+# Time instant. Both physical and logical times are represented
+# using this type.
+instant_t = int
+
+# Interval of time
 interval_t = int
