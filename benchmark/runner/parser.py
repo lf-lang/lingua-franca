f0a57e0074b60e18af021e0fc5125eac14607cc7
Christian Menard <christian.menard@tu-dresden.de>
2020-12-17 14:01:32 +0100

    collect all benchmark results, not just min, max and median

--- benchmark/runner/parser.py
+++ benchmark/runner/parser.py
@@ -1,34 +1,21 @@
-import dataclasses
-import statistics
-
-
-# A record to store benchmark results
-@dataclasses.dataclass
-class Times:
-    best: float = None
-    worst: float = None
-    median: float = None
-
-
 def parse_akka_output(lines):
     times = []
     for line in lines:
-        if "Iteration-" in line:
+        line_split = line.split()
+        if "Iteration-" in line and len(line_split) == 4 and line_split[3] == "ms":
             times.append(float(line.split()[2]))
-
-    return Times(best=min(times), worst=max(times), median=statistics.median(times))
+    return times
 
 
 def parse_lfcpp_output(lines):
-    best = None
-    worst = None
-    median = None
+    times = []
     for line in lines:
-        if "Best Time:" in line:
-            best = float(line.split()[2])
-        if "Worst Time:" in line:
-            worst = float(line.split()[2])
-        if "Median:" in line:
-            median = float(line.split()[1])
-
-    return Times(best=best, worst=worst, median=median)
+        line_split = line.split()
+        if (
+            "Iteration " in line
+            and len(line_split) == 5
+            and line_split[2] == "-"
+            and line_split[4] == "ms"
+        ):
+            times.append(float(line.split()[3]))
+    return times
