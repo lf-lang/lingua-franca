5e8b9cae891c57e9049be95a64effdf9f5706652
Soroush Bateni <soroosh129@gmail.com>
2021-01-27 00:35:17 -0600

    Further cleanup of platform.h

--- xtext/org.icyphy.linguafranca/src/lib/core/platform.h
+++ xtext/org.icyphy.linguafranca/src/lib/core/platform.h
@@ -35,23 +35,9 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
    // Windows platforms
    #include "platform/lf_Windows_support.h"
-   #ifdef _WIN64
-      //define something for Windows (64-bit only)
-   #else
-      //define something for Windows (32-bit only)
-   #endif
 #elif __APPLE__
     // Apple platforms
     #include "platform/lf_macos_support.h"
-    #if TARGET_IPHONE_SIMULATOR
-         // iOS Simulator
-    #elif TARGET_OS_IPHONE
-        // iOS device
-    #elif TARGET_OS_MAC
-        // Other kinds of Mac OS
-    #else
-    #warning "Unknown Apple platform"
-    #endif
 #elif __linux__
     // Linux
     #include "platform/lf_linux_support.h"
