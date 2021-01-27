2eca0371b85b3e0fe9062badaf70ba02575d74aa
Soroush Bateni <soroosh129@gmail.com>
2021-01-27 00:35:50 -0600

    Added the new header files

---
+++ xtext/org.icyphy.linguafranca/src/lib/core/platform/lf_macos_support.h
@@ -0,0 +1,56 @@
+/* MacOS API support for the C target of Lingua Franca. */
+
+/*************
+Copyright (c) 2021, The University of California at Berkeley.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************/
+
+/** MacOS API support for the C target of Lingua Franca.
+ *  
+ *  @author{Soroush Bateni <soroush@utdallas.edu>}
+ */
+
+#ifndef LF_MACOS_SUPPORT_H
+#define LF_MACOS_SUPPORT_H
+
+#if __STDC_VERSION__ < 201112L || defined (__STDC_NO_THREADS__) // (Not C++11 or later) or no threads support
+
+typedef pthread_mutex_t __lf_mutex_t;
+typedef pthread_cond_t __lf_cond_t;
+typedef pthread_t __lf_thread_t;
+
+#define __LF_TIMEOUT ETIMEDOUT
+
+#else
+
+typedef mtx_t __lf_mutex_t;
+typedef cnd_t __lf_cond_t;
+typedef thrd_t __lf_thread_t;
+
+#define __LF_TIMEOUT thrd_timedout
+
+#endif
+
+typedef struct timespec __lf_time_spec_t;
+typedef int __lf_clock_t;
+
+#endif LF_MACOS_SUPPORT_H
