1bfb255b893f5c0b721dc41a89acc2be3e16f48e
eal <eal@eecs.berkeley.edu>
2021-02-03 16:41:00 -0800

    Retry rather than fail after socket accept() fails or times out, something that happens every two seconds in Docker.

--- xtext/org.icyphy.linguafranca/src/lib/core/rti.c
+++ xtext/org.icyphy.linguafranca/src/lib/core/rti.c
@@ -1191,7 +1191,11 @@ void connect_to_federates(int socket_descriptor) {
         uint32_t client_length = sizeof(client_fd);
         int socket_id = accept(socket_descriptor_TCP, &client_fd, &client_length);
         if (socket_id < 0) {
-            error_print_and_exit("RTI failed to accept the socket. %s.", strerror(errno));
+            // When running within Docker, at least, accept() seems to timeout every couple of seconds.
+            // We used to exit here. Now, we just issue a warning and continue waiting.
+            warning_print("RTI failed to accept the socket. %s.", strerror(errno));
+            i--;
+            continue;
         }
 
         // The first message from the federate should contain its ID and the federation ID.
