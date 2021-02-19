8ddf1869e68138ed2ecdf6a8f5b2f3b0290ecb69
Marten Lohstroh <marten@berkeley.edu>
2021-02-18 23:35:38 -0800

    Added missing class.

---
+++ xtext/org.icyphy.linguafranca.tests/src/org/icyphy/tests/runtime/ThreadedBase.java
@@ -0,0 +1,21 @@
+package org.icyphy.tests.runtime;
+
+import org.icyphy.tests.TestRegistry.TestCategory;
+import org.icyphy.tests.runtime.TestBase;
+import org.junit.jupiter.api.Test;
+
+public abstract class ThreadedBase extends TestBase {
+
+    @Test
+    public void runSingleThreadedTestsAsThreaded() {
+        printTestHeader(
+                "Description: Run non-concurrent and non-federated test (threads = 4).");
+        this.runTestsAndPrintResults(this.target,
+                it -> (it != TestCategory.CONCURRENT &&
+                        it != TestCategory.FEDERATED),
+                it -> {
+                    it.properties.setProperty("threads", "4");
+                    return true;
+                });
+    }
+}
