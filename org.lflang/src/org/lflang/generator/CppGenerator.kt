35190ca3e9deb8483da1fb750883e3e08b776383
Christian Menard <christian.menard@tu-dresden.de>
2021-05-20 15:46:34 +0200

    cpp: move control flow from string templates to code

--- org.lflang/src/org/lflang/generator/CppGenerator.kt
+++ org.lflang/src/org/lflang/generator/CppGenerator.kt
@@ -69,95 +69,103 @@ class CppGenerator : GeneratorBase() {
 
     private fun generateCmake(): String {
         val runtimeVersion =
-            if (targetConfig.runtimeVersion != null) targetConfig.runtimeVersion else "26e6e641916924eae2e83bbf40cbc9b933414310"
+                if (targetConfig.runtimeVersion != null) targetConfig.runtimeVersion else "26e6e641916924eae2e83bbf40cbc9b933414310"
 
         val S = '$' // a little trick to escape the dollar sign with $S
-        return """
-        |cmake_minimum_required(VERSION 3.5)
-        |project(${topLevelName} VERSION 1.0.0 LANGUAGES CXX)
 
-        |# require C++ 17
-        |set(CMAKE_CXX_STANDARD 17)
-        |set(CMAKE_CXX_STANDARD_REQUIRED ON)
-        |set(CMAKE_CXX_EXTENSIONS OFF)
+        val sb = StringBuilder()
+        sb.append("""
+            |cmake_minimum_required(VERSION 3.5)
+            |project(${topLevelName} VERSION 1.0.0 LANGUAGES CXX)
 
-        |include($S{CMAKE_ROOT}/Modules/ExternalProject.cmake)
-        |include(GNUInstallDirs)
+            |# require C++ 17
+            |set(CMAKE_CXX_STANDARD 17)
+            |set(CMAKE_CXX_STANDARD_REQUIRED ON)
+            |set(CMAKE_CXX_EXTENSIONS OFF)
 
-        |set(DEFAULT_BUILD_TYPE ${targetConfig.cmakeBuildType}"})
-        |if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-        |    set(CMAKE_BUILD_TYPE "$S{DEFAULT_BUILD_TYPE}" CACHE STRING "Choose the type of build." FORCE)
-        |endif()
-        ${if (targetConfig.externalRuntimePath != null)
-            "|find_package(reactor-cpp PATHS ${targetConfig.externalRuntimePath}"
-        else
-            """
-            |if(NOT REACTOR_CPP_BUILD_DIR)
-            |    set(REACTOR_CPP_BUILD_DIR "" CACHE STRING "Choose the directory to build reactor-cpp in." FORCE)
-            |endif()
+            |include($S{CMAKE_ROOT}/Modules/ExternalProject.cmake)
+            |include(GNUInstallDirs)
 
-            |ExternalProject_Add(dep-reactor-cpp
-            |   PREFIX "$S{REACTOR_CPP_BUILD_DIR}"
-            |   GIT_REPOSITORY "https://github.com/tud-ccc/reactor-cpp.git"
-            |   GIT_TAG "$runtimeVersion"
-            |   CMAKE_ARGS
-            |   -DCMAKE_BUILD_TYPE:STRING=$S{CMAKE_BUILD_TYPE}
-            |   -DCMAKE_INSTALL_PREFIX:PATH=$S{CMAKE_INSTALL_PREFIX}
-            |   -DCMAKE_INSTALL_BINDIR:PATH=$S{CMAKE_INSTALL_BINDIR}
-            |   -DCMAKE_CXX_COMPILER=$S{CMAKE_CXX_COMPILER}
-            |   -DREACTOR_CPP_VALIDATE=${if (targetConfig.noRuntimeValidation) "OFF" else "ON"}
-            |   -DREACTOR_CPP_TRACE=${if (targetConfig.tracing != null) "ON" else "OFF"} 
-            |   # TODO «IF targetConfig.logLevel !== null»-DREACTOR_CPP_LOG_LEVEL=«logLevelsToInts.get(targetConfig.logLevel)»«ELSE»«logLevelsToInts.get(LogLevel.INFO)»«ENDIF»
+            |set(DEFAULT_BUILD_TYPE ${targetConfig.cmakeBuildType}"})
+            |if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+            |set    (CMAKE_BUILD_TYPE "$S{DEFAULT_BUILD_TYPE}" CACHE STRING "Choose the type of build." FORCE)
+            |endif()""")
+        if (targetConfig.externalRuntimePath != null) {
+            sb.append("|find_package(reactor-cpp PATHS ${targetConfig.externalRuntimePath}")
+        } else {
+            sb.append("""
+                |if(NOT REACTOR_CPP_BUILD_DIR)
+                |    set(REACTOR_CPP_BUILD_DIR "" CACHE STRING "Choose the directory to build reactor-cpp in." FORCE)
+                |endif()
+
+                |ExternalProject_Add(dep-reactor-cpp
+                |   PREFIX "$S{REACTOR_CPP_BUILD_DIR}"
+                |   GIT_REPOSITORY "https://github.com/tud-ccc/reactor-cpp.git"
+                |   GIT_TAG "$runtimeVersion"
+                |   CMAKE_ARGS
+                |   -DCMAKE_BUILD_TYPE:STRING=$S{CMAKE_BUILD_TYPE}
+                |   -DCMAKE_INSTALL_PREFIX:PATH=$S{CMAKE_INSTALL_PREFIX}
+                |   -DCMAKE_INSTALL_BINDIR:PATH=$S{CMAKE_INSTALL_BINDIR}
+                |   -DCMAKE_CXX_COMPILER=$S{CMAKE_CXX_COMPILER}
+                |   -DREACTOR_CPP_VALIDATE=${if (targetConfig.noRuntimeValidation) "OFF" else "ON"}
+                |   -DREACTOR_CPP_TRACE=${if (targetConfig.tracing != null) "ON" else "OFF"} 
+                |   # TODO «IF targetConfig.logLevel !== null»-DREACTOR_CPP_LOG_LEVEL=«logLevelsToInts.get(targetConfig.logLevel)»«ELSE»«logLevelsToInts.get(LogLevel.INFO)»«ENDIF»
+                |)
+
+                |set(REACTOR_CPP_LIB_DIR "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_LIBDIR}")
+                |set(REACTOR_CPP_BIN_DIR "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_BINDIR}")
+                |set(REACTOR_CPP_LIB_NAME "$S{CMAKE_SHARED_LIBRARY_PREFIX}reactor-cpp$S{CMAKE_SHARED_LIBRARY_SUFFIX}")
+                |set(REACTOR_CPP_IMPLIB_NAME "$S{CMAKE_STATIC_LIBRARY_PREFIX}reactor-cpp$S{CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+                |add_library(reactor-cpp SHARED IMPORTED)
+                |add_dependencies(reactor-cpp dep-reactor-cpp)
+                |if(WIN32)
+                |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_IMPLIB "$S{REACTOR_CPP_LIB_DIR}/$S{REACTOR_CPP_IMPLIB_NAME}")
+                |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_LOCATION "$S{REACTOR_CPP_BIN_DIR}/$S{REACTOR_CPP_LIB_NAME}")
+                |else()
+                |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_LOCATION "$S{REACTOR_CPP_LIB_DIR}/$S{REACTOR_CPP_LIB_NAME}")
+                |endif()
+
+                |if (APPLE)
+                |   file(RELATIVE_PATH REL_LIB_PATH "$S{REACTOR_CPP_BIN_DIR}" "$S{REACTOR_CPP_LIB_DIR}")
+                |   set(CMAKE_INSTALL_RPATH "@executable_path/$S{REL_LIB_PATH}")
+                |else ()
+                |   set(CMAKE_INSTALL_RPATH "$S{REACTOR_CPP_LIB_DIR}")
+                |endif ()""")
+        }
+        sb.append("""
+            |set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
+
+            |set(LF_MAIN_TARGET ${topLevelName})
+
+            |add_executable($S{LF_MAIN_TARGET}
+            |    main.cc
+            |#TODO
+            |#«FOR r : reactors»
+            |#«IF !r.toDefinition.isGeneric»«r.toDefinition.sourceFile.toUnixString»«ENDIF»
+            |#«ENDFOR»
+            |#«FOR r : resources»
+            |#«r.preambleSourceFile.toUnixString»
+            |#«ENDFOR»
             |)
+            |target_include_directories($S{LF_MAIN_TARGET} PUBLIC
+            |    "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_INCLUDEDIR}"
+            |    "$S{PROJECT_SOURCE_DIR}"
+            |    "$S{PROJECT_SOURCE_DIR}/__include__"
+            |)
+            |target_link_libraries($S{LF_MAIN_TARGET} reactor-cpp)
 
-            |set(REACTOR_CPP_LIB_DIR "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_LIBDIR}")
-            |set(REACTOR_CPP_BIN_DIR "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_BINDIR}")
-            |set(REACTOR_CPP_LIB_NAME "$S{CMAKE_SHARED_LIBRARY_PREFIX}reactor-cpp$S{CMAKE_SHARED_LIBRARY_SUFFIX}")
-            |set(REACTOR_CPP_IMPLIB_NAME "$S{CMAKE_STATIC_LIBRARY_PREFIX}reactor-cpp$S{CMAKE_STATIC_LIBRARY_SUFFIX}")
+            |install(TARGETS $S{LF_MAIN_TARGET})""")
 
-            |add_library(reactor-cpp SHARED IMPORTED)
-            |add_dependencies(reactor-cpp dep-reactor-cpp)
-            |if(WIN32)
-            |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_IMPLIB "$S{REACTOR_CPP_LIB_DIR}/$S{REACTOR_CPP_IMPLIB_NAME}")
-            |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_LOCATION "$S{REACTOR_CPP_BIN_DIR}/$S{REACTOR_CPP_LIB_NAME}")
-            |else()
-            |   set_target_properties(reactor-cpp PROPERTIES IMPORTED_LOCATION "$S{REACTOR_CPP_LIB_DIR}/$S{REACTOR_CPP_LIB_NAME}")
-            |endif()
+        if (!targetConfig.cmakeInclude.isNullOrEmpty()) {
+            val includeFile = FileConfig.toUnixString(fileConfig.srcPath.resolve(targetConfig.cmakeInclude))
+            sb.append("""
 
-            |if (APPLE)
-            |   file(RELATIVE_PATH REL_LIB_PATH "$S{REACTOR_CPP_BIN_DIR}" "$S{REACTOR_CPP_LIB_DIR}")
-            |   set(CMAKE_INSTALL_RPATH "@executable_path/$S{REL_LIB_PATH}")
-            |else ()
-            |   set(CMAKE_INSTALL_RPATH "$S{REACTOR_CPP_LIB_DIR}")
-            |endif ()
-            """
+            |include($includeFile)
+            """)
         }
 
-        |set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
-
-        |set(LF_MAIN_TARGET ${topLevelName})
-
-        |add_executable($S{LF_MAIN_TARGET}
-        |    main.cc
-        |#TODO
-        |#«FOR r : reactors»
-        |#«IF !r.toDefinition.isGeneric»«r.toDefinition.sourceFile.toUnixString»«ENDIF»
-        |#«ENDFOR»
-        |#«FOR r : resources»
-        |#«r.preambleSourceFile.toUnixString»
-        |#«ENDFOR»
-        |)
-        |target_include_directories($S{LF_MAIN_TARGET} PUBLIC
-        |    "$S{CMAKE_INSTALL_PREFIX}/$S{CMAKE_INSTALL_INCLUDEDIR}"
-        |    "$S{PROJECT_SOURCE_DIR}"
-        |    "$S{PROJECT_SOURCE_DIR}/__include__"
-    )
-    |target_link_libraries($S{LF_MAIN_TARGET} reactor-cpp)
-
-    |install(TARGETS $S{LF_MAIN_TARGET})
-
-    |${if (targetConfig.cmakeInclude.isNullOrEmpty()) "" else FileConfig.toUnixString(fileConfig.srcPath.resolve(targetConfig.cmakeInclude))}
-    """.trimMargin()
+        return sb.toString().trimMargin()
     }
 
     override fun generateDelayBody(action: Action, port: VarRef) = TODO()
