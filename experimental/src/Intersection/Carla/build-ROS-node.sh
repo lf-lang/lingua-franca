1c2b108cd29c56541e38b68d5eaf6bdace689157
Soroush Bateni <soroosh129@gmail.com>
2021-05-21 18:09:29 -0500

    Updated script to copy generated code and removed generated files

--- experimental/src/Intersection/Carla/build-ROS-node.sh
+++ experimental/src/Intersection/Carla/build-ROS-node.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 lfc -o . -r CarlaIntersection.lf
-src-gen/Intersection/Carla/CarlaIntersection/CarlaIntersection.c lf_carla/src/CarlaIntersection.cpp
+cp -r src-gen/Intersection/Carla/CarlaIntersection/* lf_carla/src/
+mv lf_carla/src/CarlaIntersection.c lf_carla/src/CarlaIntersection.cpp
+mv lf_carla/src/core/platform/lf_linux_support.c lf_carla/src/core/platform/lf_linux_support.cpp
 pushd lf_carla
 colcon build
 popd
