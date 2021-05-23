9d81144de4b411ef933af63c771f8bd58d63ee36
Soroush Bateni <soroosh129@gmail.com>
2021-05-22 22:06:37 -0500

    Added feedback loop from the car into the vehicle controller

--- example/C/src/ROS/build-ROS-node.sh
+++ example/C/src/ROS/build-ROS-node.sh
@@ -3,6 +3,7 @@
 lfc -r $1.lf
 cp -R src-gen/$1/* $2/src/
 mv $2/src/$1.c $2/src/$1.cpp
+mv $2/src/core/platform/lf_linux_support.c $2/src/core/platform/lf_linux_support.cpp
 pushd $2
 colcon build --packages-select $2
 popd
--- experimental/src/Intersection/Carla/build-ROS-node.sh
+++ experimental/src/Intersection/Carla/build-ROS-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lfc -o . -r CarlaIntersection.lf
+lfc -o . -r CarlaIntersection.lf ;
 cp -r src-gen/Intersection/Carla/CarlaIntersection/* lf_carla/src/
 mv lf_carla/src/CarlaIntersection.c lf_carla/src/CarlaIntersection.cpp
 mv lf_carla/src/core/platform/lf_linux_support.c lf_carla/src/core/platform/lf_linux_support.cpp
