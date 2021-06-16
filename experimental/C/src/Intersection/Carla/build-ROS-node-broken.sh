b6e3ece5cddacbd8a885be04a0dd2a850d1ddfbb
Soroush Bateni <soroosh129@gmail.com>
2021-06-16 14:02:20 -0500

    Moved the ROS-based intersection demo to a separate folder

--- experimental/C/src/Intersection/Carla/build-ROS-node-broken.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-# usage: ./build-ROS-node name-of-lf-file-without-.lf name-of-ROS-package
-lfc -r $1.lf
-cp -R src-gen/$1/* $2/src/
-mv $2/src/$1.c $2/src/$1.cpp
-pushd $2
-colcon build --packages-select $2
-popd
