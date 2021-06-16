5a9fa11585aa1ec9f8ce38ae861ab1b6e0466a43
Soroush Bateni <soroosh129@gmail.com>
2021-06-16 18:50:53 -0500

    Removed the ROS-based simulation as it is unnecessarily complicated

--- experimental/C/src/Intersection/ROS/Carla/build-ROS-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-lfc -o . -r CarlaIntersection.lf ;
-cp -r src-gen/Intersection/Carla/CarlaIntersection/* lf_carla/src/
-mv lf_carla/src/CarlaIntersection.c lf_carla/src/CarlaIntersection.cpp
-mv lf_carla/src/core/platform/lf_linux_support.c lf_carla/src/core/platform/lf_linux_support.cpp
-pushd lf_carla
-colcon build
-popd
