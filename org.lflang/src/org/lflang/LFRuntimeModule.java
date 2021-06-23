c1ebd324581aae6396cc738c421e159bd387deea
Christian Menard <christian.menard@tu-dresden.de>
2021-06-23 14:35:34 +0200

    fix LFRuntimeModule and LFGenerator

--- org.lflang/src/org/lflang/LFRuntimeModule.java
+++ org.lflang/src/org/lflang/LFRuntimeModule.java
@@ -3,25 +3,32 @@
  */
 package org.lflang;
 
+import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.scoping.IGlobalScopeProvider;
+import org.eclipse.xtext.validation.INamesAreUniqueValidationHelper;
 import org.lflang.scoping.LFGlobalScopeProvider;
 import org.lflang.validation.LFNamesAreUniqueValidationHelper;
 
 /**
- * Use this class to register components to be used at runtime / without the Equinox extension registry.
+ * Use this class to register components to be used at runtime / without the
+ * Equinox extension registry.
  */
 public class LFRuntimeModule extends AbstractLFRuntimeModule {
+
+    /** Establish a binding to our custom resource description strategy. */
+    public Class<? extends IDefaultResourceDescriptionStrategy> bindIDefaultResourceDescriptionStrategy() {
+        return LFResourceDescriptionStrategy.class;
+    }
+
     /** Establish a binding to our custom global scope provider. */
     @Override
-    public Class<LFGlobalScopeProvider> bindIGlobalScopeProvider() {
+    public Class<? extends IGlobalScopeProvider> bindIGlobalScopeProvider() {
         return LFGlobalScopeProvider.class;
     }
 
     /** Establish a binding to a helper that checks that names are unique. */
-    public Class<LFNamesAreUniqueValidationHelper> bindNamesAreUniqueValidationHelper() {
+    public Class<? extends INamesAreUniqueValidationHelper> bindNamesAreUniqueValidationHelper() {
         return LFNamesAreUniqueValidationHelper.class;
     }
-    /** Establish a binding to our custom resource description strategy. */
-//    fun bindIDefaultResourceDescriptionStrategy(): Class<out IDefaultResourceDescriptionStrategy> = LFResourceDescriptionStrategy::class.java
-    
 
 }
