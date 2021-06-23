b9ccf38c25c59b97b2cc7767b3a7c617e54c166c
Christian Menard <christian.menard@tu-dresden.de>
2021-06-23 14:35:34 +0200

    fix LFRuntimeModule and LFGenerator

--- org.lflang/src/org/lflang/generator/LFGenerator.java
+++ org.lflang/src/org/lflang/generator/LFGenerator.java
@@ -1,27 +1,67 @@
 package org.lflang.generator;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.generator.AbstractGenerator;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.lflang.Target;
+import org.lflang.lf.TargetDecl;
 
 /**
  * Generates code from your model files on save.
  * 
- * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
+ * See
+ * https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
  */
 public class LFGenerator extends AbstractGenerator {
 
     // Indicator of whether generator errors occurred.
     protected boolean generatorErrorsOccurred = false;
-    
-     public void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context) {
-        generatorErrorsOccurred = LFGeneratorImpl.INSTANCE.doGenerate(resource, fsa, context);
+
+    public void doGenerate(Resource resource, IFileSystemAccess2 fsa,
+            IGeneratorContext context) {
+        // Determine which target is desired.
+        GeneratorBase generator = null;
+
+        // first find the target decleration
+        // is there a better way to do this in plain Java?
+        final Iterable<EObject> contentIterable = IteratorExtensions
+                .toIterable(resource.getAllContents());
+        final Iterable<TargetDecl> targetDeclIterable = IterableExtensions
+                .filter(contentIterable, TargetDecl.class);
+        final String targetName = IterableExtensions.head(targetDeclIterable)
+                .getName();
+
+        // Get the actual target object
+        Target target = Target.forName(targetName);
+
+        switch (target) {
+            case C: {
+                generator = new CGenerator();
+            }
+            case CCPP: {
+                generator = new CCppGenerator();
+            }
+            case CPP: {
+                generator = new CppGenerator();
+            }
+            case TS: {
+                generator = new TypeScriptGenerator();
+            }
+            case Python: {
+                generator = new PythonGenerator();
+            }
+        }
+
+        generator.doGenerate(resource, fsa, context);
+        generatorErrorsOccurred = generator.errorsOccurred();
     }
 
     /** Return true if errors occurred in the last call to doGenerate(). */
     public boolean errorsOccurred() {
         return generatorErrorsOccurred;
     }
-
 }
