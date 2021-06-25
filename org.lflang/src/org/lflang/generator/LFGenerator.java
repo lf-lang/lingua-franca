bfb004f617628a5ff70b711d6ca2b4c8dc2c1883
Christian Menard <christian.menard@tu-dresden.de>
2021-06-25 10:33:13 +0200

    generator: port changes from LFGenerator.kt back to LFGenerator.java

--- org.lflang/src/org/lflang/generator/LFGenerator.java
+++ org.lflang/src/org/lflang/generator/LFGenerator.java
@@ -1,5 +1,7 @@
 package org.lflang.generator;
 
+import java.io.IOException;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.generator.AbstractGenerator;
@@ -7,8 +9,15 @@ import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.lflang.ErrorReporter;
+import org.lflang.FileConfig;
 import org.lflang.Target;
+import org.lflang.generator.cpp.CppFileConfig;
+import org.lflang.generator.cpp.CppGenerator;
 import org.lflang.lf.TargetDecl;
+import org.lflang.scoping.LFGlobalScopeProvider;
+
+import com.google.inject.Inject;
 
 /**
  * Generates code from your model files on save.
@@ -17,49 +26,77 @@ import org.lflang.lf.TargetDecl;
  * https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
  */
 public class LFGenerator extends AbstractGenerator {
+    
+    @Inject
+    private LFGlobalScopeProvider scopeProvider;
 
     // Indicator of whether generator errors occurred.
     protected boolean generatorErrorsOccurred = false;
 
-    public void doGenerate(Resource resource, IFileSystemAccess2 fsa,
-            IGeneratorContext context) {
-        // Determine which target is desired.
-        GeneratorBase generator = null;
-
-        // first find the target decleration
-        // is there a better way to do this in plain Java?
+    private Target getTarget(Resource resource) {
+        // FIXME: is there a better way to do this in plain Java?
         final Iterable<EObject> contentIterable = IteratorExtensions
                 .toIterable(resource.getAllContents());
         final Iterable<TargetDecl> targetDeclIterable = IterableExtensions
                 .filter(contentIterable, TargetDecl.class);
         final String targetName = IterableExtensions.head(targetDeclIterable)
                 .getName();
+        return Target.forName(targetName);
+    }
 
-        // Get the actual target object
-        Target target = Target.forName(targetName);
-
+    private FileConfig createFileConfig(final Target target, Resource resource,
+            IFileSystemAccess2 fsa, IGeneratorContext context) throws IOException {
         switch (target) {
-            case C: {
-                generator = new CGenerator();
-                break;
-            }
-            case CCPP: {
-                generator = new CCppGenerator();
-                break;
-            }
             case CPP: {
-                generator = new CppGenerator();
-                break;
+                return new CppFileConfig(resource, fsa, context);
             }
             case TS: {
-                generator = new TypeScriptGenerator();
-                break;
+                return new TypeScriptFileConfig(resource, fsa, context);
             }
-            case Python: {
-                generator = new PythonGenerator();
-                break;
+            default: {
+                return new FileConfig(resource, fsa, context);
             }
         }
+    }
+    
+    private GeneratorBase createGenerator(Target target, FileConfig fileConfig, ErrorReporter errorReporter) {
+        switch (target) {
+            case C: {
+                return new CGenerator(fileConfig, errorReporter);
+            }
+            case CCPP: {
+                return new CCppGenerator(fileConfig, errorReporter);
+            }
+            case CPP: {
+                return new CppGenerator((CppFileConfig) fileConfig, errorReporter, scopeProvider);
+            }
+            case TS: {
+                return new TypeScriptGenerator((TypeScriptFileConfig) fileConfig, errorReporter);
+            }
+            case Python: {
+                return new PythonGenerator(fileConfig, errorReporter);
+            }
+            default: {
+                throw new RuntimeException("Unexpected target!");
+            }
+        }
+
+    }
+
+    @Override
+    public void doGenerate(Resource resource, IFileSystemAccess2 fsa,
+            IGeneratorContext context) {
+        // Determine which target is desired.
+        final Target target = getTarget(resource);
+        
+        FileConfig fileConfig;
+        try {
+            fileConfig = createFileConfig(target, resource, fsa, context);
+        } catch (IOException e) {
+            throw new RuntimeException("Error during FileConfig instaniation");
+        }
+        final ErrorReporter errorReporter = new EclipseErrorReporter(fileConfig);
+        final GeneratorBase generator = createGenerator(target, fileConfig, errorReporter);
 
         generator.doGenerate(resource, fsa, context);
         generatorErrorsOccurred = generator.errorsOccurred();
