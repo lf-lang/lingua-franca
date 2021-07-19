c5080831142501aaf3bbd68e6da84a68081730f7
Christian Menard <christian.menard@tu-dresden.de>
2021-07-19 12:51:06 +0200

    util: add copyright notice and methods for modifying environment variables

--- org.lflang/src/org/lflang/util/LFCommand.java
+++ org.lflang/src/org/lflang/util/LFCommand.java
@@ -1,3 +1,27 @@
+/*************
+ Copyright (c) 2021 TU Dresaden
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************/
+
 package org.lflang.util;
 
 import java.io.ByteArrayOutputStream;
@@ -8,6 +32,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An abstraction for an external command
@@ -144,6 +169,27 @@ public class LFCommand {
             e.printStackTrace();
             return -2;
         }
+    }
+
+
+    /**
+     * Add the given variables and their values to the command's environment.
+     *
+     * @param variables A map of variable names and their values
+     */
+    public void setEnvironmentVariables(Map<String, String> variables) {
+        processBuilder.environment().putAll(variables);
+    }
+
+
+    /**
+     * Add the given variable and its value tor the command's environment.
+     *
+     * @param variableName name of the variable to add
+     * @param value        the variable's value
+     */
+    public void setEnvironmentVariable(String variableName, String value) {
+        processBuilder.environment().put(variableName, value);
     }
 
 
