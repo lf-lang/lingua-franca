b9a3d66ce52858411d95a94edf08213afd51e271
Clément Fournier <clement.fournier76@gmail.com>
2021-07-30 02:55:06 +0200

    Fix removal routines

--- org.lflang/src/org/lflang/util/CollectionUtil.java
+++ org.lflang/src/org/lflang/util/CollectionUtil.java
@@ -1,11 +1,14 @@
 package org.lflang.util;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Utilities to manipulate collections.
@@ -26,11 +29,10 @@ public class CollectionUtil {
         if (set == null || set.isEmpty()) {
             return Set.of(t);
         } else if (set.size() == 1) {
-            T curElem = set.iterator().next();
-            if (curElem.equals(t)) {
+            if (set.contains(t)) {
                 return set;
             }
-            return Set.of(curElem, t);
+            return Set.of(set.iterator().next(), t);
         } else if (set.size() == 2) {
             // make mutable
             set = new LinkedHashSet<>(set);
@@ -59,7 +61,62 @@ public class CollectionUtil {
         return map;
     }
 
-    public static <K, V> Map<K, V> compute(Map<K, V> map, K k,  BiFunction<K, V, V> computation) {
+
+    /**
+     * Remove the given value from all the sets that are values
+     * in the given map. Use this if the values of the map (the
+     * sets) were build with {@link #plus(Set, Object)}.
+     *
+     * @param map           A *modifiable* map
+     * @param valueToRemove Value to remove
+     */
+    public static <K, V> void removeFromValues(Map<K, Set<V>> map, V valueToRemove) {
+        mapValuesInPlace(map, set -> minus(set, valueToRemove));
+    }
+
+
+    /**
+     * Apply a transform to the values of the map. If the mapping
+     * function returns null, the entry is removed.
+     */
+    private static <K, V> void mapValuesInPlace(Map<K, V> map, Function<V, V> mapper) {
+        Iterator<Entry<K, V>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Entry<K, V> e = iterator.next();
+            V existing = e.getValue();
+            V mapped = mapper.apply(existing);
+            if (mapped == null) {
+                iterator.remove();
+            } else if (mapped != existing) {
+                e.setValue(mapped);
+            }
+        }
+    }
+
+
+    private static <T> Set<T> minus(Set<T> set, T t) {
+        if (set == null || set.isEmpty()) {
+            return null;
+        } else if (set.size() == 1) {
+            return set.contains(t) ? null : set;
+        } else if (set.size() == 2 && set.contains(t)) {
+            Iterator<T> iter = set.iterator();
+            T fst = iter.next();
+            T snd = iter.next();
+            if (Objects.equals(fst, t)) {
+                return Set.of(snd);
+            } else if (Objects.equals(snd, t)) {
+                return Set.of(fst);
+            }
+            return set;
+        } // else it's mutable.
+
+        set.remove(t);
+        return set;
+    }
+
+
+    public static <K, V> Map<K, V> compute(Map<K, V> map, K k, BiFunction<K, V, V> computation) {
         if (map == null || map.isEmpty()) {
             return Map.of(k, computation.apply(k, null));
         } else if (map.size() == 1) {
