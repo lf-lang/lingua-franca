2d9bfd4624cbba07b06d2ecb979be978b38652b1
Soroush Bateni <soroosh129@gmail.com>
2021-08-24 20:34:20 -0500

    Added comments

--- org.lflang/src/org/lflang/federated/FedSerialization.java
+++ org.lflang/src/org/lflang/federated/FedSerialization.java
@@ -2,9 +2,23 @@ package org.lflang.federated;
 
 import org.lflang.generator.GeneratorBase;
 
+/**
+ * Interface to enable support for automatic data serialization 
+ * in target code.
+ * 
+ * @author Soroush Bateni <soroush@utdallas.edu>
+ *
+ */
 public interface FedSerialization {
     
+    /**
+     * Variable name in the target language for the serialized data.
+     */
     final static String serializedVarName = "serialized_message";
+    
+    /**
+     * Variable name in the target language for the deserialized data.
+     */
     final static String deserializedVarName = "deserialized_message";
     
     /**
@@ -16,14 +30,53 @@ public interface FedSerialization {
      */
     public boolean isCompatible(GeneratorBase generator);
 
-    public String serializedVarLength();
-    public String seializedVarBuffer();
+    /** 
+     * @return Expression in target language that corresponds to the length
+     *  of the serialized buffer.
+     */
+    public String serializedBufferLength();
     
-    public StringBuilder generateNetworkSerialzerCode(String portName, String portType);
+    /**
+     * @return Expression in target language that is the buffer variable 
+     *  itself.
+     */
+    public String seializedBufferVar();
     
-    public StringBuilder generateNetworkDeserializerCode(String portName, String portType);
+    /**
+     * Generate code in target language that serializes 'varName'. This code
+     * will convert the data in 'varName' from its 'originalType' into an 
+     * unsigned byte array. The serialized data will be put in a variable 
+     * with the name defined by @see serializedVarName.
+     * 
+     * @param varName The variable to be serialized.
+     * @param originalType The original type of the variable.
+     * @return Target code that serializes the 'varName' from 'type'
+     *  to an unsigned byte array.
+     */
+    public StringBuilder generateNetworkSerializerCode(String varName, String originalType);
     
+    /**
+     * Generate code in target language that deserializes 'varName'. This code will
+     * convert the data in 'varName' from an unsigned byte array into the 'targetType'.
+     * The deserialized data will be put in a variable with the name defined by 
+     * @see deserializedVarName.
+     *  
+     * @param varName The variable to deserialize.
+     * @param targetType The type to deserialize into.
+     * @return Target code that deserializes 'varName' from an unsigned byte array
+     *  to 'type'.
+     */
+    public StringBuilder generateNetworkDeserializerCode(String varName, String targetType);
+    
+    /**
+     * @return Code in target language that includes all the necessary preamble to enable
+     *  support for the current serializer.
+     */
     public StringBuilder generatePreambleForSupport();
     
+    /** 
+     * @return Code that should be appended to the compiler instructions (e.g., flags to gcc or
+     *  additional lines to a CMakeLists.txt) to enable support for the current serializer.
+     */
     public StringBuilder generateCompilerExtensionForSupport();
 }
