13d3065e7debb17ce0686dd034e18303624ce837
Christian Menard <christian.menard@tu-dresden.de>
2021-08-27 14:46:32 +0200

    bench, cpp: simplify support code in guides search

--- benchmark/Cpp/Savina/src/astar/GridNode.cc
+++ benchmark/Cpp/Savina/src/astar/GridNode.cc
@@ -3,39 +3,19 @@
 #include <cmath>
 #include <algorithm>
 
-
-GridNode::GridNode(int id, int i, int j, int k):
-id(id), i(i), j(j), k(k) {
-
-	distanceFromRoot = id == 0 ? 0 : -1;
+bool GridNode::setParent(GridNode* node) {
+	GridNode* tmp = nullptr;
+	return parentInPath.compare_exchange_strong(tmp, node, std::memory_order_acq_rel);
 }
 
-
-int GridNode::numNeighbors() {
-	return neighbors.size();
-}
-
-GridNode * GridNode::neighbor(const int n) {
-	return neighbors[n];
-}
-
-bool GridNode::setParent(GridNode * node) {
-	GridNode * tmp = nullptr;
-	const bool success = parentInPath.compare_exchange_strong(tmp, node);
-	if(success) {
-		distanceFromRoot = distanceFromRoot + static_cast<int>(distanceFrom(node));
-	}
-	return success;
-}
-
-double GridNode::distanceFrom(const GridNode * node) {
-	const int iDiff = i - node->i;
-	const int jDiff = j - node->j;
-	const int kDiff = k - node->k;
+double GridNode::distanceFrom(const GridNode* node) {
+	const long iDiff = i - node->i;
+	const long jDiff = j - node->j;
+	const long kDiff = k - node->k;
 	return sqrt((iDiff * iDiff) + (jDiff * jDiff) + (kDiff * kDiff));
 }
 
-bool GridNode::addNeighbor(GridNode * node) {
+bool GridNode::addNeighbor(GridNode* node) {
 	if(node == this) {
 		return false;
 	}
--- benchmark/Cpp/Savina/src/astar/GridNode.hh
+++ benchmark/Cpp/Savina/src/astar/GridNode.hh
@@ -2,28 +2,26 @@
 
 #include <vector>
 #include <atomic>
+#include <cstddef>
 
 class GridNode {
 public:
-	int id;
-	int i;
-	int j;
-	int k;
+	const size_t id;
+	const size_t i;
+	const size_t j;
+	const size_t k;
 
+private:
 	std::vector<GridNode*> neighbors;
-	std::atomic<GridNode *> parentInPath;
+	std::atomic<GridNode*> parentInPath;
 
-	//fields used in computing distance
-	int distanceFromRoot;
+public:
+	GridNode(size_t id, size_t i, size_t j, size_t k)
+	  : id(id), i(i), j(j), k(k), parentInPath{nullptr} {}
 
-	GridNode(int id, int i, int j, int k);
-
-	GridNode(GridNode& node) = default;
-	GridNode(GridNode&& node) = default;
-
-	int numNeighbors();
-
-	GridNode * neighbor(const int n);
+	size_t numNeighbors() const { return neighbors.size(); };
+	GridNode* neighbor(size_t n) const { return neighbors[n]; };
+	const GridNode* parent() const { return parentInPath.load(std::memory_order_acquire); }
 
 	bool setParent(GridNode * node);
 
