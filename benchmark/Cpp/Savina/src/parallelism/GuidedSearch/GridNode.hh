0400b06b657d2f05ce1322992a303055e9add3cb
Christian Menard <christian.menard@tu-dresden.de>
2021-09-03 11:18:21 +0200

    bench, cpp: use spaces, no tabs

--- benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GridNode.hh
+++ benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GridNode.hh
@@ -9,28 +9,28 @@
 
 class GridNode {
 public:
-	const size_t id;
-	const size_t i;
-	const size_t j;
-	const size_t k;
+    const size_t id;
+    const size_t i;
+    const size_t j;
+    const size_t k;
 
 private:
-	std::vector<GridNode*> neighbors;
-	std::atomic<GridNode*> parentInPath;
+    std::vector<GridNode*> neighbors;
+    std::atomic<GridNode*> parentInPath;
 
 public:
-	GridNode(size_t id, size_t i, size_t j, size_t k)
-	  : id(id), i(i), j(j), k(k), parentInPath{nullptr} {}
+    GridNode(size_t id, size_t i, size_t j, size_t k)
+      : id(id), i(i), j(j), k(k), parentInPath{nullptr} {}
 
-	size_t numNeighbors() const { return neighbors.size(); };
-	GridNode* neighbor(size_t n) const { return neighbors[n]; };
-	const GridNode* parent() const { return parentInPath.load(std::memory_order_acquire); }
+    size_t numNeighbors() const { return neighbors.size(); };
+    GridNode* neighbor(size_t n) const { return neighbors[n]; };
+    const GridNode* parent() const { return parentInPath.load(std::memory_order_acquire); }
 
-	bool setParent(GridNode * node);
+    bool setParent(GridNode * node);
 
-	double distanceFrom(const GridNode * node);
+    double distanceFrom(const GridNode * node);
 
-	bool addNeighbor(GridNode * node);
+    bool addNeighbor(GridNode * node);
 };
 
 std::ostream& operator<<(std::ostream& os, const GridNode& g);
--- benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GuidedSearchCommon.hh
+++ benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GuidedSearchCommon.hh
@@ -9,41 +9,41 @@
 
 class Grid {
   private:
-	const size_t gridSize;
-	const size_t priorities;
+    const size_t gridSize;
+    const size_t priorities;
 
-	std::map<size_t, GridNode> allNodes;
+    std::map<size_t, GridNode> allNodes;
   public:
-	Grid(size_t gridSize, size_t priorities)
-	  : gridSize(gridSize), priorities(priorities) {}
+    Grid(size_t gridSize, size_t priorities)
+      : gridSize(gridSize), priorities(priorities) {}
 
-	void initializeData();
-	size_t calcPriority(const GridNode* gridNode) const;
-	GridNode* originNode() { return &allNodes.at(0); }
-	const GridNode* originNode() const { return &allNodes.at(0); }
-	GridNode* targetNode();
-	const GridNode* targetNode() const;
-	bool validate() const;
+    void initializeData();
+    size_t calcPriority(const GridNode* gridNode) const;
+    GridNode* originNode() { return &allNodes.at(0); }
+    const GridNode* originNode() const { return &allNodes.at(0); }
+    GridNode* targetNode();
+    const GridNode* targetNode() const;
+    bool validate() const;
 
-	size_t calcPriority(GridNode* gridNode) const;
+    size_t calcPriority(GridNode* gridNode) const;
 };
 
 void busyWait();
 
 struct WorkMessage {
-	GridNode* node;
-	GridNode* target;
+    GridNode* node;
+    GridNode* target;
 
-	WorkMessage(GridNode* node, GridNode* target)
-	  : node{node}, target{target} {}
+    WorkMessage(GridNode* node, GridNode* target)
+      : node{node}, target{target} {}
 };
 
 enum MsgType {
-	ReceivedMsg,
-	DoneMsg,
-	StopMsg
+    ReceivedMsg,
+    DoneMsg,
+    StopMsg
 };
 
 struct Message {
-	MsgType type;
+    MsgType type;
 };
