f2725d42b7bb7deded102d1f1bc7b35ce9b68295
Christian Menard <christian.menard@tu-dresden.de>
2021-09-03 11:18:21 +0200

    bench, cpp: use spaces, no tabs

--- benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GridNode.cc
+++ benchmark/Cpp/Savina/src/parallelism/GuidedSearch/GridNode.cc
@@ -6,30 +6,30 @@
 #include <algorithm>
 
 bool GridNode::setParent(GridNode* node) {
-	GridNode* tmp = nullptr;
-	return parentInPath.compare_exchange_strong(tmp, node, std::memory_order_relaxed);
+    GridNode* tmp = nullptr;
+    return parentInPath.compare_exchange_strong(tmp, node, std::memory_order_relaxed);
 }
 
 double GridNode::distanceFrom(const GridNode* node) {
-	const long iDiff = i - node->i;
-	const long jDiff = j - node->j;
-	const long kDiff = k - node->k;
-	return sqrt((iDiff * iDiff) + (jDiff * jDiff) + (kDiff * kDiff));
+    const long iDiff = i - node->i;
+    const long jDiff = j - node->j;
+    const long kDiff = k - node->k;
+    return sqrt((iDiff * iDiff) + (jDiff * jDiff) + (kDiff * kDiff));
 }
 
 bool GridNode::addNeighbor(GridNode* node) {
-	if(node == this) {
-		return false;
-	}
-	if(std::find(std::begin(neighbors), std::end(neighbors), node) ==
-			std::end(neighbors)) {
-		neighbors.push_back(node);
-		return true;
-	}
-	return false;
+    if(node == this) {
+        return false;
+    }
+    if(std::find(std::begin(neighbors), std::end(neighbors), node) ==
+            std::end(neighbors)) {
+        neighbors.push_back(node);
+        return true;
+    }
+    return false;
 }
 
 std::ostream& operator<<(std::ostream& os, const GridNode& g) {
-	os << "[" << g.i << ", " << g.j << ", " << g.k << "]";
-	return os;
+    os << "[" << g.i << ", " << g.j << ", " << g.k << "]";
+    return os;
 }
