2505e2c2ea6e8c7c4bfc0320e059fce496247fc2
Clément Fournier <clement.fournier76@gmail.com>
2021-09-26 17:57:07 +0200

    Add tests for standalone error messages

---
+++ org.lflang.lfc/test/kotlin/org/lflang/lfc/tests/LfIssueReportingTest.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2021, TU Dresden.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.lflang.lfc.tests
+
+import junit.framework.Assert.assertEquals
+import junit.framework.AssertionFailedError
+import org.junit.Test
+import org.lflang.LFRuntimeModule
+import org.lflang.LFStandaloneSetup
+import org.lflang.lfc.*
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
+class SpyPrintStream {
+    private val baout = ByteArrayOutputStream()
+    val ps = PrintStream(baout)
+
+    override fun toString(): String = baout.toString(StandardCharsets.UTF_8)
+}
+
+
+class LfIssueReportingTest {
+
+
+    @Test
+    fun testSimpleWarning() {
+        doTest(fileBaseName = "simpleWarning")
+    }
+
+    /**
+     * Looks for a file named [fileBaseName].lf to validate,
+     * and its sibling [fileBaseName].stderr, which is the
+     * expected output to stderr. Those files are in the test/resources directory
+     */
+    private fun doTest(fileBaseName: String, loader: Class<*> = this::class.java) {
+
+
+        val stderr = SpyPrintStream()
+
+        val backend = ReportingBackend(Io(err = stderr.ps), AnsiColors(false), 2)
+        val injector = LFStandaloneSetup(LFRuntimeModule(), LFStandaloneModule(backend))
+            .createInjectorAndDoEMFRegistration()
+        val main = injector.getInstance(Main::class.java)
+
+        val packageName = loader.packageName.replace('.', '/')
+        // relative to root of gradle project
+        val lfFile = Paths.get("test/resources/$packageName/$fileBaseName.lf")
+        val expectedPath = Paths.get("test/resources/$packageName/$fileBaseName.stderr")
+
+        // this side-effects on the ReportingBackend
+        main.getValidatedResource(lfFile)
+        main.printErrorsIfAny()
+
+        val actualOutput = stderr.toString()
+
+        if (!Files.exists(expectedPath)) {
+            Files.writeString(expectedPath, actualOutput)
+            throw AssertionFailedError("Expected file $expectedPath does not exit, created it")
+        }
+        val expected = Files.readString(expectedPath)
+
+        assertEquals(expected.normalize(), actualOutput.normalize())
+    }
+
+    private fun String.normalize() =
+        // normalize newlines and remove line-trailing whitespace
+        trim().replace(Regex("\\s+\\R"), "\n")
+
+}
