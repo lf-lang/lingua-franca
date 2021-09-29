e2108711e3e744a7842e2478c73b99c3106d6f57
Soroush Bateni <soroosh129@gmail.com>
2021-09-29 16:49:12 -0500

    Moving platform.h up to see if it fixes the issue with Windows SDK 10.0.17763.0.

--- org.lflang/src/lib/core/reactor.h
+++ org.lflang/src/lib/core/reactor.h
@@ -51,6 +51,7 @@
 #ifndef REACTOR_H
 #define REACTOR_H
 
+#include "platform.h"  // Platform-specific times and APIs
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -61,7 +62,6 @@
 #include "pqueue.h"
 #include "util.h"
 #include "tag.h"       // Time-related functions.
-#include "platform.h"  // Platform-specific times and APIs
 
 // The following file is also included, but must be included
 // after its requirements are met, so the #include appears at
