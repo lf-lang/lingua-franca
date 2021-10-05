e7974e2b2daa3171210031c03e9460a2f2f80279
Soroush Bateni <soroosh129@gmail.com>
2021-10-05 15:38:26 -0500

    Updated comment only

--- bin/run-multiple-times.ps1
+++ bin/run-multiple-times.ps1
@@ -1,5 +1,5 @@
 # A handy script that will run a command multiple times
-# Usage: run-multiple-time command number_of_times
+# Usage: run-multiple-times.ps1 command number_of_times
 $executable=$args[0]
 $number_of_times=$args[1]
 for (($k = 0); $k -lt $number_of_times; $k++) {
