47ff1ede4a8118dab426eb3b319dda935f97982c
Marten Lohstroh <marten@berkeley.edu>
2021-10-16 16:45:53 -0700

    Forgot to remove CCpp tests from CTest

--- org.lflang.tests/src/org/lflang/tests/runtime/CTest.java
+++ org.lflang.tests/src/org/lflang/tests/runtime/CTest.java
@@ -139,29 +139,5 @@ public class CTest extends AbstractTest {
         }
         super.runFederatedTests();
     }
-    
-    /**
-     * Run C tests with the target CCpp.
-     */
-    @Test
-    public void runAsCCpp() {
-        if(isWindows()) {
-            printSkipMessage(Message.DESC_AS_CCPP,
-                    Message.NO_WINDOWS_SUPPORT);
-            return; 
-        }
 
-        EnumSet<TestCategory> categories = EnumSet.allOf(TestCategory.class);
-        categories.removeAll(EnumSet.of(
-                // Don't need to test examples.
-                // If any of them uses CCpp, it will
-                // be tested when compileExamples is
-                // run.
-                TestCategory.EXAMPLE));
-
-        runTestsForTargets(Message.DESC_AS_CCPP, categories::contains,
-                it -> ASTUtils.changeTargetName(it.fileConfig.resource,
-                        Target.CCPP.getDisplayName()),
-                TestLevel.EXECUTION, true);
-    }
 }
