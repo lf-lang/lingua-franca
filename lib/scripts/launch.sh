dacc4d3aea37687f551c10e15f243675156d3e4b
Marten Lohstroh <marten@berkeley.edu>
2021-10-18 23:25:30 -0700

    More cleanup

--- lib/scripts/launch.sh
+++ lib/scripts/launch.sh
@@ -12,11 +12,11 @@ source "$(dirname "$(readlink -f "$0")")/init.sh"
 
 # Find the jar; report error if it was not found.
 if ! find_jar_path; then
-    fatal_error "Cannot find lfc jar"
+    fatal_error "Cannot find lfc jar."
 fi
 
 # Make sure the correct JRE is available.
 check_jre_version
 
-# run the command
+# Lunch the compiler.
 run_jar_with_args "$@"
