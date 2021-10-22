98b8eb7505eadf408ecd2da13afba73f41343923
Marten Lohstroh <marten@berkeley.edu>
2021-10-21 18:36:02 -0700

    Take same approach with build script.

--- lib/scripts/build.sh
+++ lib/scripts/build.sh
@@ -6,17 +6,61 @@
 # Usage:		build-lfc [options] [[-r | --run] [lfc-args]]
 #============================================================================
 
-# Initialize the environment.
-source "$(dirname "$(readlink -f "$0")")/init.sh" 
+#============================================================================
+# Preamble
+#============================================================================
+# Find the directory in which this script resides in a way that is compatible
+# with MacOS, which has a `readlink` implementation that does not support the
+# necessary `-f` flag to canonicalize by following every symlink in every 
+# component of the given name recursively.
+# This solution, adapted from an example written by Geoff Nixon, ia POSIX-
+# compliant and robust to symbolic links. If a chain of more than 1000 links
+# is encountered, we return.
+find_dir() (
+  start_dir=$PWD
+  cd "$(dirname "$1")"
+  link=$(readlink "$(basename "$1")")
+  count=0
+  while [ "${link}" ]; do
+    if [[ "${count}" -lt 1000 ]]; then
+        cd "$(dirname "${link}")"
+        link=$(readlink "$(basename "$1")")
+        ((count++))
+    else
+        return      
+    fi
+  done
+  real_path="$PWD/$(basename "$1")"
+  cd "${start_dir}"
+  echo `dirname "${real_path}"`
+)
 
-# Find the jar and check whether sources are present or not.
+# Report fatal error and exit.
+function fatal_error() {
+    1>&2 echo -e "\e[1mlfc: \e[31mfatal error: \e[0m$1"
+    exit 1
+}
+
+rel_path="/lib/scripts"
+abs_path="$(find_dir "$0")"
+
+if [[ "${abs_path}" ]]; then
+    base=`dirname $(dirname ${abs_path})`
+    # Initialize Lingua Franca shell environment.
+    source "${base}/${rel_path}/init.sh"
+else
+    fatal_error "Unable to determine absolute path to $0."
+fi
+#============================================================================
+
+# 1. Find the jar and check whether sources are present or not.
 find_jar_path
 
 if ! src_exists; then
     fatal_error "Cannot find sources."
 fi
 
-# Print message explaining the CLI args.
+# 2. Print message explaining the CLI args.
 function usage() {
     echo "Usage: build-lfc [options] [[-r | --run] [lfc-args]]"
     echo "Options:"
@@ -58,12 +102,13 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
+# 3. Clean up if requested.
 if [ $clean -eq 1 ]; then
     echo "Performing cleanup..."
     "${base}/gradlew" -p "${base}" clean;
 fi
 
-# Check if jar is missing or out-of-date compared to the sources
+# 4. Check if jar is missing or out-of-date compared to the sources
 if [ ! -f "${lfc_jar_build_path}" ] || ! "${FIND}" "${lfbase}/src" -path "${lfbase}/test" -prune -o -type f -newer "${lfc_jar_build_path}" -exec false {} +; then
 	1>&2 echo "Jar file is missing or out-of-date; running Gradle..."
 	"${base}/gradlew" ${flags} -p "${base}" buildLfc
@@ -72,7 +117,7 @@ else
     echo "Already up-to-date."
 fi
 
-# Run lfc with the provided arguments.
+# 5. Run lfc with the provided arguments.
 if [[ "${run}" == "true" ]]; then
     check_jre_version
     echo "Running lfc..."
