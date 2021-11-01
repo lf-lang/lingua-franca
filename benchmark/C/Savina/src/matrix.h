32d34395d52a887d581d1af8cdf7ded8a1dff412
Peter Donovan <p.v.donovan4@gmail.com>
2021-11-01 15:16:03 -0700

    Apply suggestions.

---
+++ benchmark/C/Savina/src/matrix.h
@@ -0,0 +1,61 @@
+/**
+ * Implementation of the Matrix abstract data type for use in some benchmarks.
+ */
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <stddef.h>
+
+typedef struct matrix_t {
+    int size_x;
+    int size_y;
+    union {
+        double* data_d;
+        int* data_i;
+    };
+} matrix_t;
+
+/*
+ * Allocate a new matrix_t.
+ * @param size_x The number of rows in the matrix.
+ * @param size_y The number of columns in the matrix.
+ */
+matrix_t mat_new_d(size_t size_x, size_t size_y);
+
+/*
+ * Allocate a new matrix_t.
+ * @param size_x The number of rows in the matrix.
+ * @param size_y The number of columns in the matrix.
+ */
+matrix_t mat_new_i(size_t size_x, size_t size_y);
+
+/*
+ * Deallocate the given matrix_t.
+ * @param matrix The matrix to deallocate.
+ */
+void mat_destroy_d(matrix_t matrix);
+
+/*
+ * Deallocate the given matrix_t.
+ * @param matrix The matrix to deallocate.
+ */
+void mat_destroy_i(matrix_t matrix);
+
+/*
+ * Return a pointer to entry (i, j) of the given matrix_t.
+ * @param matrix The matrix to be accessed.
+ * @param i The row to be accessed.
+ * @param j The column to be accessed.
+ */
+double* mat_at_d(matrix_t matrix, size_t i, size_t j);
+
+/*
+ * Return a pointer to entry (i, j) of the given matrix_t.
+ * @param matrix The matrix to be accessed.
+ * @param i The row to be accessed.
+ * @param j The column to be accessed.
+ */
+int* mat_at_i(matrix_t matrix, size_t i, size_t j);
+
+#endif
