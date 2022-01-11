43fc7f4aad0b59cd7c12f5fe985bad92b0e0d264
Soroush Bateni <soroosh129@gmail.com>
2022-01-10 18:33:17 -0600

    Added explicit return status to Python code

--- org.lflang/src/org/lflang/generator/python/PythonMainGenerator.java
+++ org.lflang/src/org/lflang/generator/python/PythonMainGenerator.java
@@ -47,7 +47,12 @@ public final class PythonMainGenerator {
                 + "# As is customary in Python programs, the main() function\n"
                 + "# should only be executed if the main module is active.\n"
                 + "if __name__==\"__main__\":\n"
-                + "    main(sys.argv)\n"
+                + "    try:\n"
+                + "        main(sys.argv)\n"
+                + "        sys.exit(0)\n"
+                + "    except Exception as ex:\n"
+                + "        print(\"Error: \"+ex)\n"
+                + "        sys.exit(1)\n"
        );
        return code.toString();
     }
