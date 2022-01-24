94142e13efd44aef8d7bdff95e87cb2694d7616c
Soroush Bateni <soroosh129@gmail.com>
2022-01-24 14:16:10 -0600

    Skeleton of the intersection demo in ROS

---
+++ experimental/Python/src/Intersection/Carla/ROS/carla_intersection/carla_intersection/vehicle.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+
+import random
+
+from std_msgs.msg import String
+
+from carla_intersection_msgs import Request, Grant
+from geometry_msgs import Vector3
+
+class Vehicle(Node):
+    def __init__(self):
+        super().__init__(f"vehicle_{random.randint(0,1000)}")
+
+        self.declare_parameter('vehicle_id', 0)
+        
+        self.vehicle_stat_ = self.create_publisher(Grant, "vehicle_grant", 10)
+        self.request_ = self.create_subscription(Request, "rsu_request", self.request_callback, 10)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    ego_vehicle = Vehicle()
+
+    rclpy.spin(ego_vehicle)
+
+    # Destroy the node explicitly
+    ego_vehicle.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
