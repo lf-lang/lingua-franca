53ebe1c2b1777aa05f5b2a066f7fb993773c444c
Marten Lohstroh <marten@berkeley.edu>
2022-01-25 14:02:03 -0800

    Removed generation of `LFProjectTemplateProvider` from workflow. Ported remainder of file.

--- org.lflang.ui/src/org/lflang/ui/wizard/LFProjectTemplateProvider.java
+++ org.lflang.ui/src/org/lflang/ui/wizard/LFProjectTemplateProvider.java
@@ -4,31 +4,51 @@
 package org.lflang.ui.wizard;
 
 import java.io.BufferedReader;
-import java.util.List;
 import java.io.InputStreamReader;
-import java.lang.annotation.Annotation;
+import java.util.List;
 import java.util.stream.Collectors;
+
 import org.eclipse.xtext.ui.XtextProjectHelper;
 import org.eclipse.xtext.ui.util.PluginProjectFactory;
+import org.eclipse.xtext.ui.util.ProjectFactory;
+import org.eclipse.xtext.ui.wizard.template.AbstractProjectTemplate;
+import org.eclipse.xtext.ui.wizard.template.GroupTemplateVariable;
 import org.eclipse.xtext.ui.wizard.template.IProjectGenerator;
 import org.eclipse.xtext.ui.wizard.template.IProjectTemplateProvider;
 import org.eclipse.xtext.ui.wizard.template.ProjectTemplate;
-import org.eclipse.xtext.ui.wizard.template.AbstractProjectTemplate;
+import org.eclipse.xtext.ui.wizard.template.StringSelectionTemplateVariable;
 
 /**
  * Create a list with all project templates to be shown in the template new project wizard.
  * 
  * Each template is able to generate one or more projects. Each project can be configured such that any number of files are included.
  */
+@SuppressWarnings("restriction")
 class LFProjectTemplateProvider implements IProjectTemplateProvider {
 	@Override
 	   public AbstractProjectTemplate[] getProjectTemplates() {
 	    // TODO: new HelloWorldProject(), new InteractiveProject(), new WebServerProject(), new ReflexGameProject(), new ParallelProject(),
-        return new AbstractProjectTemplate[] {new FederatedProject(), new PipelineProject()};
+        return new AbstractProjectTemplate[] {new FederatedProject(), 
+                new HelloWorldProject(), new InteractiveProject(), new ParallelProject(), 
+                new PipelineProject()};
     }
-		
-	static String getTemplateFromFile(AbstractProjectTemplate tpl, String target, String fileName) {
-        var stream = tpl.getClass().getResourceAsStream("templates/" + target + "/" + fileName);
+}
+
+@SuppressWarnings("restriction")
+abstract class LFProjectTemplate extends AbstractProjectTemplate {
+    
+    ProjectFactory setup(List<String> folders) {
+        var proj = new PluginProjectFactory();
+        proj.setProjectName(this.getProjectInfo().getProjectName());
+        proj.setLocation(this.getProjectInfo().getLocationPath());
+        proj.addProjectNatures(XtextProjectHelper.NATURE_ID);
+        proj.addBuilderIds(XtextProjectHelper.BUILDER_ID);
+        proj.addFolders(folders);
+        return proj;
+    }
+    
+    String readFromFile(String target, String fileName) {
+        var stream = this.getClass().getResourceAsStream("templates/" + target + "/" + fileName);
         var str = "";
         if (stream != null) {
             str = new BufferedReader(
@@ -41,68 +61,133 @@ class LFProjectTemplateProvider implements IProjectTemplateProvider {
     }
 }
 
+@SuppressWarnings("restriction")
 @ProjectTemplate(label="Parallel", icon="project_template.png", description="<p><b>Parallel</b></p><p>A simple" + 
         " fork-join pattern that exploits parallelism.</p>")
-final class PipelineProject extends AbstractProjectTemplate {
+final class PipelineProject extends LFProjectTemplate {
 
    @Override
    public void generateProjects(IProjectGenerator generator) {
-        var proj = new PluginProjectFactory();
-        proj.setProjectName(this.getProjectInfo().getProjectName());
-        proj.setLocation(this.getProjectInfo().getLocationPath());
-        proj.addProjectNatures(XtextProjectHelper.NATURE_ID);
-        proj.addBuilderIds(XtextProjectHelper.BUILDER_ID);
-        proj.addFolders(List.of("src"));
+        var proj = setup(List.of("src"));
         var fileName = "src/Pipeline.lf";
-        this.addFile(proj, fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "c", fileName));
+        this.addFile(proj, fileName, readFromFile("c", fileName));
         generator.generate(proj);
     }
 }
 
 @ProjectTemplate(label="Federated", icon="project_template.png", description="<p><b>Federated</b></p>" +
         "<p>A federated \"Hello World\" program.</p>")
-final class FederatedProject extends AbstractProjectTemplate {
+@SuppressWarnings("restriction")
+final class FederatedProject extends LFProjectTemplate {
     
     @Override
     public void generateProjects(IProjectGenerator generator) {
-        var proj = new PluginProjectFactory();
-        proj.setProjectName(this.getProjectInfo().getProjectName());
-        proj.setLocation(this.getProjectInfo().getLocationPath());
-        proj.addProjectNatures(XtextProjectHelper.NATURE_ID);
-        proj.addBuilderIds(XtextProjectHelper.BUILDER_ID);
-        proj.addFolders(List.of("src"));
+        var proj = setup(List.of("src"));
         var fileName = "src/FederatedHelloWorld.lf";
-        this.addFile(proj, fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "c", fileName));
+        this.addFile(proj, fileName, readFromFile("c", fileName));
         generator.generate(proj);            
     }
 }
 
+@ProjectTemplate(label="Parallel", icon="project_template.png", description="<p><b>Parallel</b></p>" +
+"<p>A simple fork-join pattern that exploits parallelism.</p>")
+@SuppressWarnings("restriction")
+final class ParallelProject extends LFProjectTemplate {
+    
+    @Override
+    public void generateProjects(IProjectGenerator generator) {
+        var proj = setup(List.of("src"));
+        var fileName = "src/Parallel.lf";
+        this.addFile(proj, fileName, readFromFile("c", fileName));
+        generator.generate(proj);
+    }
+}
 
-//@ProjectTemplate(label="Parallel", icon="project_template.png", description="<p><b>Parallel</b></p>
-//    <p>A simple fork-join pattern that exploits parallelism.</p>")
-//    final class ParallelProject {
-//        // val advanced = check("Advanced:", false)
-//        //val config = group("Configuration")
-//        //val target = combo("Target:", #["C"], "The target language to compile down to", config)
-//
-//
-//        override generateProjects(IProjectGenerator generator) {
-//            generator.generate(new PluginProjectFactory => [
-//                projectName = projectInfo.projectName
-//                location = projectInfo.locationPath
-//                projectNatures += #[XtextProjectHelper.NATURE_ID]
-//                builderIds += #[XtextProjectHelper.BUILDER_ID]
-//                folders += #["src"]
-//                val fileName = "src/Parallel.lf"
-//                addFile(fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "c", fileName))
-//            ])
-//        }
-//    
-//    }
-//
-//
-//@ProjectTemplate(label="Hello World", icon="project_template.png", description="<p><b>Hello World</b></p>
-//<p>Print \"Hello world!\" in a target language of choice.</p>")
+@ProjectTemplate(label="Hello World", icon="project_template.png", description="<p><b>Hello World</b></p>" +
+"<p>Print \"Hello world!\" in a target language of choice.</p>")
+@SuppressWarnings("restriction")
+final class HelloWorldProject extends LFProjectTemplate {
+    GroupTemplateVariable config = group("Configuration");
+    // FIXME: draw from Target enum instead
+    StringSelectionTemplateVariable target = combo("Target:", new String[] {"C", "C++", "Python", "TypeScript"}, "The target language to compile down to", config);
+    
+    @Override
+    public void generateProjects(IProjectGenerator generator) {
+        var proj = setup(List.of("src"));
+        var fileName = "src/HelloWorld.lf";
+        this.addFile(proj, fileName, readFromFile("c", fileName));
+        generator.generate(proj);
+          switch (target.getValue()) {
+          case "C++":
+              addFile(proj, fileName, readFromFile("cpp", fileName));
+          case "C":
+              addFile(proj, fileName, readFromFile("c", fileName));
+          case "Python":
+              addFile(proj, fileName, readFromFile("py", fileName));
+          case "TypeScript":
+              addFile(proj, fileName, readFromFile("ts", fileName));
+      }
+    }
+}
+
+@ProjectTemplate(label="Interactive", icon="project_template.png", description="<p><b>Interactive</b></p>" +
+"<p>Simulate sensor input through key strokes.</p>")
+@SuppressWarnings("restriction")
+final class InteractiveProject extends LFProjectTemplate {
+    
+    @Override
+    public void generateProjects(IProjectGenerator generator) {
+        var proj = setup(List.of("src", "src/include"));
+        var fileName = "src/Interactive.lf";        
+        this.addFile(proj, fileName, readFromFile("c", fileName));
+        var cmakeFile = "src/include/ncurses-cmake-extension.txt";
+        this.addFile(proj, cmakeFile, readFromFile("c", cmakeFile));
+        generator.generate(proj);
+    }
+}
+
+@ProjectTemplate(label="WebServer", icon="project_template.png", description="<p><b>Web Server</b></p>" +
+"<p>A simple web server implemented using TypeScript.</p>")
+@SuppressWarnings("restriction")
+final class WebServer extends LFProjectTemplate {
+    
+    @Override
+    public void generateProjects(IProjectGenerator generator) {
+        var proj = setup(List.of("src"));
+        var fileName = "src/WebServer.lf";        
+        this.addFile(proj, fileName, readFromFile("ts", fileName));
+        generator.generate(proj);
+    }
+}
+
+
+@ProjectTemplate(label="ReflexGame", icon="project_template.png", description="<p><b>ReflexGame</b></p>" +
+"<p>A simple reflex game.</p>")
+@SuppressWarnings("restriction")
+final class ReflexGame extends LFProjectTemplate {
+    GroupTemplateVariable config = group("Configuration");
+    // FIXME: draw from Target enum instead
+    StringSelectionTemplateVariable target = combo("Target:",
+            new String[] { "C", "Python" },
+            "The target language to compile down to", config);
+
+    @Override
+    public void generateProjects(IProjectGenerator generator) {
+        var proj = setup(List.of("src"));
+        var fileName = "src/ReflexGame.lf";
+        this.addFile(proj, fileName, readFromFile("ts", fileName));
+
+        if (target.getValue().equals("C")) {
+            this.addFile(proj, fileName, readFromFile("c", fileName));
+        } else if (target.getValue().equals("Python")) {
+            this.addFile(proj, fileName, readFromFile("py", fileName));
+            this.addFile(proj, "src/gui.py", readFromFile("py", "src/gui.py"));
+        }
+        generator.generate(proj);
+    }
+}
+
+
 //final class HelloWorldProject {
 //	//val advanced = check("Advanced:", false)
 //	val config = group("Configuration")
@@ -150,73 +235,4 @@ final class FederatedProject extends AbstractProjectTemplate {
 //    }
 //}
 //
-//@ProjectTemplate(label="Interactive", icon="project_template.png", description="<p><b>Interactive</b></p>
-//<p>Simulate sensor input through key strokes.</p>")
-//final class InteractiveProject {
-//    //val advanced = check("Advanced:", false)
-//    val config = group("Configuration")
-//    val target = combo("Target:", #["C"], "The target language to compile down to", config)
-//    
-//    override generateProjects(IProjectGenerator generator) {
-//        generator.generate(new PluginProjectFactory => [
-//            projectName = projectInfo.projectName
-//            location = projectInfo.locationPath
-//            projectNatures += #[XtextProjectHelper.NATURE_ID]
-//            builderIds += #[XtextProjectHelper.BUILDER_ID] 
-//            folders += #["src", "src/include"]
-//            val fileName = "src/Interactive.lf"
-//            addFile(fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "c", fileName))
-//            val cmakeFile = "src/include/ncurses-cmake-extension.txt"
-//            addFile(cmakeFile, LFProjectTemplateProvider.getTemplateFromFile(this, "c", cmakeFile))
-//        ])
-//    }
-//}
-//
-//@ProjectTemplate(label="WebServer", icon="project_template.png", description="<p><b>Web Server</b></p>
-//<p>A simple web server implemented using TypeScript.</p>")
-//final class WebServerProject {
-//    //val advanced = check("Advanced:", false)
-//    val config = group("Configuration")
-//    val target = combo("Target:", #["TypeScript"], "The target language to compile down to", config)
-//    
-//    override generateProjects(IProjectGenerator generator) {
-//        generator.generate(new PluginProjectFactory => [
-//            projectName = projectInfo.projectName
-//            location = projectInfo.locationPath
-//            projectNatures += #[XtextProjectHelper.NATURE_ID]
-//            builderIds += #[XtextProjectHelper.BUILDER_ID] 
-//            folders += #["src"]
-//            val fileName = "src/WebServer.lf"
-//            addFile(fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "ts", fileName))
-//        ])
-//    }
-//}
-//
-//@ProjectTemplate(label="ReflexGame", icon="project_template.png", description="<p><b>ReflexGame</b></p>
-//<p>A simple reflex game.</p>")
-//final class ReflexGameProject {
-//    //val advanced = check("Advanced:", false)
-//    val config = group("Configuration")
-//    val target = combo("Target:", #["C", "Python"], "The target language to compile down to", config)
-//    
-//    override generateProjects(IProjectGenerator generator) {
-//        generator.generate(new PluginProjectFactory => [
-//            projectName = projectInfo.projectName
-//            location = projectInfo.locationPath
-//            projectNatures += #[XtextProjectHelper.NATURE_ID]
-//            builderIds += #[XtextProjectHelper.BUILDER_ID] 
-//            folders += #["src"]
-//            val fileName = "src/ReflexGame.lf"
-//            if (target.value.equals("C")) {
-//                addFile(fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "c", fileName))
-//            } else if (target.value.equals("Python")) {
-//                addFile(fileName, LFProjectTemplateProvider.getTemplateFromFile(this, "py", fileName))
-//                addFile("src/gui.py", LFProjectTemplateProvider.getTemplateFromFile(this, "py", "src/gui.py"))
-//            }            
-//        ])
-//    
-//    }
-//        
-//}
-//
-//
+
