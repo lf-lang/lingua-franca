6af62578fd77bccfd074631b02a12105db0b6862
Hou Seng Wong <stevenhouseng@gmail.com>
2022-01-24 17:10:26 -0800

    put entry_points inside setup() function

--- experimental/Python/src/Intersection/Carla/ROS/carla_intersection/setup.py
+++ experimental/Python/src/Intersection/Carla/ROS/carla_intersection/setup.py
@@ -20,12 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'rsu = carla_intersection.rsu:main'
         ],
     },
 )
-
-entry_points={
-        'console_scripts': [
-                'rsu = carla_intersection.rsu:main',
-        ],
-},
