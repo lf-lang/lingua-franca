cc4f1269879dcd6c9ba979c21430c29711a5af75
revol-xut <revol-xut@protonmail.com>
2022-01-27 16:06:34 +0100

    fixed Crow in cmake

--- example/Cpp/src/AlarmClock/AlarmClock.cmake
+++ example/Cpp/src/AlarmClock/AlarmClock.cmake
@@ -1,8 +1,8 @@
 find_package (Threads)
-find_package (CROW)
+find_package (Crow)
 
 set(CPP_SOURCES ${CMAKE_CURRENT_LIST_DIR})
 
-target_link_libraries(${LF_MAIN_TARGET} PUBLIC ${CMAKE_THREAD_LIBS_INIT} CROW::CROW)
-target_include_directories(${LF_MAIN_TARGET} PUBLIC CROW::CROW ${CPP_SOURCES})
+target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} Crow::Crow)
+target_include_directories(${LF_MAIN_TARGET} PUBLIC ${CPP_SOURCES})
 
