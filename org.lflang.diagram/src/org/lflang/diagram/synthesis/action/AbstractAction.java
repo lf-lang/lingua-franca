50b28f985160deac44ba82d033da8decf10b9e7b
Hou Seng Wong <stevenhouseng@gmail.com>
2022-01-28 17:09:31 -0800

    port AbstractAction.xtend to java

--- org.lflang.diagram/src/org/lflang/diagram/synthesis/action/AbstractAction.java
+++ org.lflang.diagram/src/org/lflang/diagram/synthesis/action/AbstractAction.java
@@ -22,33 +22,31 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************/
-package org.lflang.diagram.synthesis.action
+package org.lflang.diagram.synthesis.action;
 
-import de.cau.cs.kieler.klighd.IAction
-import de.cau.cs.kieler.klighd.internal.util.KlighdInternalProperties
-import de.cau.cs.kieler.klighd.kgraph.KGraphElement
-import de.cau.cs.kieler.klighd.kgraph.KNode
-import org.lflang.lf.Reactor
+import de.cau.cs.kieler.klighd.IAction;
+import de.cau.cs.kieler.klighd.internal.util.KlighdInternalProperties;
+import de.cau.cs.kieler.klighd.kgraph.KGraphElement;
+import de.cau.cs.kieler.klighd.kgraph.KNode;
+import org.lflang.lf.Reactor;
 
 /**
  * Abstract super class for diagram actions that provides some convince methods.
  * 
  * @author{Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>}
  */
-abstract class AbstractAction implements IAction {
-	
-	def Object sourceElement(KGraphElement elem) {
-		return elem.getProperty(KlighdInternalProperties.MODEL_ELEMEMT)
+public abstract class AbstractAction implements IAction {
+	public Object sourceElement(final KGraphElement elem) {
+		return elem.<Object>getProperty(KlighdInternalProperties.MODEL_ELEMEMT);
 	}
 	
-	def boolean sourceIsReactor(KNode node) {
-		return node.sourceElement() instanceof Reactor
+	public boolean sourceIsReactor(final KNode node) {
+		return this.sourceElement(node) instanceof Reactor;
 	}
 	
-	def Reactor sourceAsReactor(KNode node) {
-		if (node.sourceIsReactor()) {
-			return node.sourceElement() as Reactor
-		}
-		return null
+	public Reactor sourceAsReactor(final KNode node) {
+		return this.sourceIsReactor(node) ? (Reactor) this.sourceElement(node) : null;
+
 	}
 }
+  
