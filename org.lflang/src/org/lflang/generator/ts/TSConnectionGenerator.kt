f26cf452866dadc2bac43b63f231319a13fb1beb
Hokeun Kim <hokeunkim@berkeley.edu>
2022-02-05 21:46:16 -0800

    Add code generation for multiport in TypeScript.

--- org.lflang/src/org/lflang/generator/ts/TSConnectionGenerator.kt
+++ org.lflang/src/org/lflang/generator/ts/TSConnectionGenerator.kt
@@ -1,7 +1,12 @@
 package org.lflang.generator.ts
 
 import org.lflang.ErrorReporter
+import org.lflang.getWidth
+import org.lflang.hasMultipleConnections
+import org.lflang.isMultiport
 import org.lflang.lf.Connection
+import org.lflang.lf.Port
+import org.lflang.lf.VarRef
 import java.util.*
 
 /**
@@ -13,31 +18,56 @@ class TSConnectionGenerator (
     private val errorReporter: ErrorReporter
 ) {
     // There is no generateClassProperties() for connections
+    
+    private fun getPortName(port: VarRef): String {
+        var portName = ""
+        if (port.container != null) {
+            portName += port.container.name + "."
+        }
+        portName += port.variable.name
+        return portName
+    }
+
+    private fun getPortNames(ports: List<VarRef>): List<String> {
+        var portNames = LinkedList<String>()
+        for (varRef in ports) {
+            val port = varRef.variable as Port
+            if (port.isMultiport) {
+                repeat(port.widthSpec.getWidth()) { index ->
+                    portNames.add("this.${getPortName(varRef)}[$index]")
+                }
+            } else {
+                portNames.add("this.${getPortName(varRef)}")
+            }
+        }
+        return portNames
+    }
 
     fun generateInstantiations(): String {
         val connectionInstantiations = LinkedList<String>()
         for (connection in connections) {
-            var leftPortName = ""
-            // FIXME: Add support for multiports.
-            if (connection.leftPorts.size > 1) {
-                errorReporter.reportError(connection, "Multiports are not yet supported in the TypeScript target.")
+            if (connection.hasMultipleConnections) {
+                var leftPortNames = getPortNames(connection.leftPorts)
+                var rightPortNames = getPortNames(connection.rightPorts)
+                val leftPortName = leftPortNames.joinToString(prefix = "[", separator = ", ", postfix = "]")
+                val rightPortName = rightPortNames.joinToString(prefix = "[", separator = ", ", postfix = "]")
+                connectionInstantiations.add("this._connectMultiplePorts($leftPortName, $rightPortName, ${connection.isIterated});")
             } else {
+                var leftPortName = ""
+
                 if (connection.leftPorts[0].container != null) {
                     leftPortName += connection.leftPorts[0].container.name + "."
                 }
                 leftPortName += connection.leftPorts[0].variable.name
-            }
-            var rightPortName = ""
-            if (connection.rightPorts.size > 1) {
-                errorReporter.reportError(connection, "Multiports are not yet supported in the TypeScript target.")
-            } else {
+                var rightPortName = ""
+
                 if (connection.rightPorts[0].container != null) {
                     rightPortName += connection.rightPorts[0].container.name + "."
                 }
                 rightPortName += connection.rightPorts[0].variable.name
-            }
-            if (leftPortName != "" && rightPortName != "") {
-                connectionInstantiations.add("this._connect(this.$leftPortName, this.$rightPortName);")
+                if (leftPortName != "" && rightPortName != "") {
+                    connectionInstantiations.add("this._connect(this.$leftPortName, this.$rightPortName);")
+                }
             }
         }
         return connectionInstantiations.joinToString("\n")
