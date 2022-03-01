416fb12971342d950e0d371b9e027b793f87d7a7
Christian Menard <christian.menard@tu-dresden.de>
2022-03-01 09:23:53 +0100

    generate a script for executing a compiled node

--- org.lflang/src/org/lflang/generator/cpp/CppRos2Generator.kt
+++ org.lflang/src/org/lflang/generator/cpp/CppRos2Generator.kt
@@ -2,6 +2,7 @@ package org.lflang.generator.cpp
 
 import org.lflang.generator.LFGeneratorContext
 import org.lflang.util.FileUtil
+import java.nio.file.Files
 import java.nio.file.Path
 
 class CppRos2Generator(generator: CppGenerator) : CppPlatformGenerator(generator) {
@@ -14,13 +15,25 @@ class CppRos2Generator(generator: CppGenerator) : CppPlatformGenerator(generator
         val nodeGenerator = CppRos2NodeGenerator(mainReactor, targetConfig, fileConfig);
         FileUtil.writeToFile(
             nodeGenerator.generateHeader(),
-            packagePath.resolve("include").resolve("${nodeGenerator.nodeName}.hh")
+            packagePath.resolve("include").resolve("${nodeGenerator.nodeName}.hh"),
+            true
         )
-        FileUtil.writeToFile(nodeGenerator.generateSource(), packagePath.resolve("src").resolve("${nodeGenerator.nodeName}.cc"))
+        FileUtil.writeToFile(
+            nodeGenerator.generateSource(),
+            packagePath.resolve("src").resolve("${nodeGenerator.nodeName}.cc"),
+            true
+        )
 
         val packageGenerator = CppRos2PackageGenerator(generator, nodeGenerator.nodeName)
-        FileUtil.writeToFile(packageGenerator.generatePackageXml(), packagePath.resolve("package.xml"))
-        FileUtil.writeToFile(packageGenerator.generatePackageCmake(generator.cppSources), packagePath.resolve("CMakeLists.txt"))
+        FileUtil.writeToFile(packageGenerator.generatePackageXml(), packagePath.resolve("package.xml"), true)
+        FileUtil.writeToFile(
+            packageGenerator.generatePackageCmake(generator.cppSources),
+            packagePath.resolve("CMakeLists.txt"),
+            true
+        )
+        val scriptPath = fileConfig.binPath.resolve(fileConfig.name);
+        FileUtil.writeToFile(packageGenerator.generateBinScript(), scriptPath)
+        scriptPath.toFile().setExecutable(true);
     }
 
     override fun doCompile(context: LFGeneratorContext, onlyGenerateBuildFiles: Boolean): Boolean {
