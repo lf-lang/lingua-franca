f54f7be63e82bf0138d7960cee194f1c3f47f334
Christian Menard <christian.menard@tu-dresden.de>
2022-03-01 08:39:13 +0100

    properly shutdown ros2 nodes after LF execution terminates

--- org.lflang/src/org/lflang/generator/cpp/CppRos2NodeGenerator.kt
+++ org.lflang/src/org/lflang/generator/cpp/CppRos2NodeGenerator.kt
@@ -23,9 +23,15 @@ class CppRos2NodeGenerator(
             |
             |class $nodeName : public rclcpp::Node {
             |private:
-            |  std::unique_ptr<reactor::Environment> __lf_env;
-            |  std::unique_ptr<${main.name}> __lf_main_reactor;
-            |  std::thread __lf_thread;
+            |  std::unique_ptr<reactor::Environment> lf_env;
+            |  std::unique_ptr<${main.name}> lf_main_reactor;
+            |  // main thread of the LF execution
+            |  std::thread lf_main_thread;
+            |  // an additional thread that we use for waiting for LF termination
+            |  // and then shutting down the LF node
+            |  std::thread lf_shutdown_thread;
+            |  
+            |  void wait_for_lf_shutdown();
             |public:
             |  $nodeName(const rclcpp::NodeOptions& node_options);
             |  ~$nodeName();
@@ -40,27 +46,35 @@ class CppRos2NodeGenerator(
             |
             |#include <thread>
             |
+            |void $nodeName::wait_for_lf_shutdown() {
+            |  lf_main_thread.join();
+            |  this->get_node_options().context()->shutdown("LF execution terminated");
+            |}
+            |
             |$nodeName::$nodeName(const rclcpp::NodeOptions& node_options)
             |  : Node("$nodeName", node_options) {
             |  unsigned threads = ${if (targetConfig.threads != 0) targetConfig.threads else "std::thread::hardware_concurrency()"};
             |  bool fast{${targetConfig.fastMode}};
             |  bool keepalive{${targetConfig.keepalive}};
             | 
-            |  __lf_env = std::make_unique<reactor::Environment>(threads, keepalive, fast);
+            |  lf_env = std::make_unique<reactor::Environment>(threads, keepalive, fast);
             |
             |  // instantiate the main reactor
-            |  __lf_main_reactor = std::make_unique<${main.name}> ("${main.name}", __lf_env.get());
+            |  lf_main_reactor = std::make_unique<${main.name}> ("${main.name}", lf_env.get());
             |
             |  // assemble reactor program
-            |  __lf_env->assemble();
+            |  lf_env->assemble();
             |
             |  // start execution
-            |  __lf_thread = __lf_env->startup();
+            |  lf_main_thread = lf_env->startup();
+            |  lf_shutdown_thread = std::thread([=] { wait_for_lf_shutdown(); });
             |}
             |
             |$nodeName::~$nodeName() {
-            |  __lf_env->async_shutdown();
-            |  __lf_thread.join();
+            |  if (lf_env->phase() == reactor::Environment::Phase::Execution) { 
+            |    lf_env->async_shutdown();
+            |  }
+            |  lf_shutdown_thread.join();
             |}
             |
             |RCLCPP_COMPONENTS_REGISTER_NODE($nodeName)
