bc4c6b0146c9d4872ee584724a437f237184b26b
Hokeun Kim <hokeunkim@berkeley.edu>
2022-03-09 11:05:56 +0900

    Simplify multi connection generation using .port() API.

--- org.lflang/src/org/lflang/generator/ts/TSConnectionGenerator.kt
+++ org.lflang/src/org/lflang/generator/ts/TSConnectionGenerator.kt
@@ -22,20 +22,11 @@ class TSConnectionGenerator (
 ) {
     // There is no generateClassProperties() for connections
 
-    private fun getPortTypeName(port: Port, genericTypeMap: HashMap<String, String>): String {
-        var portType = getPortType(port)
-        portType = genericTypeMap[portType]?: portType
-        return (if(port.isInput) "__In" else "__Out") +
-                "${if(port.isMultiport) "Multi" else ""}Port<$portType>"
-    }
-
-    private fun getPortName(port: VarRef, genericTypeMap: HashMap<String, String>): String {
+    private fun getPortName(port: VarRef): String {
         var portName = ""
         if (port.container != null) {
             if (port.container.isBank) {
-                portName += "...this.${port.container.name}.all().reduce(" +
-                        "(__acc, __val) => __acc.concat(__val.${port.variable.name}), " +
-                        "new Array<${getPortTypeName(port.variable as Port, genericTypeMap)}>(0))"
+                portName += "...this.${port.container.name}.port(it => it.${port.variable.name})"
                 return portName
             }
             portName += port.container.name + "."
@@ -44,19 +35,13 @@ class TSConnectionGenerator (
         return "this.$portName"
     }
 
+    /**
+     * Return names of the ports on multiple connections.
+     */
     private fun getPortNames(ports: List<VarRef>): List<String> {
         var portNames = LinkedList<String>()
-        var genericTypeMap: HashMap<String, String>? = null
         for (varRef in ports) {
-            if (genericTypeMap == null) {
-                genericTypeMap = HashMap<String, String>()
-            }
-            if (varRef.container != null) {
-                for ((index, typeParam) in (varRef.container.reactorClass as Reactor).typeParms.withIndex()) {
-                    genericTypeMap[typeParam.toText().split(" ")[0]] = varRef.container.typeParms[index].toText()
-                }
-            }
-            portNames.add(getPortName(varRef, genericTypeMap))
+            portNames.add(getPortName(varRef))
         }
         return portNames
     }
