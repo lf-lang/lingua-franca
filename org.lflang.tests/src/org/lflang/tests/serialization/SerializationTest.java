69cce38893250ef89d3753eadf83a7880d86f388
Soroush Bateni <soroosh129@gmail.com>
2022-03-11 01:14:28 -0600

    Cleaned up imports

--- org.lflang.tests/src/org/lflang/tests/serialization/SerializationTest.java
+++ org.lflang.tests/src/org/lflang/tests/serialization/SerializationTest.java
@@ -1,15 +1,10 @@
 package org.lflang.tests.serialization;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.lflang.Target;
-import org.lflang.tests.AbstractTest;
 import org.lflang.tests.Configurators;
 import org.lflang.tests.TestBase;
-import org.lflang.tests.TestBase.Message;
-import org.lflang.tests.TestBase.TestLevel;
 import org.lflang.tests.TestRegistry.TestCategory;
 
 public class SerializationTest extends TestBase {
