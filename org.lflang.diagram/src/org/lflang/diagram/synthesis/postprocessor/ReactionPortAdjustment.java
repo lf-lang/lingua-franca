924e85fe77c11c3a0eac3dc43ea4c361d9581466
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2022-03-16 15:22:27 +0100

    diagrams: Replaced all tabs with spaces in the entire diagrams plugin

--- org.lflang.diagram/src/org/lflang/diagram/synthesis/postprocessor/ReactionPortAdjustment.java
+++ org.lflang.diagram/src/org/lflang/diagram/synthesis/postprocessor/ReactionPortAdjustment.java
@@ -75,109 +75,109 @@ public class ReactionPortAdjustment implements IStyleModifier {
 
     @Override
     public boolean modify(IStyleModifier.StyleModificationContext context) {
-		try {
-		    KGraphElement node = context.getGraphElement();
-			if (node instanceof KNode) {
-			    KNode knode = (KNode) node;
-			    
-				// Find root node
-			    KNode parent = knode;
-				while (parent.eContainer() != null) {
-					parent = (KNode) parent.eContainer();
-				}
-				
-				// Get viewer (this is a bit brittle because it fetches the viewer from some internal property)
-				Object viewer = 
+        try {
+            KGraphElement node = context.getGraphElement();
+            if (node instanceof KNode) {
+                KNode knode = (KNode) node;
+                
+                // Find root node
+                KNode parent = knode;
+                while (parent.eContainer() != null) {
+                    parent = (KNode) parent.eContainer();
+                }
+                
+                // Get viewer (this is a bit brittle because it fetches the viewer from some internal property)
+                Object viewer = 
                         parent.getAllProperties().entrySet().stream().filter(entry -> 
                                     entry.getKey().getId().equals("de.cau.cs.kieler.klighd.viewer")
                                     || entry.getKey().getId().equals("klighd.layout.viewer"))
                                 .findAny().map(entry -> entry.getValue()).orElse(null);
-								
-				ILayoutRecorder recorder = null;
-				if (viewer instanceof IViewer) {
-				    recorder = ((IViewer) viewer).getViewContext().getLayoutRecorder();
-				}
-				
-				if (!knode.getPorts().isEmpty()) {
-					if (IterableExtensions.head(knode.getPorts()).getYpos() != 0 && 
-					         // Only adjust if layout is already applied important for incremental update animation
-					        !knode.getProperty(ReactionPortAdjustment.PROCESSED)) {
-					    if (recorder != null) {
-					        recorder.startRecording();
-					    }
-					    
-					    var in = knode.getPorts().stream().filter(p -> 
-					        p.getProperty(CoreOptions.PORT_SIDE) == PortSide.WEST).sorted((p1, p2) ->  
+                                
+                ILayoutRecorder recorder = null;
+                if (viewer instanceof IViewer) {
+                    recorder = ((IViewer) viewer).getViewContext().getLayoutRecorder();
+                }
+                
+                if (!knode.getPorts().isEmpty()) {
+                    if (IterableExtensions.head(knode.getPorts()).getYpos() != 0 && 
+                             // Only adjust if layout is already applied important for incremental update animation
+                            !knode.getProperty(ReactionPortAdjustment.PROCESSED)) {
+                        if (recorder != null) {
+                            recorder.startRecording();
+                        }
+                        
+                        var in = knode.getPorts().stream().filter(p -> 
+                            p.getProperty(CoreOptions.PORT_SIDE) == PortSide.WEST).sorted((p1, p2) ->  
                                 Float.compare(p1.getYpos(), p2.getYpos())).collect(Collectors.toList());
-					    
+                        
                         var out = knode.getPorts().stream().filter(p -> 
                             p.getProperty(CoreOptions.PORT_SIDE) == PortSide.EAST).sorted((p1, p2) ->  
                                 Float.compare(p1.getYpos(), p2.getYpos())).collect(Collectors.toList());
 
-						// Adjust
+                        // Adjust
                         if (in.stream().anyMatch(p -> !p.hasProperty(CoreOptions.PORT_BORDER_OFFSET))) {
                             adjustPositions(IterableExtensions.indexed(in), in.size(), true);
                         }
                         if (out.stream().anyMatch(p -> !p.hasProperty(CoreOptions.PORT_BORDER_OFFSET))) {
                             adjustPositions(IterableExtensions.indexed(out), out.size(), false);
                         }
-						knode.setProperty(ReactionPortAdjustment.PROCESSED, true);
-						
-						if (recorder!=null) {
-			              recorder.stopRecording(0);
-			            }
-						
-					} else if (IterableExtensions.head(knode.getPorts()).getYpos() == 0) {
-						knode.setProperty(PROCESSED, false);
-					}
-				}
-			}
-		} catch (Exception e) {
-		    e.printStackTrace();
-			// do not disturb rendering process
-		}
-		return false;
-	}
+                        knode.setProperty(ReactionPortAdjustment.PROCESSED, true);
+                        
+                        if (recorder!=null) {
+                          recorder.stopRecording(0);
+                        }
+                        
+                    } else if (IterableExtensions.head(knode.getPorts()).getYpos() == 0) {
+                        knode.setProperty(PROCESSED, false);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // do not disturb rendering process
+        }
+        return false;
+    }
 
     public void adjustPositions(Iterable<Pair<Integer, KPort>> indexedPorts, int count, boolean input) {
         float segments = LinguaFrancaShapeExtensions.REACTION_POINTINESS * 2 / (count + 1);
-		for (Pair<Integer, KPort> indexedPort : indexedPorts) {
-		    KPort port = indexedPort.getValue();
-	        int idx = indexedPort.getKey();
-	        float offset = 0;
-	        
-	        if (count % 2 != 0 && idx == count / 2) {
-	            offset += LinguaFrancaShapeExtensions.REACTION_POINTINESS;
-	        } else if (idx < count / 2) {
-	            offset += segments * (idx + 1);
-	        } else {
-	            offset += segments * (count - idx);
-	        }
-			
-			if (!input) { // reverse
-				offset -= LinguaFrancaShapeExtensions.REACTION_POINTINESS;
-			}
-			
-			// apply
-			port.setPos(port.getXpos() + offset, port.getYpos());
-			for (KEdge edge : port.getEdges()) {
-				if (input) {
-				    edge.setTargetPoint(adjustedKPoint(edge.getTargetPoint(), offset));
-				} else {
-				    edge.setSourcePoint(adjustedKPoint(edge.getSourcePoint(), offset));
-				}
-			}
-			
-			// Save for future layout
-			port.setProperty(CoreOptions.PORT_BORDER_OFFSET, (double) (input ? -offset : offset));
-		}
-	}
+        for (Pair<Integer, KPort> indexedPort : indexedPorts) {
+            KPort port = indexedPort.getValue();
+            int idx = indexedPort.getKey();
+            float offset = 0;
+            
+            if (count % 2 != 0 && idx == count / 2) {
+                offset += LinguaFrancaShapeExtensions.REACTION_POINTINESS;
+            } else if (idx < count / 2) {
+                offset += segments * (idx + 1);
+            } else {
+                offset += segments * (count - idx);
+            }
+            
+            if (!input) { // reverse
+                offset -= LinguaFrancaShapeExtensions.REACTION_POINTINESS;
+            }
+            
+            // apply
+            port.setPos(port.getXpos() + offset, port.getYpos());
+            for (KEdge edge : port.getEdges()) {
+                if (input) {
+                    edge.setTargetPoint(adjustedKPoint(edge.getTargetPoint(), offset));
+                } else {
+                    edge.setSourcePoint(adjustedKPoint(edge.getSourcePoint(), offset));
+                }
+            }
+            
+            // Save for future layout
+            port.setProperty(CoreOptions.PORT_BORDER_OFFSET, (double) (input ? -offset : offset));
+        }
+    }
 
     public KPoint adjustedKPoint(KPoint point, float xOffset) {
         KPoint kPoint = _kGraphFactory.createKPoint();
         kPoint.setX(point.getX() + xOffset);
         kPoint.setY(point.getY());
         return kPoint;
-	}
+    }
 
 }
