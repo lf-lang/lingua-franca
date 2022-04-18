012a669a9fe56acb96a0af10fc1ecd29f4fcc153
Steven <stevenhouseng@gmail.com>
2022-04-18 13:29:42 -0700

    add test for SET_COPY_CONSTRUCTOR

--- test/C/src/include/array.h
+++ test/C/src/include/array.h
@@ -15,6 +15,15 @@ int_array_t* int_array_constructor(size_t length) {
     return val;
 }
 
+void* int_array_copy_constructor(void* arr) {
+    int_array_t* other_arr = (int_array_t*) arr;
+    int_array_t* val = (int_array_t*) malloc(sizeof(int_array_t));
+    val->data = (int*) calloc(other_arr->length, sizeof(int));
+    val->length = other_arr->length;
+    memcpy(val->data, other_arr->data, other_arr->length * sizeof(int));
+    return (void*) val;
+}
+
 void int_array_destructor(void* arr) {
     free(((int_array_t*) arr)->data);
     free(arr);
