4c4d3009259da6c510a5ad265b92af0af61800bd
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2022-05-31 13:28:05 +0200

    diagrams: Activated new options for port label placement

--- org.lflang.diagram/src/org/lflang/diagram/synthesis/LinguaFrancaSynthesis.java
+++ org.lflang.diagram/src/org/lflang/diagram/synthesis/LinguaFrancaSynthesis.java
@@ -51,6 +51,7 @@ import org.eclipse.elk.core.options.BoxLayouterOptions;
 import org.eclipse.elk.core.options.CoreOptions;
 import org.eclipse.elk.core.options.Direction;
 import org.eclipse.elk.core.options.PortConstraints;
+import org.eclipse.elk.core.options.PortLabelPlacement;
 import org.eclipse.elk.core.options.PortSide;
 import org.eclipse.elk.core.options.SizeConstraint;
 import org.eclipse.elk.graph.properties.Property;
@@ -585,6 +586,10 @@ public class LinguaFrancaSynthesis extends AbstractDiagramSynthesis<Model> {
         setLayoutOption(node, CoreOptions.NODE_SIZE_CONSTRAINTS, SizeConstraint.minimumSizeWithPorts());
         // Allows to freely shuffle ports on each side
         setLayoutOption(node, CoreOptions.PORT_CONSTRAINTS, PortConstraints.FIXED_SIDE);
+        // Adjust port label spacing to be closer to edge but not overlap with port figure
+        setLayoutOption(node, CoreOptions.PORT_LABELS_PLACEMENT, EnumSet.of(PortLabelPlacement.ALWAYS_OTHER_SAME_SIDE, PortLabelPlacement.OUTSIDE, PortLabelPlacement.NEXT_TO_PORT_IF_POSSIBLE));
+        setLayoutOption(node, CoreOptions.SPACING_LABEL_PORT_HORIZONTAL, 2.0);
+        setLayoutOption(node, CoreOptions.SPACING_LABEL_PORT_VERTICAL, -3.0);
         // Balanced placement with straight long edges.
         setLayoutOption(node, LayeredOptions.NODE_PLACEMENT_STRATEGY, NodePlacementStrategy.NETWORK_SIMPLEX);
         // Otherwise nodes are not sorted if they are not connected
