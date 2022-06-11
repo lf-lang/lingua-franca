b67bf88880f7e83c4d657056c9a929495a4fab09
Peter Donovan <peterdonovan@berkeley.edu>
2022-06-10 18:22:19 -0700

    [tests] Attempt linking.

--- org.lflang.tests/src/org/lflang/tests/compiler/RoundTripTests.java
+++ org.lflang.tests/src/org/lflang/tests/compiler/RoundTripTests.java
@@ -1,13 +1,21 @@
 package org.lflang.tests.compiler;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Function;
 
 import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,33 +29,56 @@ import org.lflang.tests.TestRegistry;
 import org.lflang.tests.TestRegistry.TestCategory;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 @ExtendWith(InjectionExtension.class)
 @InjectWith(LFInjectorProvider.class)
 public class RoundTripTests {
     @Inject
+    IResourceValidator validator;
+
+    @Inject
+    Provider<ResourceSet> resourceSetProvider;
+
+    @Inject
     ParseHelper<Model> parser;
 
     @Test
     public void roundTripTest() throws Exception {
+        int nonFailures = 0;
         for (Target target : Target.values()) {
             for (TestCategory category : TestCategory.values()) {
                 for (LFTest test : TestRegistry.getRegisteredTests(target, category, false)) {
-                    run(test);
+                    run(test.srcFile);
+                    System.out.printf("%s non-failures%n", ++nonFailures);
                 }
             }
         }
     }
 
-    private void run(LFTest test) throws Exception {
-        String testCase = Files.readString(test.srcFile);
-        Model originalModel = parser.parse(testCase);
+    private void run(Path file) throws Exception {
+        String testCase = Files.readString(file);
+        Resource resource = resourceSetProvider.get().getResource(
+            URI.createFileURI(file.toFile().getAbsolutePath()),
+            true
+        );
+        var issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+        Assertions.assertTrue(issues.isEmpty());
+//        EcoreUtil2.resolveAll(resource);
+        EcoreUtil2.resolveLazyCrossReferences(resource, CancelIndicator.NullImpl);
+        ResourceSet resourceSet = resource.getResourceSet();
+//        EcoreUtil2.resolveAll(resourceSet);
+        Model originalModel = parser.parse(testCase, resourceSet);
         Assertions.assertTrue(originalModel.eResource().getErrors().isEmpty());
         String reformattedTestCase = ToText.instance.doSwitch(originalModel);
-        Model resultingModel = parser.parse(reformattedTestCase);
+        System.out.printf("Reformatted test case:%n%s%n%n", reformattedTestCase);
+        Model resultingModel = parser.parse(reformattedTestCase, resourceSet);
         Assertions.assertNotNull(resultingModel);
-        if (!resultingModel.eResource().getErrors().isEmpty()) System.err.println(reformattedTestCase);
-        Assertions.assertTrue(resultingModel.eResource().getErrors().isEmpty());
+        if (!resultingModel.eResource().getErrors().isEmpty()) {
+            System.err.println(reformattedTestCase);
+            resultingModel.eResource().getErrors().forEach(System.err::println);
+            Assertions.assertTrue(resultingModel.eResource().getErrors().isEmpty());
+        }
         checkSemanticallyEquivalent(originalModel, resultingModel);
     }
 
