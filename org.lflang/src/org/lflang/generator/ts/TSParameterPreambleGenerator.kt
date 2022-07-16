7aa3654f60ea71748a3c3535d86bf6791f3c75a6
Hokeun Kim <hokeunkim@berkeley.edu>
2022-07-15 21:16:53 -0700

    Move timeInTargetLanguage to TSExtensions.

--- org.lflang/src/org/lflang/generator/ts/TSParameterPreambleGenerator.kt
+++ org.lflang/src/org/lflang/generator/ts/TSParameterPreambleGenerator.kt
@@ -31,7 +31,6 @@ import org.lflang.TimeValue
 import org.lflang.generator.PrependOperator
 import org.lflang.lf.Parameter
 import org.lflang.lf.Reactor
-import org.lflang.TimeUnit
 import java.util.StringJoiner
 
 /**
@@ -65,15 +64,6 @@ class TSParameterPreambleGenerator(
             timeInTargetLanguage(targetConfig.coordinationOptions.advance_message_interval)
         } else {
             "undefined"
-        }
-    }
-
-    private fun timeInTargetLanguage(value: TimeValue): String {
-        return if (value.unit != null) {
-            "TimeValue.${value.unit.canonicalName}(${value.time})"
-        } else {
-            // The value must be zero.
-            "TimeValue.zero()"
         }
     }
 
