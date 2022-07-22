794f4c467bd162de43b4bb0c7076e5d17956ce2b
Peter Donovan <peterdonovan@berkeley.edu>
2022-07-21 21:58:51 -0700

    Try to fix build issue.

--- org.lflang/src/org/lflang/generator/c/CGenerator.java
+++ org.lflang/src/org/lflang/generator/c/CGenerator.java
@@ -2224,7 +2224,6 @@ public class CGenerator extends GeneratorBase {
         targetConfig.compileDefinitions.put("LOG_LEVEL", targetConfig.logLevel.ordinal() + "");
         targetConfig.compileAdditionalSources.addAll(CCoreFilesUtils.getCTargetSrc());
         targetConfig.compileAdditionalSources.add("core" + File.separator + "mixed_radix.c");
-        targetConfig.compileAdditionalSources.add("core" + File.separator + "port.c");
         setCSpecificDefaults();
         // Create the main reactor instance if there is a main reactor.
         createMainReactorInstance();
