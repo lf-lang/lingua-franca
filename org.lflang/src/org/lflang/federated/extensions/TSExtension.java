99ad50f42e3bd2b125ec4503265ad29f421bbd21
Hokeun Kim <hokeunkim@berkeley.edu>
2022-07-28 16:02:04 -0700

    Generate min_output_delay preamble.

--- org.lflang/src/org/lflang/federated/extensions/TSExtension.java
+++ org.lflang/src/org/lflang/federated/extensions/TSExtension.java
@@ -1,18 +1,25 @@
 package org.lflang.federated.extensions;
 
+import static org.lflang.util.StringUtil.addDoubleQuotes;
+
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
+import org.lflang.ASTUtils;
 import org.lflang.ErrorReporter;
 import org.lflang.InferredType;
 import org.lflang.TargetProperty.CoordinationType;
 import org.lflang.TimeValue;
+import org.lflang.federated.generator.FedASTUtils;
 import org.lflang.federated.generator.FedConnectionInstance;
 import org.lflang.federated.generator.FedFileConfig;
 import org.lflang.federated.generator.FederateInstance;
+import org.lflang.generator.GeneratorBase;
 import org.lflang.generator.LFGeneratorContext;
+import org.lflang.generator.ReactorInstance;
 import org.lflang.lf.Action;
+import org.lflang.lf.Output;
 import org.lflang.lf.VarRef;
 import org.lflang.lf.Variable;
 
@@ -72,6 +79,7 @@ public class TSExtension implements FedTargetExtension {
      */
     @Override
     public String generatePreamble(FederateInstance federate, FedFileConfig fileConfig, LinkedHashMap<String, Object> federationRTIProperties, ErrorReporter errorReporter) {
+        var minOutputDelay = getMinOutputDelay(federate, fileConfig, errorReporter);
         return
         """
         preamble {=
@@ -81,7 +89,8 @@ public class TSExtension implements FedTargetExtension {
             port: %d,
             network_message_actions: [%s],
             depends_on: [%s],
-            sends_to: [%s]
+            sends_to: [%s],
+            %s
         =}""".formatted(federate.id,
                         federationRTIProperties.get("host"),
                         federationRTIProperties.get("port"),
@@ -94,7 +103,50 @@ public class TSExtension implements FedTargetExtension {
                                           .collect(Collectors.joining(";")),
                         federate.sendsTo.keySet().stream()
                                         .map(e->String.valueOf(e.id))
-                                        .collect(Collectors.joining(";"))
+                                        .collect(Collectors.joining(";")),
+                        minOutputDelay == null ? "" : "min_output_delay: %s".formatted(minOutputDelay)
         );
+    }
+
+    private TimeValue getMinOutputDelay(FederateInstance federate, FedFileConfig fileConfig, ErrorReporter errorReporter) {
+        if (federate.targetConfig.coordination.equals(CoordinationType.CENTRALIZED)) {
+            // If this program uses centralized coordination then check
+            // for outputs that depend on physical actions so that null messages can be
+            // sent to the RTI.
+            var federateClass = ASTUtils.toDefinition(federate.instantiation.getReactorClass());
+            var main = new ReactorInstance(FedASTUtils.findFederatedReactor(federate.instantiation.eResource()), errorReporter, 1);
+            var instance = new ReactorInstance(federateClass, main, errorReporter);
+            var outputDelayMap = federate
+                .findOutputsConnectedToPhysicalActions(instance);
+            var minOutputDelay = TimeValue.MAX_VALUE;
+            Output outputFound = null;
+            for (Output output : outputDelayMap.keySet()) {
+                var outputDelay = outputDelayMap.get(output);
+                if (outputDelay.isEarlierThan(minOutputDelay)) {
+                    minOutputDelay = outputDelay;
+                    outputFound = output;
+                }
+            }
+            if (minOutputDelay != TimeValue.MAX_VALUE) {
+                // Unless silenced, issue a warning.
+                if (federate.targetConfig.coordinationOptions.advance_message_interval
+                    == null) {
+                    errorReporter.reportWarning(outputFound, String.join("\n",
+                                                                         "Found a path from a physical action to output for reactor "
+                                                                             + addDoubleQuotes(instance.getName())
+                                                                             + ". ",
+                                                                         "The amount of delay is "
+                                                                             + minOutputDelay
+                                                                             + ".",
+                                                                         "With centralized coordination, this can result in a large number of messages to the RTI.",
+                                                                         "Consider refactoring the code so that the output does not depend on the physical action,",
+                                                                         "or consider using decentralized coordination. To silence this warning, set the target",
+                                                                         "parameter coordination-options with a value like {advance-message-interval: 10 msec}"
+                    ));
+                }
+                return minOutputDelay;
+            }
+        }
+        return null;
     }
 }
