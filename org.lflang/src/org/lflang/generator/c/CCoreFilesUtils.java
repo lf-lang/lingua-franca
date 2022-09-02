ab160482a972787d6637299a4598d479ec54ed75
Peter Donovan <peterdonovan@berkeley.edu>
2022-09-02 12:24:05 -0700

    Fix lock-time single-threaded build on Linux.

--- org.lflang/src/org/lflang/generator/c/CCoreFilesUtils.java
+++ org.lflang/src/org/lflang/generator/c/CCoreFilesUtils.java
@@ -93,6 +93,7 @@ public class CCoreFilesUtils {
             "platform/lf_POSIX_threads_support.c",
             "platform/lf_C11_threads_support.c",
             "platform/lf_C11_threads_support.h",
+            "platform/lf_os_single_threaded_support.c",
             "platform/lf_POSIX_threads_support.h",
             "platform/lf_POSIX_threads_support.c",
             "platform/lf_unix_clock_support.c",
