67b316a68128791ae527bfa3be31c9b17732a4e6
Peter Donovan <peterdonovan@berkeley.edu>
2022-09-23 12:51:48 -0700

    Get current lff version from StringsBundle.

--- buildSrc/src/main/java/lfformat/LfFormatStep.java
+++ buildSrc/src/main/java/lfformat/LfFormatStep.java
@@ -5,34 +5,59 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+/**
+ * {@code LfFormatStep} is used by the Spotless Gradle plugin as a custom formatting step for
+ * formatting LF code.
+ */
 public final class LfFormatStep {
     private LfFormatStep() {}
 
-    public static FormatterStep create() {
+    /** Return a {@code FormatterStep} for LF code. */
+    public static FormatterStep create(File projectRoot) {
+        Step.projectRoot = projectRoot.toPath();
         return new Step();
     }
 
-    private static final class Step implements FormatterStep {
+    /** Implement LF-specific formatting functionality. */
+    public static class Step implements FormatterStep, Serializable {
+        // The use of the static keyword here is a workaround for serialization difficulties.
+        /** The path to the lingua-franca repository. */
+        private static Path projectRoot;
+
         @Override
         public String format(@SuppressWarnings("NullableProblems") String rawUnix, File file)
                 throws IOException, InterruptedException {
-            // It looks stupid to invoke Java from Java, but it is necessary in
+            // It looks silly to invoke Java from Java, but it is necessary in
             // order to break the circularity of needing the program to be built
             // in order for it to be built.
+            final Path resourcePath =
+                    projectRoot.resolve(Path.of("org.lflang", "src", "org", "lflang"));
+            final ResourceBundle properties =
+                    ResourceBundle.getBundle(
+                            "StringsBundle",
+                            Locale.getDefault(),
+                            new URLClassLoader(new URL[] {resourcePath.toUri().toURL()}));
+            final Path lffPath =
+                    Path.of(
+                            "org.lflang.cli",
+                            "build",
+                            "libs",
+                            String.format(
+                                    "org.lflang.cli-%s-lff.jar", properties.getString("VERSION")));
             Process p =
                     new ProcessBuilder(
                                     List.of(
                                             "java",
                                             "-jar",
-                                            Path.of(
-                                                            "org.lflang.cli",
-                                                            "build",
-                                                            "libs",
-                                                            "org.lflang.cli-0.3.1-SNAPSHOT-lff.jar")
-                                                    .toString(),
+                                            lffPath.toString(),
                                             "--dry-run",
                                             file.getAbsoluteFile().toString()))
                             .start();
@@ -40,12 +65,18 @@ public final class LfFormatStep {
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line;
             while ((line = in.readLine()) != null) {
-                if (!output.isEmpty()) output.append("\n");
+                if (!output.isEmpty()) {
+                    output.append("\n");
+                }
                 // Filtering by "lff: " is yet another string-processing hack that is not airtight!
-                if (!line.startsWith("lff: ")) output.append(line);
+                if (!line.startsWith("lff: ")) {
+                    output.append(line);
+                }
             }
             int returnCode = p.waitFor();
-            if (returnCode != 0) throw new RuntimeException("Failed to reformat file.");
+            if (returnCode != 0) {
+                throw new RuntimeException("Failed to reformat file.");
+            }
             return output.toString().stripTrailing() + "\n"; // Not sure why this is necessary
         }
 
