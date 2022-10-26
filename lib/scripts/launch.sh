c036e4cc9b0140660eb5cc0f94a8c77979adb4b5
Christian Menard <christian.menard@tu-dresden.de>
2022-10-26 11:48:05 +0200

    add missing shebang

--- lib/scripts/launch.sh
+++ lib/scripts/launch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #============================================================================
 # Description:      Run the Lingua Franca compiler.
 # Authors:          Marten Lohstroh
