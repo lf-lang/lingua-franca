c8595facb2bc9a4e2009e8ed939bbc391d121538
Konpaku Youmu <axmmisaka@users.noreply.github.com>
2022-10-27 00:04:21 -0700

    Concentrated PowerShell commands
    Right now, launch.ps1 will, just like how lfc and lff (which are symbolic links) on *NIX works, get the name of the invoker and run the corresponding jar file, which is not as PowerShell-y. We can use arguments like outlined here: https://stackoverflow.com/a/43940404.
    Nevertheless, it appears it does work somehow.

--- bin/lff.ps1
+++ bin/lff.ps1
@@ -1,50 +1,8 @@
 #==========================================================
 # Description: 	    Run the lff compiler.
-# Authors:          Christian Menard, Peter Donovan, Ruomu Xu
+# Authors:          Ruomu Xu
 # Usage:            Usage: lff [options] files...
 #==========================================================
 
-$base="$PSScriptRoot\.."
-$java_home = "$Env:JAVA_HOME"
-$java_cmd = "$java_home\bin\java.exe"
-$jarpath_dev="$base\org.lflang.cli\build\libs\org.lflang.cli-*-lff.jar"
-$jarpath_release="$base\lib\jars\org.lflang.cli-*-lff.jar"
-
-function Test-Dev {
-    Test-Path "$base\org.lflang.cli" -PathType container
-}
-
-function Get-JarPath {
-    if (Test-Dev) {
-        if (Test-Path $jarpath_dev -PathType leaf) {
-            $jarpath=$(Get-ChildItem $jarpath_dev).toString()
-        } else {
-            throw "Failed to find a copy of the Lingua Franca compiler matching the pattern ""$jarpath_dev"". Did you remember to build?"
-        }
-    } else {
-        if (Test-Path $jarpath_release -PathType leaf) {
-            $jarpath=$(Get-ChildItem $jarpath_release).toString()
-        } else {
-            throw "Failed to find a copy of the Lingua Franca compiler matching the pattern ""$jarpath_release""."
-        }
-    }
-    $jarpath
-}
-
-# check if we can find java executable in $java_home
-if (-not (Test-Path $java_cmd)) {
-    # otherwise, try to run java directly
-	if (-not (Get-Command java -errorAction SilentlyContinue)) {
-	    throw "JRE not found"
-	}
-    $java_cmd = "java"
-}
-
-# check for correct java version
-$java_version = (Get-Command java | Select-Object -ExpandProperty Version).toString()
-if ([version]$java_version -lt [version]"17.0") {
-    throw "JRE $java_version found but 17.0 or greater is required."
-}
-
-# invoke lff
-& $java_cmd -jar $(Get-JarPath) $args
+$launchScript="$PSScriptRoot\..\lib\scripts\launch.ps1"
+. $launchScript $args
---
+++ lib/scripts/launch.ps1
@@ -0,0 +1,57 @@
+#==========================================================
+# Description: 	    Launch lfc or lff depending on the invoking script.
+# Authors:          Christian Menard, Peter Donovan, Ruomu Xu
+# Usage:            Usage: launch args...
+#                   with invoker with name same as the programme to be invoked.
+#==========================================================
+
+# If the invoker is Z:\nkamihara\lf\bin\lfc.ps1, $invokerName will strip out "lfc.ps1" and then get "lfc".
+# See https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.2#myinvocation
+$invokerPath = $MyInvocation.PSCommandPath
+$invokerName = [System.IO.Path]::GetFileNameWithoutExtension("$(Split-Path -Path $invokerPath -Leaf -Resolve)")
+
+# This script is in $base\lib\scripts
+$base="$PSScriptRoot\..\..\"
+$java_home = "$Env:JAVA_HOME"
+$java_cmd = "$java_home\bin\java.exe"
+$jarpath_dev="$base\org.lflang.cli\build\libs\org.lflang.cli-*-$invokerName.jar"
+$jarpath_release="$base\lib\jars\org.lflang.cli-*-$invokerName.jar"
+
+function Test-Dev {
+    Test-Path "$base\org.lflang.cli" -PathType container
+}
+
+function Get-JarPath {
+    if (Test-Dev) {
+        if (Test-Path $jarpath_dev -PathType leaf) {
+            $jarpath=$(Get-ChildItem $jarpath_dev).toString()
+        } else {
+            throw "Failed to find a copy of the Lingua Franca compiler matching the pattern ""$jarpath_dev"". Did you remember to build?"
+        }
+    } else {
+        if (Test-Path $jarpath_release -PathType leaf) {
+            $jarpath=$(Get-ChildItem $jarpath_release).toString()
+        } else {
+            throw "Failed to find a copy of the Lingua Franca compiler matching the pattern ""$jarpath_release""."
+        }
+    }
+    $jarpath
+}
+
+# check if we can find java executable in $java_home
+if (-not (Test-Path $java_cmd)) {
+    # otherwise, try to run java directly
+	if (-not (Get-Command java -errorAction SilentlyContinue)) {
+	    throw "JRE not found"
+	}
+    $java_cmd = "java"
+}
+
+# check for correct java version
+$java_version = (Get-Command java | Select-Object -ExpandProperty Version).toString()
+if ([version]$java_version -lt [version]"17.0") {
+    throw "JRE $java_version found but 17.0 or greater is required."
+}
+
+# invoke lff
+& $java_cmd -jar $(Get-JarPath) $args
