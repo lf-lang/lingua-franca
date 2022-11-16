7177e4e7c406ccbac24a3ecfc5a5e51e3c703213
Jakio815 <jakio@hanyang.ac.kr>
2022-11-16 19:43:41 +0900

    Added cmake mac options

--- .github/actions/install-rti/install.sh
+++ .github/actions/install-rti/install.sh
@@ -2,6 +2,9 @@
 cd org.lflang/src/lib/c/reactor-c/core/federated/RTI
 mkdir build
 cd build
-cmake -DAUTH=ON ../
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DAUTH=ON ../
+else
+    cmake -DAUTH=ON ../
 make
 sudo make install
