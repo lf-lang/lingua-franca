7082ffcd1f11e3b27cc11b4c5285586ade6f63d6
Jakio815 <jakio@hanyang.ac.kr>
2022-11-21 20:18:37 +0900

    FIX bugs

--- .github/actions/install-rti/install.sh
+++ .github/actions/install-rti/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd org.lflang/src/lib/c/reactor-c/core/federated/RTI
 mkdir build
+echo bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 cd build
 # if [[ "$OSTYPE" == "darwin"* ]]; then
 #     cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DAUTH=ON ../
 # else
 #     cmake -DAUTH=ON ../
 # fi
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 echo $OPENSSL_ROOT_DIR
 cmake -DAUTH=ON ../
 make
