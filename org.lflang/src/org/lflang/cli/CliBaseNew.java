c3c43b2e53f4f5db0208a05ccf210b7ae872eba6
Atharva Patil <atharva.patil@berkeley.edu>
2023-01-11 02:04:59 +0900

    Migrated Lff.java from Apache Commons CLI to Picocli and created a temporary class CliBaseNew.java to support the changes.

---
+++ org.lflang/src/org/lflang/cli/CliBaseNew.java
@@ -0,0 +1,208 @@
+package org.lflang.cli;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Model.OptionSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParseResult;
+import picocli.CommandLine.Spec;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
+
+import org.lflang.ErrorReporter;
+import org.lflang.LFRuntimeModule;
+import org.lflang.LFStandaloneSetup;
+import org.lflang.LocalStrings;
+import org.lflang.generator.LFGeneratorContext.BuildParm;
+import org.lflang.util.FileUtil;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+
+public abstract class CliBaseNew implements Runnable {
+    @Spec CommandSpec spec;
+
+    /*
+     * Options and parameters present in both Lfc and Lff.
+     */
+    @Parameters(
+        arity = "1..",
+        paramLabel = "<path1> <path2>",
+        description = "Paths of the files to run the formatter on.")
+    protected List<String> files;
+
+    /**
+     * Used to collect all errors that happen during validation/generation.
+     */
+    @Inject
+    protected IssueCollector issueCollector;
+
+    /**
+     * Used to report error messages at the end.
+     */
+    @Inject
+    protected ReportingBackend reporter;
+
+    /**
+     * Used to report error messages at the end.
+     */
+    @Inject
+    protected ErrorReporter errorReporter;
+
+    /**
+     * IO context of this run.
+     */
+    @Inject
+    protected Io io;
+    
+    /**
+     * Injected resource provider.
+     */
+    @Inject
+    private Provider<ResourceSet> resourceSetProvider;
+    
+    /**
+     * Injected resource validator.
+     */
+    @Inject
+    private IResourceValidator validator;
+
+    /** Name of the program, eg "lfc". */
+    // private final String toolName;
+
+    // protected CliBaseNew(String toolName) {
+    //     this.toolName = toolName;
+    // }
+
+    protected static void cliMain(
+            String toolName, Class<? extends CliBaseNew> toolClass,
+            Io io, String[] args) {
+        // Injector used to obtain Main instance.
+        final Injector injector = getInjector(toolName, io);
+        // Main instance.
+        final CliBaseNew main = injector.getInstance(toolClass);
+        // Parse arguments and execute main logic.
+        new CommandLine(main).execute(args);
+    }
+
+    public abstract void run();
+    protected abstract void runTool(List<Path> inputFiles);
+
+    protected static Injector getInjector(String toolName, Io io) {
+        final ReportingBackend reporter 
+            = new ReportingBackend(io, toolName + ": ");
+
+        // Injector used to obtain Main instance.
+        return new LFStandaloneSetup(
+            new LFRuntimeModule(),
+            new LFStandaloneModule(reporter, io)
+        ).createInjectorAndDoEMFRegistration();
+    }
+
+    /**
+     * Resolve to an absolute path, in the given {@link #io} context.
+     */
+    protected Path toAbsolutePath(Path other) {
+        return io.getWd().resolve(other).toAbsolutePath();
+    }
+
+    /**
+     * Store command-line arguments as properties, to be passed on to the runtime.
+     */
+    protected Properties filterProps() {
+        Properties props = new Properties();
+        for (OptionSpec option : spec.options()) {
+            props.setProperty(option.longestName(), option.getValue());
+        }
+        return props;
+    }
+
+    /**
+     * If some errors were collected, print them and abort execution. Otherwise, return.
+     */
+    protected void exitIfCollectedErrors() {
+        if (issueCollector.getErrorsOccurred()) {
+            // if there are errors, don't print warnings.
+            List<LfIssue> errors = printErrorsIfAny();
+            String cause = errors.size() + " previous error";
+            if (errors.size() > 1) {
+                cause += 's';
+            }
+            reporter.printFatalErrorAndExit("Aborting due to " + cause + '.');
+        }
+    }
+
+    /**
+     * If any errors were collected, print them, then return them.
+     * @return A list of collected errors.
+     */
+    public List<LfIssue> printErrorsIfAny() {
+        List<LfIssue> errors = issueCollector.getErrors();
+        errors.forEach(reporter::printIssue);
+        return errors;
+    }
+
+    /**
+     * Validates a given resource. If issues arise during validation,
+     * these are recorded using the issue collector.
+     *
+     * @param resource The resource to validate.
+     */
+    public void validateResource(Resource resource) {
+        assert resource != null;
+
+        List<Issue> issues = this.validator.validate(
+                resource, CheckMode.ALL, CancelIndicator.NullImpl);
+
+        for (Issue issue : issues) {
+            URI uri = issue.getUriToProblem(); // Issues may also relate to imported resources.
+            try {
+                issueCollector.accept(
+                        new LfIssue(
+                            issue.getMessage(),
+                            issue.getSeverity(),
+                            issue.getLineNumber(),
+                            issue.getColumn(),
+                            issue.getLineNumberEnd(),
+                            issue.getColumnEnd(),
+                            issue.getLength(),
+                            FileUtil.toPath(uri)));
+            } catch (IOException e) {
+                reporter.printError(
+                        "Unable to convert '" + uri + "' to path." + e);
+            }
+        }
+    }
+
+    /**
+     * Obtains a resource from a path. Returns null if path is not an LF file.
+     *
+     * @param path The path to obtain the resource from.
+     * @return The obtained resource. Set to null if path is not an LF file.
+     */
+    public Resource getResource(Path path) {
+        final ResourceSet set = this.resourceSetProvider.get();
+        try {
+            return set.getResource(URI.createFileURI(path.toString()), true);
+        } catch (RuntimeException e) {
+            return null;
+        }
+    }
+}
--- org.lflang/src/org/lflang/cli/Lff.java
+++ org.lflang/src/org/lflang/cli/Lff.java
@@ -1,4 +1,3 @@
-/** Stand-alone version of the Lingua Franca formatter (lff). */
 package org.lflang.cli;
 
 import java.io.IOException;
@@ -11,235 +10,214 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import org.eclipse.emf.ecore.resource.Resource;
 
 import org.lflang.ast.FormattingUtils;
+import org.lflang.LocalStrings;
 import org.lflang.util.FileUtil;
 
-/**
- * Standalone version of the Lingua Franca formatter (lff). Based on lfc.
- *
- * @author {Marten Lohstroh <marten@berkeley.edu>}
- * @author {Christian Menard <christian.menard@tu-dresden.de>}
- * @author {Billy Bao <billybao@berkeley.edu>}
- */
-public class Lff extends CliBase {
+@Command(
+    name = "lff",
+    // Automatically add usageHelp and versionHelp options.
+    mixinStandardHelpOptions = true,
+    version = "0.3.1-SNAPSHOT")
+public class Lff extends CliBaseNew {
+    /**
+     * Supported CLI options for LFF.
+     */
+    @Option(
+        names = {"-d", "--dry-run"},
+        description = "Send the formatted file contents to stdout"
+                        + " without writing to the file system.")
+    private boolean dryRun = false;
 
-  public Lff() {
-    super("lff");
-  }
+    @Option(
+        names = {"-w", "--wrap"},
+        description = "Causes the formatter to line wrap the files to a"
+                        + " specified length.",
+        defaultValue = "" + FormattingUtils.DEFAULT_LINE_LENGTH,
+        fallbackValue = "" + FormattingUtils.DEFAULT_LINE_LENGTH)
+    private int lineLength;
 
-  /**
-   * Supported CLI options.
-   *
-   * <p>Stores an Apache Commons CLI Option for each entry, sets it to be required if so
-   * specified,
-   * and stores whether to pass the option to the code generator.
-   *
-   * @author Marten Lohstroh <marten@berkeley.edu>
-   * @author {Billy Bao <billybao@berkeley.edu>}
-   */
-  enum CLIOption {
-    DRY_RUN(
-        "d",
-        "dry-run",
-        false,
-        false,
-        "Send the formatted file contents to stdout without writing to the file system."),
-    LINE_WRAP(
-        "w",
-        "wrap",
-        true,
-        false,
-        "Causes the formatter to line wrap the files to a specified length."),
-    NO_RECURSE(
-        null,
-        "no-recurse",
-        false,
-        false,
-        "Do not format files in subdirectories of the specified paths."),
-    OUTPUT_PATH(
-        "o",
-        "output-path",
-        true,
-        false,
-        "If specified, outputs all formatted files into this directory instead of"
-            + " overwriting the original files. Subdirectory structure will be preserved."),
-    VERBOSE("v", "verbose", false, false, "Print more details on files affected."),
-    ;
+    @Option(
+        names = "--no-recurse",
+        description = "Do not format files in subdirectories of the specified paths.")
+    private boolean noRecurse = false;
 
-    /** The corresponding Apache CLI Option object. */
-    public final Option option;
+    @Option(
+        names = {"-o", "--output-path"},
+        defaultValue = "",
+        fallbackValue = "",
+        description = "If specified, outputs all formatted files into this" 
+                        + " directory instead of overwriting the original"
+                        + " files. Subdirectory structure will be preserved.")
+    private String outputPath;
+
+    @Option(
+        names = {"-v", "--verbose"},
+        description = "Print more details on files affected.")
+    private boolean verbose = false;
+
+    // public Lff() {
+    //     super("lff");
+    // }
 
     /**
-     * Construct a new CLIOption.
+     * Programmatic entry point, with a custom IO.
      *
-     * @param opt The short option name. E.g.: "f" denotes a flag "-f".
-     * @param longOpt The long option name. E.g.: "foo" denotes a flag "--foo".
-     * @param hasArg Whether or not this option has an argument. E.g.: "--foo bar" where "bar" is
-     *     the argument value.
-     * @param isReq Whether or not this option is required. If it is required but not specified a
-     *     menu is shown.
-     * @param description The description used in the menu.
+     * @param io IO streams.
+     * @param args Command-line arguments.
      */
-    CLIOption(String opt, String longOpt, boolean hasArg, boolean isReq, String description) {
-      this.option = new Option(opt, longOpt, hasArg, description);
-      option.setRequired(isReq);
+    public static void main(Io io, final String... args) {
+        cliMain("lff", Lff.class, io, args);
     }
 
-    /**
-     * Create an Apache Commons CLI Options object and add all the options.
-     *
-     * @return Options object that includes all the options in this enum.
+    public static void main(String[] args) {
+        main(Io.SYSTEM, args);
+    }
+
+    /*
+     * The first method in LFF that is invoked when the parent CliBase Runnable
+     * class is instantiated, i.e. the first method to run after the arguments
+     * are parsed.
      */
-    public static Options getOptions() {
-      Options opts = new Options();
-      Arrays.asList(CLIOption.values()).forEach(o -> opts.addOption(o.option));
-      return opts;
-    }
-  }
-
-
-  /**
-   * Entry point of the formatter.
-   * Caution: this will invoke System.exit.
-   *
-   * @param args CLI arguments
-   */
-  public static void main(final String[] args) {
-    main(Io.SYSTEM, args);
-  }
-
-  /**
-   * Programmatic entry point, with a custom IO.
-   *
-   * @param io IO streams.
-   * @param args Command-line arguments.
-   */
-  public static void main(Io io, final String... args) {
-    cliMain("lff", Lff.class, io, args);
-  }
-
-
-  @Override
-  protected Options getOptions() {
-    return CLIOption.getOptions();
-  }
-
-  /**
-   * Check all given input paths and the output path, then invokes the formatter on all files
-   * given.
-   */
-  @Override
-  protected void runTool(CommandLine cmd, List<Path> files) {
-    String pathOption = CLIOption.OUTPUT_PATH.option.getOpt();
-    final Path outputRoot;
-    if (cmd.hasOption(pathOption)) {
-      outputRoot = io.getWd().resolve(cmd.getOptionValue(pathOption)).toAbsolutePath().normalize();
-      if (!Files.exists(outputRoot)) {
-        reporter.printFatalErrorAndExit("Output location '" + outputRoot + "' does not exist.");
-      }
-      if (!Files.isDirectory(outputRoot)) {
-        reporter.printFatalErrorAndExit("Output location '" + outputRoot + "' is not a directory.");
-      }
-    } else {
-      outputRoot = null;
-    }
-
-    for (Path path : files) {
-      if (!Files.exists(path)) {
-        reporter.printFatalErrorAndExit(path + ": No such file or directory");
-      }
-    }
-
-    final int lineLength =
-        !cmd.hasOption(CLIOption.LINE_WRAP.option.getOpt())
-            ? FormattingUtils.DEFAULT_LINE_LENGTH
-            : Integer.parseInt(cmd.getOptionValue(CLIOption.LINE_WRAP.option.getOpt()));
-
-    final boolean dryRun = cmd.hasOption(CLIOption.DRY_RUN.option.getOpt());
-
-    boolean verbose = cmd.hasOption(CLIOption.VERBOSE.option.getOpt());
-    for (Path relPath : files) {
-      if (verbose) {
-        reporter.printInfo("Formatting " + io.getWd().relativize(relPath) + ":");
-      }
-      Path path = toAbsolutePath(relPath);
-      if (Files.isDirectory(path) && !cmd.hasOption(CLIOption.NO_RECURSE.option.getLongOpt())) {
-        // this is a directory, walk its contents.
+    @Override
+    public void run() {
+        // TODO: Directly load paths instead of converting strings to paths.
         try {
-          Files.walkFileTree(path, new SimpleFileVisitor<>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-              formatSingleFile(file, path, outputRoot, lineLength, dryRun, verbose);
-              return FileVisitResult.CONTINUE;
+            List<Path> paths = files.stream().map(
+                    io.getWd()::resolve).collect(Collectors.toList());
+            runTool(paths);
+        } catch (RuntimeException e) {
+            reporter.printFatalErrorAndExit("An unexpected error occurred:", e);
+        }
+        io.callSystemExit(0);
+    }
+
+    /**
+     * Check all given input paths and the output path, then invokes the formatter on all files
+     * given.
+     */
+    @Override
+    protected void runTool(List<Path> files) {
+        final Path outputRoot;
+        if (!outputPath.isEmpty()){
+            outputRoot = 
+                io.getWd().resolve(outputPath).toAbsolutePath().normalize();
+            if (!Files.exists(outputRoot)) {
+                reporter.printFatalErrorAndExit(
+                        "Output location '" + outputRoot + "' does not exist.");
             }
-          });
-        } catch (IOException e) {
-          reporter.printError("IO error: " + e);
+            if (!Files.isDirectory(outputRoot)) {
+                reporter.printFatalErrorAndExit(
+                        "Output location '"
+                        + outputRoot + "' is not a directory.");
+            }
+        } else {
+            outputRoot = null;
         }
 
-      } else {
-        // Simple file
-        formatSingleFile(path, path.getParent(), outputRoot, lineLength, dryRun, verbose);
-      }
-    }
-
-    exitIfCollectedErrors();
-    if (!dryRun || verbose) {
-      reporter.printInfo("Done formatting.");
-    }
-  }
-
-  /** Load and validate a single file, then format it and output to the given outputPath. */
-  private void formatSingleFile(Path file, Path inputRoot, Path outputRoot, int lineLength, boolean dryRun, boolean verbose) {
-    file = file.normalize();
-    Path outputPath = outputRoot == null
-        ? file // format in place
-        : outputRoot.resolve(inputRoot.relativize(file)).normalize();
-    final Resource resource = getResource(file);
-    if (resource == null) {
-      if (verbose) {
-        reporter.printInfo("Skipped " + file + ": not an LF file");
-      }
-      return; // not an LF file, nothing to do here
-    }
-    validateResource(resource);
-
-    // todo don't abort whole run if one file has errors
-    exitIfCollectedErrors();
-    final String formattedFileContents =
-        FormattingUtils.render(resource.getContents().get(0), lineLength);
-
-    if (dryRun) {
-      io.getOut().print(formattedFileContents);
-    } else {
-      try {
-        FileUtil.writeToFile(formattedFileContents, outputPath, true);
-      } catch (IOException e) {
-        if (e instanceof FileAlreadyExistsException) {
-          // only happens if a subdirectory is named with ".lf" at the end
-          reporter.printFatalErrorAndExit(
-              "Error writing to "
-                  + outputPath
-                  + ": file already exists. Make sure that no file or directory"
-                  + " within provided input paths have the same relative paths.");
+        for (Path path : files) {
+            if (!Files.exists(path)) {
+                reporter.printFatalErrorAndExit(
+                        path + ": No such file or directory");
+            }
         }
-      }
+
+        for (Path relativePath : files) {
+            if (verbose) {
+                reporter.printInfo("Formatting "
+                        + io.getWd().relativize(relativePath) + ":");
+            }
+            Path path = toAbsolutePath(relativePath);
+            if (Files.isDirectory(path) && !noRecurse) {
+                // this is a directory, walk its contents.
+                try {
+                    Files.walkFileTree(path, new SimpleFileVisitor<>() {
+                        @Override
+                        public FileVisitResult visitFile(
+                                Path file, BasicFileAttributes attrs) {
+                            formatSingleFile(
+                                file, path, outputRoot,
+                                lineLength, dryRun, verbose);
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
+                } catch (IOException e) {
+                    reporter.printError("IO error: " + e);
+                }
+
+            } else {
+                // Simple file
+                formatSingleFile(
+                    path, path.getParent(), outputRoot,
+                    lineLength, dryRun, verbose);
+            }
+        }
+
+        exitIfCollectedErrors();
+        if (!dryRun || verbose) {
+            reporter.printInfo("Done formatting.");
+        }
     }
 
-    exitIfCollectedErrors();
-    issueCollector.getAllIssues().forEach(reporter::printIssue);
-    if (verbose) {
-      String msg = "Formatted " + io.getWd().relativize(file);
-      if (file != outputPath) msg += " -> " + io.getWd().relativize(outputPath);
-      reporter.printInfo(msg);
-    }
-  }
+    private void formatSingleFile(
+            Path file, Path inputRoot, Path outputRoot,
+            int lineLength, boolean dryRun, boolean verbose) {
+        file = file.normalize();
+        Path outputPath = outputRoot == null
+            ? file // format in place
+            : outputRoot.resolve(inputRoot.relativize(file)).normalize();
 
+        final Resource resource = getResource(file);
+        // Skip file if not an LF file.
+        if (resource == null) {
+            if (verbose) {
+                reporter.printInfo("Skipped " + file + ": not an LF file");
+            }
+            return; 
+        }
+        validateResource(resource);
+
+        // todo don't abort whole run if one file has errors
+        exitIfCollectedErrors();
+        final String formattedFileContents =
+            FormattingUtils.render(resource.getContents().get(0), lineLength);
+
+        if (dryRun) {
+            io.getOut().print(formattedFileContents);
+        } else {
+            try {
+                FileUtil.writeToFile(formattedFileContents, outputPath, true);
+            } catch (IOException e) {
+                if (e instanceof FileAlreadyExistsException) {
+                    // Only happens if a subdirectory is named with 
+                    // ".lf" at the end.
+                    reporter.printFatalErrorAndExit(
+                        "Error writing to "
+                        + outputPath
+                        + ": file already exists. Make sure that no file or" 
+                        + " directory within provided input paths have the"
+                        + " same relative paths.");
+                }
+            }
+        }
+
+        exitIfCollectedErrors();
+        issueCollector.getAllIssues().forEach(reporter::printIssue);
+        if (verbose) {
+            String msg = "Formatted " + io.getWd().relativize(file);
+            if (file != outputPath) msg += 
+                " -> " + io.getWd().relativize(outputPath);
+            reporter.printInfo(msg);
+        }
+    }
+    
 }
