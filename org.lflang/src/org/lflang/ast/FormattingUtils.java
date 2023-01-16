3bdeb4cf2903dd176c6308abf7236da6a49f3b22
Marten Lohstroh <marten@berkeley.edu>
2023-01-15 16:25:11 -0800

    Revert "Add missing file"
    This reverts commit f10f5cef9d6801d5c49c6baaf1df0610b8e0c3b9.

--- org.lflang/src/org/lflang/ast/FormattingUtils.java
+++ org.lflang/src/org/lflang/ast/FormattingUtils.java
@@ -67,7 +67,6 @@ public class FormattingUtils {
      */
     public static String render(EObject object, int lineLength, Target target, boolean codeMapTags) {
         MalleableString ms = ToLf.instance.doSwitch(object);
-        String singleLineCommentPrefix = target.
         String singleLineCommentPrefix = target == Target.Python ? "#" : "//";
         ms.findBestRepresentation(
             () -> ms.render(INDENTATION, singleLineCommentPrefix, codeMapTags, null),
--- org.lflang/src/org/lflang/generator/MainContext.java
+++ org.lflang/src/org/lflang/generator/MainContext.java
@@ -37,7 +37,7 @@ public class MainContext implements LFGeneratorContext {
     /** Whether the requested build is required to be complete. */
     private final Mode mode;
 
-    private TargetConfig targetConfig;
+    private final TargetConfig targetConfig;
 
     /** The result of the code generation process. */
     private GeneratorResult result = null;
@@ -96,7 +96,10 @@ public class MainContext implements LFGeneratorContext {
 
         this.errorReporter = constructErrorReporter.apply(this.fileConfig);
 
-        loadTargetConfig();
+        this.targetConfig = GeneratorUtils.getTargetConfig(
+            args, GeneratorUtils.findTarget(fileConfig.resource), errorReporter
+        );
+
 
     }
 
@@ -146,18 +149,5 @@ public class MainContext implements LFGeneratorContext {
     @Override
     public void reportProgress(String message, int percentage) {
         reportProgress.apply(message, percentage);
-    }
-
-    /**
-     * Load the target configuration based on the contents of the resource.
-     * This is done automatically upon instantiation of the context, but
-     * in case the resource changes (e.g., due to an AST transformation),
-     * this method can be called to reload to ensure that the changes are
-     * reflected in the target configuration.
-     */
-    public void loadTargetConfig() {
-        this.targetConfig = GeneratorUtils.getTargetConfig(
-            args, GeneratorUtils.findTarget(fileConfig.resource), errorReporter
-        );
     }
 }
