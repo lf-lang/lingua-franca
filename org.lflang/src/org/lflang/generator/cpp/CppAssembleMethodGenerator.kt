d2efc42c201fd4f5dcb6c1380cc128ff6550a8d8
Christian Menard <christian.menard@tu-dresden.de>
2023-01-17 15:46:57 +0100

    don't bind delayed connections directly

--- org.lflang/src/org/lflang/generator/cpp/CppAssembleMethodGenerator.kt
+++ org.lflang/src/org/lflang/generator/cpp/CppAssembleMethodGenerator.kt
@@ -203,7 +203,7 @@ class CppAssembleMethodGenerator(private val reactor: Reactor) {
      * The body of this method will declare all triggers, dependencies and antidependencies to the runtime.
      */
     fun generateDefinition() = with(PrependOperator) {
-        val indexedConnections = reactor.connections.withIndex()
+        val indexedConnections = reactor.connections.filter { it.delay == null }.withIndex()
         """
             |${reactor.templateLine}
             |void ${reactor.templateName}::assemble() {
