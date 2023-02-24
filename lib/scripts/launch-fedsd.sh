7464e2e3faa533a05e2d24f0632dd6da937a59d9
Edward A. Lee <eal@eecs.berkeley.edu>
2023-02-24 11:58:33 +0100

    Reorganized how to invoke visualization utility

---
+++ lib/scripts/launch-fedsd.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#============================================================================
+# Description:      Visualize federated trace data for RTI-federate interactions.
+# Authors:          Chadlia Jerad
+#                   Edward A. Lee
+# Usage:            Usage: fedsd -r [rti.csv] -f [fed.csv ...]
+#============================================================================
+
+#============================================================================
+# Preamble
+#============================================================================
+
+# Copied from build.sh    FIXME: How to avoid copying
+
+# Find the directory in which this script resides in a way that is compatible
+# with MacOS, which has a `readlink` implementation that does not support the
+# necessary `-f` flag to canonicalize by following every symlink in every
+# component of the given name recursively.
+# This solution, adapted from an example written by Geoff Nixon, is POSIX-
+# compliant and robust to symbolic links. If a chain of more than 1000 links
+# is encountered, we return.
+find_dir() (
+  start_dir=$PWD
+  cd "$(dirname "$1")"
+  link=$(readlink "$(basename "$1")")
+  count=0
+  while [ "${link}" ]; do
+    if [[ "${count}" -lt 1000 ]]; then
+        cd "$(dirname "${link}")"
+        link=$(readlink "$(basename "$1")")
+        ((count++))
+    else
+        return
+    fi
+  done
+  real_path="$PWD/$(basename "$1")"
+  cd "${start_dir}"
+  echo `dirname "${real_path}"`
+)
+
+# Report fatal error and exit.
+function fatal_error() {
+    1>&2 echo -e "\e[1mfedsd: \e[31mfatal error: \e[0m$1"
+    exit 1
+}
+
+abs_path="$(find_dir "$0")"
+
+if [[ "${abs_path}" ]]; then
+    base=`dirname $(dirname ${abs_path})`
+else
+    fatal_error "Unable to determine absolute path to $0."
+fi
+
+# FIXME: Check that python3 is in the path.
+
+python3 "${base}/util/tracing/visualization/fedsd.py" "$@"
