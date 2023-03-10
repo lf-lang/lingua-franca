e3b9622d9965ca3bedc90a61978e3d0a0fa2bbe9
Clément Fournier <clement.fournier@tu-dresden.de>
2023-03-10 14:47:17 +0100

    Make LFF ignore errors

--- org.lflang/src/org/lflang/cli/Lff.java
+++ org.lflang/src/org/lflang/cli/Lff.java
@@ -51,13 +51,18 @@ public class Lff extends CliBase {
     @Option(
         names = "--no-recurse",
         description = "Do not format files in subdirectories of the"
-                    + " specified paths.")
+            + " specified paths.")
     private boolean noRecurse = false;
 
     @Option(
         names = {"-v", "--verbose"},
         description = "Print more details on files affected.")
     private boolean verbose = false;
+
+    @Option(
+        names = {"--ignore-errors"},
+        description = "Ignore validation errors in files and format them anyway.")
+    private boolean ignoreErrors = false;
 
     /**
      * Main function of the formatter.
@@ -151,8 +156,9 @@ public class Lff extends CliBase {
         }
         validateResource(resource);
 
-        // todo don't abort whole run if one file has errors
-        exitIfCollectedErrors();
+        if (!ignoreErrors) {
+            exitIfCollectedErrors();
+        }
         final String formattedFileContents =
             FormattingUtils.render(resource.getContents().get(0), lineLength);
 
@@ -175,7 +181,9 @@ public class Lff extends CliBase {
             }
         }
 
-        exitIfCollectedErrors();
+        if (!ignoreErrors) {
+            exitIfCollectedErrors();
+        }
         // Only errors are printed. Warnings are not helpful for LFF
         // and since they don't prevent the file from being formatted,
         // the position of the issue may be wrong in the formatted file.
