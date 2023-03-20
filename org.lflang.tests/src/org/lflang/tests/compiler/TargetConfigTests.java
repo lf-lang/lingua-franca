44d36f24f21a74d3c1ee551b76f57aeb71f8f07b
Marten Lohstroh <marten@berkeley.edu>
2023-03-20 00:09:31 -0700

    Basic test to verify that target config propagates to federates.

---
+++ org.lflang.tests/src/org/lflang/tests/compiler/TargetConfigTests.java
@@ -0,0 +1,90 @@
+package org.lflang.tests.compiler;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.nio.file.Files;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.lflang.federated.generator.FedFileConfig;
+import org.lflang.generator.LFGenerator;
+import org.lflang.generator.LFGeneratorContext.Mode;
+import org.lflang.generator.MainContext;
+import org.lflang.lf.Model;
+import org.lflang.tests.LFInjectorProvider;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(LFInjectorProvider.class)
+
+/**
+ *
+ */
+class TargetConfigTests {
+
+    @Inject
+    ParseHelper<Model> parser;
+
+    @Inject
+    LFGenerator generator;
+
+    @Inject
+    JavaIoFileSystemAccess fileAccess;
+
+    @Inject
+    Provider<ResourceSet> resourceSetProvider;
+
+    private void assertHasTargetProperty(Model model, String name) {
+        Assertions.assertNotNull(model);
+        Assertions.assertTrue(
+            model.getTarget().getConfig().getPairs().stream().anyMatch(
+                p -> p.getName().equals(name)
+            )
+        );
+    }
+
+    @Test
+    public void testParsing() throws Exception {
+        assertHasTargetProperty(parser.parse("""
+            target C {
+              tracing: true
+            }
+            """), "tracing");
+    }
+
+    @Test
+    public void testFederation() throws Exception {
+        fileAccess.setOutputPath("junit"); // FIXME: this does not appear to do anything. Why?
+        fileAccess.setCurrentSource("Federation.lf");
+        Model federation = parser.parse("""
+            target C {
+              tracing: true
+            }
+            reactor Foo {
+            
+            }
+            federated reactor {
+                a = new Foo()
+                b = new Foo()
+            }
+            """, URI.createFileURI("Federation.lf"), resourceSetProvider.get());
+        assertHasTargetProperty(federation, "tracing");
+
+        var resource = federation.eResource();
+        var context = new MainContext(Mode.STANDALONE, resource, fileAccess, () -> false);
+
+        generator.doGenerate(resource, fileAccess, context);
+
+        String lfSrc = Files.readAllLines(
+            ((FedFileConfig)context.getFileConfig()).getSrcPath().resolve("a.lf")
+        ).stream().reduce("\n", String::concat);
+        Model federate = parser.parse(lfSrc);
+        assertHasTargetProperty(federate, "tracing");
+    }
+}
