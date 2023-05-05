67ee262ae5b575f674635fe33ec7475edafae0b1
Anirudh Rengarajan <44007330+arengarajan99@users.noreply.github.com>
2023-05-04 21:14:48 -0700

    Add pair class to prevent reliance on Kotlin

---
+++ org.lflang/src/org/lflang/util/Pair.java
@@ -0,0 +1,21 @@
+package org.lflang.util;
+
+public class Pair<F, S> {
+    private final F first; 
+    private final S second; 
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
+
+//TimeValue maxSTP = findMaxSTP(connection, coordination);
