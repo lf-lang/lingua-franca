15222f117eb2e00f16495ea14b28fc91678c8363
erlingrj <erling.jellum@gmail.com>
2023-06-09 22:24:02 +0200

    Rename lf test because of naming conflict with windows

--- test/C/c/sendreceive.c
+++ test/C/c/sendreceive.c
@@ -4,11 +4,11 @@
 #include "../include/IntPrint/Check.h"
 #include "../include/api/set.h"
 
-void send(print_self_t* self, print_out_t* out) {
+void sender(print_self_t* self, print_out_t* out) {
     lf_set(out, 42);
 }
 
-void receive(check_self_t* self, check_in_t* in) {
+void receiver(check_self_t* self, check_in_t* in) {
     printf("Received: %d\n", in->value);
     if (in->value != self->expected) {
         printf("ERROR: Expected value to be %d.\n", self->expected);
