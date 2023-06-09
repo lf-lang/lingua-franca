54ad2b63b6a7cced4ae7ec85b40ce443520f59e0
Marten Lohstroh <marten@berkeley.edu>
2023-06-09 13:41:12 -0700

    Fix quotation problem

--- .github/scripts/check-diff.sh
+++ .github/scripts/check-diff.sh
@@ -4,7 +4,7 @@ changes() {
   git diff --name-only HEAD $(git merge-base HEAD origin/master)
 }
 
-if changes | grep $1 | grep -q -v '^.*md\|txt$'; then
+if changes | grep "$1" | grep -q -v "^.*md\|txt$"; then
   echo "changed_$2=1" >> $GITHUB_OUTPUT
 else
   echo "changed_$2=0" >> $GITHUB_OUTPUT
