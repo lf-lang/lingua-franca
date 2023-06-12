48e512811d84598f693817f28e7df37dce24e93a
Christian Menard <christian.menard@tu-dresden.de>
2023-06-12 15:55:00 +0200

    also pass cmake arguments in ros2 compilation

--- core/src/main/kotlin/org/lflang/generator/cpp/CppPlatformGenerator.kt
+++ core/src/main/kotlin/org/lflang/generator/cpp/CppPlatformGenerator.kt
@@ -22,4 +22,14 @@ abstract class CppPlatformGenerator(protected val generator: CppGenerator) {
     abstract fun generatePlatformFiles()
 
     abstract fun doCompile(context: LFGeneratorContext, onlyGenerateBuildFiles: Boolean = false): Boolean
+
+    protected val cmakeArgs: List<String>
+        get() = listOf(
+            "-DCMAKE_BUILD_TYPE=${targetConfig.cmakeBuildType}",
+            "-DREACTOR_CPP_VALIDATE=${if (targetConfig.noRuntimeValidation) "OFF" else "ON"}",
+            "-DREACTOR_CPP_PRINT_STATISTICS=${if (targetConfig.printStatistics) "ON" else "OFF"}",
+            "-DREACTOR_CPP_TRACE=${if (targetConfig.tracing != null) "ON" else "OFF"}",
+            "-DREACTOR_CPP_LOG_LEVEL=${targetConfig.logLevel.severity}",
+            "-DLF_SRC_PKG_PATH=${fileConfig.srcPkgPath}",
+        )
 }
