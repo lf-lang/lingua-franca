4f614b2e23fe6d5ff7a0e2ba8d7995b2b1e72869
Clément Fournier <clement.fournier@tu-dresden.de>
2023-06-13 15:12:30 +0200

    Another cleanup

--- core/src/main/java/org/lflang/generator/HumanReadableReportingStrategy.java
+++ core/src/main/java/org/lflang/generator/HumanReadableReportingStrategy.java
@@ -4,12 +4,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.lsp4j.DiagnosticSeverity;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure0;
 
 import org.lflang.ErrorReporter;
 
@@ -95,27 +93,27 @@ public class HumanReadableReportingStrategy implements DiagnosticReporting.Strat
     Matcher matcher = diagnosticMessagePattern.matcher(stripEscaped(line));
     if (matcher.matches()) {
       final Path path = Paths.get(matcher.group("path"));
+      final DiagnosticSeverity severity = DiagnosticReporting.severityOf(matcher.group("severity"));
+
+      String column = matcher.group("column");
       final Position generatedFilePosition =
           Position.fromOneBased(
               Integer.parseInt(matcher.group("line")),
-              Integer.parseInt(
-                  matcher.group("column") != null
-                      ? matcher.group("column")
-                      : "0") // FIXME: Unreliable heuristic
-              );
+              column == null ? 1 // FIXME: Unreliable heuristic
+                  : Integer.parseInt(column)
+          );
       final String message =
           DiagnosticReporting.messageOf(matcher.group("message"), path, generatedFilePosition);
       final CodeMap map = maps.get(relativeTo != null ? relativeTo.resolve(path) : path);
-      final DiagnosticSeverity severity = DiagnosticReporting.severityOf(matcher.group("severity"));
       if (map == null) {
         errorReporter.nowhere().report(severity, message);
         return;
       }
       for (Path srcFile : map.lfSourcePaths()) {
         Position lfFilePosition = map.adjusted(srcFile, generatedFilePosition);
-        if (matcher.group("column") != null) {
-          reportAppropriateRange(
-              range -> errorReporter.at(srcFile, range).report(severity, message), lfFilePosition, it);
+        if (column != null) {
+          Range range = findAppropriateRange(lfFilePosition, it);
+          errorReporter.at(srcFile, range).report(severity, message);
         } else {
           errorReporter.at(srcFile, lfFilePosition.getOneBasedLine()).report(severity, message);
         }
@@ -124,38 +122,28 @@ public class HumanReadableReportingStrategy implements DiagnosticReporting.Strat
   }
 
   /**
-   * Report the appropriate range to {@code report}.
+   * Find the appropriate range to {@code report}.
    *
-   * @param report A reporting method whose first and second parameters are the (included) start and
-   *     (excluded) end of the relevant range.
    * @param lfFilePosition The point about which the relevant range is anchored.
    * @param it An iterator over the lines immediately following a diagnostic message.
    */
-  private void reportAppropriateRange(
-      Consumer<Range> report, Position lfFilePosition, Iterator<String> it) {
-    Procedure0 failGracefully =
-        () -> report.accept(new Range(lfFilePosition, lfFilePosition.plus(" ")));
-    if (!it.hasNext()) {
-      failGracefully.apply();
-      return;
+  private Range findAppropriateRange(Position lfFilePosition, Iterator<String> it) {
+    while (it.hasNext()) {
+      String line = it.next();
+      Matcher labelMatcher = labelPattern.matcher(line);
+      if (labelMatcher.find()) {
+        Position start = Position.fromZeroBased(
+            lfFilePosition.getZeroBasedLine(),
+            lfFilePosition.getZeroBasedColumn() - labelMatcher.group(1).length());
+        Position end = lfFilePosition.plus(labelMatcher.group(2));
+        return new Range(start, end);
+      } else if (diagnosticMessagePattern.matcher(line).find()) {
+        bufferedLine = line;
+        break;
+      }
     }
-    String line = it.next();
-    Matcher labelMatcher = labelPattern.matcher(line);
-    if (labelMatcher.find()) {
-      report.accept(
-          new Range(
-              Position.fromZeroBased(
-                  lfFilePosition.getZeroBasedLine(),
-                  lfFilePosition.getZeroBasedColumn() - labelMatcher.group(1).length()),
-              lfFilePosition.plus(labelMatcher.group(2))));
-      return;
-    }
-    if (diagnosticMessagePattern.matcher(line).find()) {
-      failGracefully.apply();
-      bufferedLine = line;
-      return;
-    }
-    reportAppropriateRange(report, lfFilePosition, it);
+    // fallback, we didn't find it.
+    return new Range(lfFilePosition, lfFilePosition.plus(" "));
   }
 
   /**
