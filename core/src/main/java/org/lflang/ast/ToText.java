a9a34db552daba6609069c660198b2121b9f897f
Peter Donovan <peterdonovan@berkeley.edu>
2023-06-13 17:39:52 -0700

    Do not capture certain comments in toText.

--- core/src/main/java/org/lflang/ast/ToText.java
+++ core/src/main/java/org/lflang/ast/ToText.java
@@ -47,21 +47,15 @@ public class ToText extends LfSwitch<String> {
     ICompositeNode node = NodeModelUtils.getNode(code);
     if (node != null) {
       StringBuilder builder = new StringBuilder(Math.max(node.getTotalLength(), 1));
+      boolean started = false;
       for (ILeafNode leaf : node.getLeafNodes()) {
-        builder.append(leaf.getText());
+        if (!leaf.getText().contains("{=")
+            && (leaf.getText().contains("\n") && !ASTUtils.isComment(leaf))) {
+          started = true;
+        }
+        if (started && !leaf.getText().contains("=}")) builder.append(leaf.getText());
       }
       String str = builder.toString().trim();
-      // Remove the code delimiters (and any surrounding comments).
-      // This assumes any comment before {= does not include {=.
-      int start = str.indexOf("{=");
-      int end = str.lastIndexOf("=}");
-      if (start == -1 || end == -1) {
-        // Silent failure is needed here because toText is needed to create the intermediate
-        // representation,
-        // which the validator uses.
-        return str;
-      }
-      str = str.substring(start + 2, end);
       if (str.split("\n").length > 1) {
         // multi line code
         return StringUtil.trimCodeBlock(str, 1);
