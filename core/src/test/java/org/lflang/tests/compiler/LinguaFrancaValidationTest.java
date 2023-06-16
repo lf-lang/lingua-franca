fddce8e7ee5fa0d305228527cf09cb1084a52275
Clément Fournier <clement.fournier@tu-dresden.de>
2023-06-16 15:42:21 +0200

    Also use dynamic tests for target property checks

--- core/src/test/java/org/lflang/tests/compiler/LinguaFrancaValidationTest.java
+++ core/src/test/java/org/lflang/tests/compiler/LinguaFrancaValidationTest.java
@@ -28,17 +28,27 @@
 package org.lflang.tests.compiler;
 
 import com.google.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
 import org.eclipse.xtext.testing.validation.ValidationTestHelper;
 import org.eclipse.xtext.validation.Issue;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
 import org.lflang.Target;
 import org.lflang.TargetProperty;
 import org.lflang.TargetProperty.ArrayType;
@@ -55,8 +65,6 @@ import org.lflang.lf.Visibility;
 import org.lflang.tests.LFInjectorProvider;
 import org.lflang.util.StringUtil;
 
-@ExtendWith(InjectionExtension.class)
-@InjectWith(LFInjectorProvider.class)
 
 /**
  * Collection of unit tests to ensure validation is done correctly.
@@ -67,6 +75,8 @@ import org.lflang.util.StringUtil;
  * @author Christian Menard
  * @author Alexander Schulz-Rosengarten
  */
+@ExtendWith(InjectionExtension.class)
+@InjectWith(LFInjectorProvider.class)
 public class LinguaFrancaValidationTest {
 
   @Inject ParseHelper<Model> parser;
@@ -1216,13 +1226,15 @@ public class LinguaFrancaValidationTest {
       Map.of(
           PrimitiveType.BOOLEAN, List.of("1 sec", "foo", "\"foo\"", "[1]", "{baz: 42}", "'c'"),
           PrimitiveType.INTEGER,
-              List.of("foo", "\"bar\"", "1 sec", "[1, 2]", "{foo: \"bar\"}", "'c'"),
+          List.of("foo", "\"bar\"", "1 sec", "[1, 2]", "{foo: \"bar\"}", "'c'"),
           PrimitiveType.NON_NEGATIVE_INTEGER,
-              List.of("-42", "foo", "\"bar\"", "1 sec", "[1, 2]", "{foo: \"bar\"}", "'c'"),
+          List.of("-42", "foo", "\"bar\"", "1 sec", "[1, 2]", "{foo: \"bar\"}", "'c'"),
           PrimitiveType.TIME_VALUE,
-              List.of(
-                  "foo", "\"bar\"", "\"3 sec\"", "\"4 weeks\"", "[1, 2]", "{foo: \"bar\"}", "'c'"),
-          PrimitiveType.STRING, List.of("1 msec", "[1, 2]", "{foo: \"bar\"}", "'c'"));
+          List.of(
+              "foo", "\"bar\"", "\"3 sec\"", "\"4 weeks\"", "[1, 2]", "{foo: \"bar\"}", "'c'"),
+          PrimitiveType.STRING, List.of("1 msec", "[1, 2]", "{foo: \"bar\"}", "'c'"),
+          PrimitiveType.FILE, List.of("\"\"")
+      );
 
   /**
    * Maps a type to a list, each entry of which represents a list with three entries: a known wrong
@@ -1361,8 +1373,11 @@ public class LinguaFrancaValidationTest {
     if (type instanceof PrimitiveType) {
       Map<PrimitiveType, List<String>> values =
           correct ? primitiveTypeToKnownGood : primitiveTypeToKnownBad;
+      if (type == PrimitiveType.FILE) {
+        return Collections.emptyList();
+      }
       List<String> examples = values.get(type);
-      Assertions.assertNotNull(examples);
+      Assertions.assertNotNull(examples, "No examples for " + type);
       return examples;
     } else {
       if (type instanceof UnionType) {
@@ -1386,7 +1401,6 @@ public class LinguaFrancaValidationTest {
    */
   private Model createModel(TargetProperty key, String value) throws Exception {
     String target = key.supportedBy.get(0).getDisplayName();
-    System.out.printf("%s: %s%n", key, value);
     return parseWithoutError(
         """
                 target %s {%s: %s};
@@ -1399,29 +1413,34 @@ public class LinguaFrancaValidationTest {
   }
 
   /** Perform checks on target properties. */
-  @Test
-  public void checkTargetProperties() throws Exception {
+  @TestFactory
+  public Collection<DynamicTest> checkTargetProperties() throws Exception {
+    List<DynamicTest> result = new ArrayList<>();
+
     for (TargetProperty prop : TargetProperty.getOptions()) {
       if (prop == TargetProperty.CARGO_DEPENDENCIES) {
         // we test that separately as it has better error messages
-        return;
+        continue;
       }
-      System.out.printf("Testing target property %s which is %s%n", prop, prop.type);
-      System.out.println("====");
-      System.out.println("Known good assignments:");
       List<String> knownCorrect = synthesizeExamples(prop.type, true);
 
       for (String it : knownCorrect) {
-        Model model = createModel(prop, it);
-        validator.assertNoErrors(model);
-        // Also make sure warnings are produced when files are not present.
-        if (prop.type == PrimitiveType.FILE) {
-          validator.assertWarning(
-              model,
-              LfPackage.eINSTANCE.getKeyValuePair(),
-              null,
-              String.format("Could not find file: '%s'.", StringUtil.removeQuotes(it)));
-        }
+        var test = DynamicTest.dynamicTest(
+            "Property %s (%s) - known good assignment: %s".formatted(prop, prop.type, it),
+            () -> {
+              Model model = createModel(prop, it);
+              validator.assertNoErrors(model);
+              // Also make sure warnings are produced when files are not present.
+              if (prop.type == PrimitiveType.FILE) {
+                validator.assertWarning(
+                    model,
+                    LfPackage.eINSTANCE.getKeyValuePair(),
+                    null,
+                    String.format("Could not find file: '%s'.", StringUtil.removeQuotes(it)));
+              }
+            }
+        );
+        result.add(test);
       }
 
       // Extra checks for filenames. (This piece of code was commented out in the original xtend
@@ -1439,47 +1458,50 @@ public class LinguaFrancaValidationTest {
       //                ]
       //            }
 
-      System.out.println("Known bad assignments:");
       List<String> knownIncorrect = synthesizeExamples(prop.type, false);
       if (!(knownIncorrect == null || knownIncorrect.isEmpty())) {
         for (String it : knownIncorrect) {
-          if (prop.type instanceof StringDictionaryType) {
-            validator.assertError(
-                createModel(prop, it),
-                LfPackage.eINSTANCE.getKeyValuePair(),
-                null,
-                String.format("Target property '%s.", prop),
-                "' is required to be a string.");
-          } else {
-            validator.assertError(
-                createModel(prop, it),
-                LfPackage.eINSTANCE.getKeyValuePair(),
-                null,
-                String.format("Target property '%s' is required to be %s.", prop, prop.type));
-          }
+          var test = DynamicTest.dynamicTest(
+              "Property %s (%s) - known bad assignment: %s".formatted(prop, prop.type, it),
+              () -> {
+                if (prop.type instanceof StringDictionaryType) {
+                  validator.assertError(
+                      createModel(prop, it),
+                      LfPackage.eINSTANCE.getKeyValuePair(),
+                      null,
+                      String.format("Target property '%s.", prop),
+                      "' is required to be a string.");
+                } else {
+                  validator.assertError(
+                      createModel(prop, it),
+                      LfPackage.eINSTANCE.getKeyValuePair(),
+                      null,
+                      String.format("Target property '%s' is required to be %s.", prop, prop.type));
+                }
+              }
+          );
+          result.add(test);
         }
       } else {
         // No type was synthesized. It must be a composite type.
         List<List<Object>> list = compositeTypeToKnownBad.get(prop.type);
-        if (list == null) {
-          System.out.printf(
-              "No known incorrect values provided for target property '%s'. Aborting test.%n",
-              prop);
-          Assertions.fail();
-        } else {
+        if (list != null) {
           for (List<Object> it : list) {
-            validator.assertError(
-                createModel(prop, it.get(0).toString()),
-                LfPackage.eINSTANCE.getKeyValuePair(),
-                null,
-                String.format(
-                    "Target property '%s%s' is required to be %s.", prop, it.get(1), it.get(2)));
+            var test = DynamicTest.dynamicTest(
+                "Property %s (%s) - known bad assignment: %s".formatted(prop, prop.type, it),
+                () -> validator.assertError(
+                    createModel(prop, it.get(0).toString()),
+                    LfPackage.eINSTANCE.getKeyValuePair(),
+                    null,
+                    String.format(
+                        "Target property '%s%s' is required to be %s.", prop, it.get(1), it.get(2)))
+            );
+            result.add(test);
           }
         }
       }
-      System.out.println("====");
     }
-    System.out.println("Done!");
+    return result;
   }
 
   @Test
