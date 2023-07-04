55583e15a9f77eb7ea72ac2f4df31bfb145b5cb1
Shaokai Lin <shaokai_lin@126.com>
2023-07-04 18:12:47 +0200

    Prune unused code

--- core/src/main/java/org/lflang/analyses/statespace/StateSpaceNode.java
+++ core/src/main/java/org/lflang/analyses/statespace/StateSpaceNode.java
@@ -1,6 +1,5 @@
 package org.lflang.analyses.statespace;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -26,35 +25,6 @@ public class StateSpaceNode {
     this.time = TimeValue.fromNanoSeconds(tag.timestamp);
   }
 
-  /**
-   * Check if two state space nodes have the same time distance from their respective future events.
-   * Given eventQs from both nodes have the same length, check if the time distances between the two
-   * nodes' tags and the tags of a pair of events are equal, for all pairs of events (one from n1's
-   * eventQ and the other from n2's eventQ),.
-   */
-  private boolean equidistantNodes(StateSpaceNode n1, StateSpaceNode n2) {
-    if (n1.eventQcopy.size() != n2.eventQcopy.size()) return false;
-    for (int i = 0; i < n1.eventQcopy.size(); i++) {
-      if (n1.eventQcopy.get(i).getTag().timestamp - n1.getTag().timestamp
-          != n2.eventQcopy.get(i).getTag().timestamp - n2.getTag().timestamp) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-   * Check if two event queues are analogous, meaning that 1) the two event queues have the same
-   * size, and 2) each pair of events has the same triggers.
-   */
-  private boolean analogousEventQs(ArrayList<Event> q1, ArrayList<Event> q2) {
-    if (q1.size() != q2.size()) return false;
-    for (int i = 0; i < q1.size(); i++) {
-      if (!q1.get(i).hasSameTriggers(q2.get(i))) return false;
-    }
-    return true;
-  }
-
   /** Two methods for pretty printing */
   public void display() {
     System.out.println("(" + this.time + ", " + reactionsInvoked + ", " + eventQcopy + ")");
@@ -65,32 +35,12 @@ public class StateSpaceNode {
   }
 
   /**
-   * This equals method does NOT compare tags, only compares reactionsInvoked, eventQcopy, and
-   * whether future events are equally distant.
-   *
-   * <p>FIXME: Where is this used? This is not used!!!
-   */
-  @Override
-  public boolean equals(Object o) {
-    try {
-      throw new IOException();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    if (o == null) return false;
-    if (o instanceof StateSpaceNode) {
-      StateSpaceNode node = (StateSpaceNode) o;
-      if (this.reactionsInvoked.equals(node.reactionsInvoked)
-          && analogousEventQs(this.eventQcopy, node.eventQcopy)
-          && equidistantNodes(this, node)) return true;
-    }
-    return false;
-  }
-
-  /**
-   * Generate hash for the node. This hash function is used for checking the uniqueness of nodes. It
-   * is not meant to be used as a hashCode() because doing so interferes with node insertion in the
-   * state space diagram.
+   * Generate hash for the node. This hash function is used for checking whether two nodes are
+   * analogous, meaning that 1) they have the same reactions invoked at their tags, 2) the queued
+   * events have the same triggers, and 3) the time offsets between future events' tags of one node
+   * and its tag are the same as the time offsets between future events' tags of the other node and
+   * the other node's tag. The hash() method is not meant to replace the hashCode() method because
+   * doing so changes the way nodes are inserted in the state space diagram.
    */
   public int hash() {
     // Initial value
@@ -107,7 +57,8 @@ public class StateSpaceNode {
             .collect(Collectors.toList());
     result = 31 * result + eventNames.hashCode();
 
-    // Generate hash for the time differences.
+    // Generate hash for a list of time differences between future events' tags and
+    // the current tag.
     List<Long> timeDiff =
         this.eventQcopy.stream()
             .map(
