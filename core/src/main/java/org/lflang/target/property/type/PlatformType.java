2d47aebd4fe15444510d3561338fd0938113b88a
Marten Lohstroh <marten@berkeley.edu>
2023-10-03 23:58:58 -0700

    Custom types for composite target properties

---
+++ core/src/main/java/org/lflang/target/property/type/PlatformType.java
@@ -0,0 +1,56 @@
+package org.lflang.target.property.type;
+
+import org.lflang.target.property.type.PlatformType.Platform;
+
+/** Enumeration of supported platforms */
+public class PlatformType extends OptionsType<Platform> {
+
+  @Override
+  protected Class<Platform> enumClass() {
+    return Platform.class;
+  }
+
+  public enum Platform {
+    AUTO,
+    ARDUINO, // FIXME: not multithreaded
+    NRF52("Nrf52", true),
+    RP2040("Rp2040", false),
+    LINUX("Linux", true),
+    MAC("Darwin", true),
+    ZEPHYR("Zephyr", true),
+    WINDOWS("Windows", true);
+
+    final String cMakeName;
+
+    private final boolean multiThreaded;
+
+    Platform() {
+      this.cMakeName = this.toString();
+      this.multiThreaded = true;
+    }
+
+    Platform(String cMakeName, boolean isMultiThreaded) {
+      this.cMakeName = cMakeName;
+      this.multiThreaded = isMultiThreaded;
+    }
+
+    /** Return the name in lower case. */
+    @Override
+    public String toString() {
+      return this.name().toLowerCase();
+    }
+
+    /** Get the CMake name for the platform. */
+    public String getcMakeName() {
+      return this.cMakeName;
+    }
+
+    public boolean isMultiThreaded() {
+      return this.multiThreaded;
+    }
+
+    public Platform getDefault() {
+      return Platform.AUTO;
+    }
+  }
+}
