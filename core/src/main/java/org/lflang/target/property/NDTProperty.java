27a8d65e058989c73c86e3420520e01ca7b5269d
Byeong-gil Jun <junbg@hanyang.ac.kr>
2023-11-21 18:44:15 +0900

    Make NDTProperty extends BooleanProperty

--- core/src/main/java/org/lflang/target/property/NDTProperty.java
+++ core/src/main/java/org/lflang/target/property/NDTProperty.java
@@ -1,44 +1,25 @@
 package org.lflang.target.property;
 
-import org.lflang.MessageReporter;
-import org.lflang.ast.ASTUtils;
-import org.lflang.lf.Element;
-import org.lflang.target.property.type.LoggingType;
-import org.lflang.target.property.type.LoggingType.LogLevel;
-
 /**
- * FIXME: Add the NDT property.
+ * If true, the RTI will send NDT messages to federates to reduce the number of messages.
+ * The default is false.
  */
-public final class NDTProperty extends TargetProperty<LogLevel, LoggingType> {
+public final class NDTProperty extends BooleanProperty {
 
   /** Singleton target property instance. */
   public static final NDTProperty INSTANCE = new NDTProperty();
 
   private NDTProperty() {
-    super(new LoggingType());
+    super();
   }
 
-  @Override
-  public LogLevel initialValue() {
-    return LogLevel.getDefault();
-  }
-
-  @Override
-  protected LogLevel fromAst(Element node, MessageReporter reporter) {
-    return fromString(ASTUtils.elementToSingleString(node), reporter);
-  }
-
-  protected LogLevel fromString(String string, MessageReporter reporter) {
-    return LogLevel.valueOf(string.toUpperCase());
-  }
-
-  @Override
-  public Element toAstElement(LogLevel value) {
-    return ASTUtils.toElement(value.toString());
+  @Repeatatable
+  public Boolean initialValue() {
+    return false;
   }
 
   @Override
   public String name() {
-    return "disabled";
+    return "ndt";
   }
 }
