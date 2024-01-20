0f6ff2b41c1b102c08c6da179420986d1f8f9f9a
Shaokai Lin <shaokai_lin@126.com>
2024-01-20 10:02:29 -0800

    Unify DAG generation logic for both cyclic and acyclic diagrams. Use the 'associated node' method for both types of diagrams.

--- core/src/main/java/org/lflang/analyses/dag/DagGenerator.java
+++ core/src/main/java/org/lflang/analyses/dag/DagGenerator.java
@@ -15,9 +15,6 @@ import org.lflang.generator.c.CFileConfig;
  * Constructs a Directed Acyclic Graph (DAG) from the State Space Diagram. This is part of the
  * static schedule generation.
  *
- * <p>FIXME: Currently, there is significant code duplication between generateDagForAcyclicDiagram
- * and generateDagForCyclicDiagram. Redundant code needs to be pruned.
- *
  * <p>FIXME: DAG generation does not need to be stateful. The methods in this class can be
  * refactored into static methods.
  *
@@ -44,31 +41,34 @@ public class DagGenerator {
    * can successfully generate DAGs.
    */
   public Dag generateDag(StateSpaceDiagram stateSpaceDiagram) {
-    if (stateSpaceDiagram.isCyclic()) {
-      return generateDagForCyclicDiagram(stateSpaceDiagram);
-    } else {
-      return generateDagForAcyclicDiagram(stateSpaceDiagram);
-    }
-  }
-
-  public Dag generateDagForAcyclicDiagram(StateSpaceDiagram stateSpaceDiagram) {
     // Variables
     Dag dag = new Dag();
     StateSpaceNode currentStateSpaceNode = stateSpaceDiagram.head;
     TimeValue previousTime = TimeValue.ZERO;
     DagNode previousSync = null;
     final TimeValue timeOffset = stateSpaceDiagram.head.getTime();
+    int loopNodeCounter = 0; // Only used when the diagram is cyclic.
+    ArrayList<DagNode> currentReactionNodes = new ArrayList<>();
+    ArrayList<DagNode> reactionsUnconnectedToSync = new ArrayList<>();
+    ArrayList<DagNode> reactionsUnconnectedToNextInvocation = new ArrayList<>();
+    DagNode sync = null; // Local variable for tracking the current SYNC node.
 
     // Check if a DAG can be generated for the given state space diagram.
     // Only a diagram without a loop or a loopy diagram without an
     // initialization phase can generate the DAG.
 
-    ArrayList<DagNode> currentReactionNodes = new ArrayList<>();
-    ArrayList<DagNode> reactionsUnconnectedToSync = new ArrayList<>();
-    ArrayList<DagNode> reactionsUnconnectedToNextInvocation = new ArrayList<>();
-
-    DagNode sync = null; // Local variable for tracking the current SYNC node.
-    while (currentStateSpaceNode != null) {
+    while (true) {
+      // Check stop conditions based on whether the diagram is cyclic or not.
+      if (stateSpaceDiagram.isCyclic()) {
+        // If the current node is the loop node.
+        // The stop condition is when the loop node is encountered the 2nd time.
+        if (currentStateSpaceNode == stateSpaceDiagram.loopNode) {
+          loopNodeCounter++;
+          if (loopNodeCounter >= 2) break;
+        }
+      } else {
+        if (currentStateSpaceNode == null) break;
+      }
 
       // Get the current logical time. Or, if this is the last iteration,
       // set the loop period as the logical time.
@@ -167,174 +167,49 @@ public class DagGenerator {
       previousTime = time;
     }
 
-    // Set the time of the last SYNC node to be the tag of the first pending
-    // event in the tail node of the state space diagram.
-    // Assumption: this assumes that the heap-to-arraylist convertion puts the
-    // earliest event in the first location in arraylist.
     TimeValue time;
-    if (stateSpaceDiagram.phase == Phase.INIT
+    if (stateSpaceDiagram.isCyclic()) {
+      // Set the time of the last SYNC node to be the hyperperiod.
+      time = new TimeValue(stateSpaceDiagram.hyperperiod, TimeUnit.NANO);
+    } else {
+      // Set the time of the last SYNC node to be the tag of the first pending
+      // event in the tail node of the state space diagram.
+      // Assumption: this assumes that the heap-to-arraylist convertion puts the
+      // earliest event in the first location in arraylist.
+      if (stateSpaceDiagram.phase == Phase.INIT
         && stateSpaceDiagram.tail.getEventQcopy().size() > 0) {
-      time =
-          new TimeValue(
-              stateSpaceDiagram.tail.getEventQcopy().get(0).getTag().timestamp, TimeUnit.NANO);
-    }
-    // If there are no pending events, set the time of the last SYNC node to
-    // forever. This is just a convention for building DAGs. In reality, we do
-    // not want to generate any DU instructions when we see the tail node has
-    // TimeValue.MAX_VALUE.
-    else time = TimeValue.MAX_VALUE;
-
-    // Wrap-up procedure
-    wrapup(dag, time, previousSync, previousTime, reactionsUnconnectedToSync);
-
-    return dag;
-  }
-
-  public Dag generateDagForCyclicDiagram(StateSpaceDiagram stateSpaceDiagram) {
-    // Variables
-    Dag dag = new Dag();
-    StateSpaceNode currentStateSpaceNode = stateSpaceDiagram.head;
-    TimeValue previousTime = TimeValue.ZERO;
-    DagNode previousSync = null;
-    int counter = 0;
-    final TimeValue timeOffset = stateSpaceDiagram.head.getTime();
-
-    // Check if a DAG can be generated for the given state space diagram.
-    // Only a diagram without a loop or a loopy diagram without an
-    // initialization phase can generate the DAG.
-
-    ArrayList<DagNode> currentReactionNodes = new ArrayList<>();
-    ArrayList<DagNode> reactionsUnconnectedToSync = new ArrayList<>();
-    ArrayList<DagNode> reactionsUnconnectedToNextInvocation = new ArrayList<>();
-
-    DagNode sync = null; // Local variable for tracking the current SYNC node.
-    while (true) {
-      // If the current node is the loop node.
-      // The stop condition is when the loop node is encountered the 2nd time.
-      if (currentStateSpaceNode == stateSpaceDiagram.loopNode) {
-        counter++;
-        if (counter >= 2) break;
+        time =
+            new TimeValue(
+                stateSpaceDiagram.tail.getEventQcopy().get(0).getTag().timestamp, TimeUnit.NANO);
       }
-
-      // Get the current logical time. Or, if this is the last iteration,
-      // set the loop period as the logical time.
-      TimeValue time = currentStateSpaceNode.getTime().sub(timeOffset);
-
-      // Add a SYNC node.
-      sync = dag.addNode(DagNode.dagNodeType.SYNC, time);
-      if (dag.head == null) dag.head = sync;
-
-      // Create DUMMY and Connect SYNC and previous SYNC to DUMMY
-      if (!time.equals(TimeValue.ZERO)) {
-        TimeValue timeDiff = time.sub(previousTime);
-        DagNode dummy = dag.addNode(DagNode.dagNodeType.DUMMY, timeDiff);
-        dag.addEdge(previousSync, dummy);
-        dag.addEdge(dummy, sync);
-      }
-
-      // Add reaction nodes, as well as the edges connecting them to SYNC.
-      currentReactionNodes.clear();
-      for (ReactionInstance reaction : currentStateSpaceNode.getReactionsInvoked()) {
-        DagNode node = dag.addNode(DagNode.dagNodeType.REACTION, reaction);
-        currentReactionNodes.add(node);
-        dag.addEdge(sync, node);
-      }
-
-      // Now add edges based on reaction dependencies.
-      for (DagNode n1 : currentReactionNodes) {
-        for (DagNode n2 : currentReactionNodes) {
-          if (n1.nodeReaction.dependentReactions().contains(n2.nodeReaction)) {
-            dag.addEdge(n1, n2);
-          }
-        }
-      }
-
-      // Create a list of ReactionInstances from currentReactionNodes.
-      ArrayList<ReactionInstance> currentReactions =
-          currentReactionNodes.stream()
-              .map(DagNode::getReaction)
-              .collect(Collectors.toCollection(ArrayList::new));
-
-      // If there is a newly released reaction found and its prior
-      // invocation is not connected to a downstream SYNC node,
-      // connect it to a downstream SYNC node to
-      // preserve a deterministic order. In other words,
-      // check if there are invocations of the same reaction across two
-      // time steps, if so, connect the previous invocation to the current
-      // SYNC node.
-      //
-      // FIXME: This assumes that the (conventional) deadline is the
-      // period. We need to find a way to integrate LF deadlines into
-      // the picture.
-      ArrayList<DagNode> toRemove = new ArrayList<>();
-      for (DagNode n : reactionsUnconnectedToSync) {
-        if (currentReactions.contains(n.nodeReaction)) {
-          dag.addEdge(n, sync);
-          toRemove.add(n);
-        }
-      }
-      reactionsUnconnectedToSync.removeAll(toRemove);
-      reactionsUnconnectedToSync.addAll(currentReactionNodes);
-
-      // Check if there are invocations of reactions from the same reactor
-      // across two time steps. If so, connect invocations from the
-      // previous time step to those in the current time step, in order to
-      // preserve determinism.
-      ArrayList<DagNode> toRemove2 = new ArrayList<>();
-      for (DagNode n1 : reactionsUnconnectedToNextInvocation) {
-        for (DagNode n2 : currentReactionNodes) {
-          ReactorInstance r1 = n1.getReaction().getParent();
-          ReactorInstance r2 = n2.getReaction().getParent();
-          if (r1.equals(r2)) {
-            dag.addEdge(n1, n2);
-            toRemove2.add(n1);
-          }
-        }
-      }
-      reactionsUnconnectedToNextInvocation.removeAll(toRemove2);
-      reactionsUnconnectedToNextInvocation.addAll(currentReactionNodes);
-
-      // Move to the next state space node.
-      currentStateSpaceNode = stateSpaceDiagram.getDownstreamNode(currentStateSpaceNode);
-      previousSync = sync;
-      previousTime = time;
+      // If there are no pending events, set the time of the last SYNC node to
+      // forever. This is just a convention for building DAGs. In reality, we do
+      // not want to generate any DU instructions when we see the tail node has
+      // TimeValue.MAX_VALUE.
+      else time = TimeValue.MAX_VALUE;
     }
 
-    // Set the time of the last SYNC node to be the hyperperiod.
-    TimeValue time = new TimeValue(stateSpaceDiagram.hyperperiod, TimeUnit.NANO);
-
-    // Wrap-up procedure
-    wrapup(dag, time, previousSync, previousTime, reactionsUnconnectedToSync);
-
-    return dag;
-  }
-
-  /** A wrap-up procedure */
-  private void wrapup(
-      Dag dag,
-      TimeValue time,
-      DagNode previousSync,
-      TimeValue previousTime,
-      ArrayList<DagNode> reactionsUnconnectedToSync) {
     // Add a SYNC node.
-    DagNode sync = dag.addNode(DagNode.dagNodeType.SYNC, time);
-    if (dag.head == null) dag.head = sync;
+    DagNode lastSync = dag.addNode(DagNode.dagNodeType.SYNC, time);
+    if (dag.head == null) dag.head = lastSync;
 
     // Create DUMMY and Connect SYNC and previous SYNC to DUMMY
     if (!time.equals(TimeValue.ZERO)) {
       TimeValue timeDiff = time.sub(previousTime);
       DagNode dummy = dag.addNode(DagNode.dagNodeType.DUMMY, timeDiff);
       dag.addEdge(previousSync, dummy);
-      dag.addEdge(dummy, sync);
+      dag.addEdge(dummy, lastSync);
     }
 
     // Add edges from existing reactions to the last node,
     // and break the loop before adding more reaction nodes.
     for (DagNode n : reactionsUnconnectedToSync) {
-      dag.addEdge(n, sync);
+      dag.addEdge(n, lastSync);
     }
 
     // After exiting the while loop, assign the last SYNC node as tail.
-    dag.tail = sync;
+    dag.tail = lastSync;
+
+    return dag;
   }
 }
