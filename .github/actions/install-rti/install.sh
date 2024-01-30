185a801c730f2b24f4febc9d4a3e4589f725caf0
Peter Donovan <peterdonovan@berkeley.edu>
2024-01-29 20:38:03 -0800

    Fix install script.

--- .github/actions/install-rti/install.sh
+++ .github/actions/install-rti/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd core/src/main/resources/lib/c/reactor-c/core/federated/RTI
+cd ../../../plugin-defaults/trace/ && ./build.sh && cd -
 mkdir build
 cd build
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
 fi
-cd ../../../plugin-defaults/trace/ && ./build.sh && cd -
 cmake -DAUTH=ON ../
 make
 sudo make install
