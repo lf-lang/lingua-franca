71cb6e644b237a223963282b85d24a86d0239564
Peter Donovan <peter@xronos.com>
2024-02-26 00:24:57 -0800

    Add trace-plugin target property.

---
+++ core/src/main/java/org/lflang/target/property/TracePluginProperty.java
@@ -0,0 +1,54 @@
+package org.lflang.target.property;
+
+import org.lflang.MessageReporter;
+import org.lflang.lf.Element;
+import org.lflang.target.property.TracePluginProperty.TracePluginOptions;
+import org.lflang.target.property.type.PrimitiveType;
+
+/**
+ * Property that provides an alternative tracing implementation.
+ */
+public class TracePluginProperty extends TargetProperty<TracePluginOptions, PrimitiveType> {
+
+    /** Singleton target property instance. */
+    public static final TracePluginProperty INSTANCE = new TracePluginProperty();
+
+    private TracePluginProperty() {
+        super(PrimitiveType.STRING);
+    }
+
+    @Override
+    public TracePluginOptions initialValue() {
+        return null;
+    }
+
+    @Override
+    protected TracePluginOptions fromAst(Element node, MessageReporter reporter) {
+        throw new IllegalArgumentException("There is no representation of this property in the LF target property syntax");
+    }
+
+    @Override
+    protected TracePluginOptions fromString(String s, MessageReporter reporter) {
+        return new TracePluginOptions(s);
+    }
+
+    @Override
+    public String name() {
+        return "trace-plugin";
+    }
+
+    @Override
+    public Element toAstElement(TracePluginOptions value) {
+        throw new IllegalArgumentException("There is no representation of this property in the LF target property syntax");
+    }
+
+    public static class TracePluginOptions {
+        private final String implementationArchiveFile;
+        public TracePluginOptions(String implementationArchiveFile) {
+            this.implementationArchiveFile = implementationArchiveFile;
+        }
+        public String getImplementationArchiveFile() {
+            return implementationArchiveFile;
+        }
+    }
+}
