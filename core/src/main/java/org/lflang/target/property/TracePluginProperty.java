75428c2739f96800d49c6c7b7f765bf607c33fdc
erlingrj <erling.jellum@gmail.com>
2024-05-10 16:31:37 +0200

    Have TracePluginProperty simple inherit from StringProperty

--- core/src/main/java/org/lflang/target/property/TracePluginProperty.java
+++ core/src/main/java/org/lflang/target/property/TracePluginProperty.java
@@ -4,64 +4,22 @@ import org.lflang.MessageReporter;
 import org.lflang.ast.ASTUtils;
 import org.lflang.lf.Element;
 import org.lflang.target.TargetConfig;
-import org.lflang.target.property.TracePluginProperty.TracePluginOptions;
+import org.lflang.target.property.type.LoggingType.LogLevel;
 import org.lflang.target.property.type.PrimitiveType;
 
 /** Property that provides an alternative tracing implementation. */
-public class TracePluginProperty extends TargetProperty<TracePluginOptions, PrimitiveType> {
+/** The compiler to invoke, unless a build command has been specified. */
+public final class TracePluginProperty extends StringProperty {
 
   /** Singleton target property instance. */
   public static final TracePluginProperty INSTANCE = new TracePluginProperty();
 
   private TracePluginProperty() {
-    super(PrimitiveType.STRING);
-  }
-
-  @Override
-  public TracePluginOptions initialValue() {
-    return null;
-  }
-
-  @Override
-  protected TracePluginOptions fromAst(Element node, MessageReporter reporter) {
-    var s = ASTUtils.elementToSingleString(node);
-    if (s != null) {
-      return new TracePluginOptions(s);
-    } else {
-      reporter.at(node).error("Expected a string literal");
-      return null;
-    }
-  }
-
-  @Override
-  protected TracePluginOptions fromString(String s, MessageReporter reporter) {
-    return new TracePluginOptions(s);
+    super();
   }
 
   @Override
   public String name() {
     return "trace-plugin";
-  }
-
-  @Override
-  public Element toAstElement(TracePluginOptions value) {
-    return ASTUtils.toElement(value.implementationArchiveFile);
-  }
-
-  public static class TracePluginOptions {
-    private final String implementationArchiveFile;
-
-    public TracePluginOptions(String implementationArchiveFile) {
-      this.implementationArchiveFile = implementationArchiveFile;
-    }
-
-    public String getImplementationArchiveFile() {
-      return implementationArchiveFile;
-    }
-
-    @Override
-    public String toString() {
-      return getImplementationArchiveFile();
-    }
   }
 }
