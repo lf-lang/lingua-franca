3ebdc170d91df6d6e321ff8a91073ca674c0db60
erlingrj <erling.jellum@gmail.com>
2024-05-15 15:22:10 +0200

    Add integer value to the ClockSyncMode enum which matches reactor-c enum

--- core/src/main/java/org/lflang/target/property/type/ClockSyncModeType.java
+++ core/src/main/java/org/lflang/target/property/type/ClockSyncModeType.java
@@ -17,17 +17,25 @@ public class ClockSyncModeType extends OptionsType<ClockSyncMode> {
    *   <li>STARTUP: Clock synchronization occurs at startup only.
    *   <li>ON: Clock synchronization occurs at startup and at runtime.
    * </ul>
-   *
+   * The values associated with the enum must match what is defined in
+   * clock_sync.h in reactor-c
    * @author Edward A. Lee
    */
   public enum ClockSyncMode {
-    OFF,
-    INIT,
-    ON;
+    OFF(1),
+    INIT(2),
+    ON(3);
+    private final int value;
+    private ClockSyncMode(int value) {
+      this.value = value;
+    }
     /** Return the name in lower case. */
     @Override
     public String toString() {
       return this.name().toLowerCase();
+    }
+    public int toInt() {
+      return this.value;
     }
   }
 }
