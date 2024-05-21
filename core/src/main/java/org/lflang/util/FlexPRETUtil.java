59545e4af34731f1c8d9ec5b7ac3eb701132171c
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-05-21 12:21:16 +0200

    Remove unecessary exceptions in FlexPRETUtil.java

--- core/src/main/java/org/lflang/util/FlexPRETUtil.java
+++ core/src/main/java/org/lflang/util/FlexPRETUtil.java
@@ -1,8 +1,6 @@
 package org.lflang.util;
 
-import java.io.IOException;
 import java.util.Collections;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.lflang.FileConfig;
 import org.lflang.MessageReporter;
 import org.lflang.generator.GeneratorCommandFactory;
@@ -32,50 +30,46 @@ public class FlexPRETUtil {
     var cmd = binPath + "/" + exeName;
     var args = Collections.<String>emptyList();
 
-    try {
-      LFCommand flash = commandFactory.createCommand(cmd, args);
+    LFCommand flash = commandFactory.createCommand(cmd, args);
 
-      /**
-       * Check if environment variable FP_SDK_FPGA_INTERFACE_PROGRAM is set. If it is set, we should
-       * let the user know it is not supported by LF.
-       *
-       * <p>We cannot support it because FP_SDK_FPGA_INTERFACE_PROGRAM will lead to a [Y/n] prompt,
-       * and the LFCommands do not have a way of getting the prompt response.
-       *
-       * <p>When FP_SDK_FPGA_INTERFACE_PROGRAM is not set, there is no prompt.
-       */
-      var envInterfaceProgram = System.getenv("FP_SDK_FPGA_INTERFACE_PROGRAM");
+    /**
+     * Check if environment variable FP_SDK_FPGA_INTERFACE_PROGRAM is set. If it is set, we should
+     * let the user know it is not supported by LF.
+     *
+     * <p>We cannot support it because FP_SDK_FPGA_INTERFACE_PROGRAM will lead to a [Y/n] prompt,
+     * and the LFCommands do not have a way of getting the prompt response.
+     *
+     * <p>When FP_SDK_FPGA_INTERFACE_PROGRAM is not set, there is no prompt.
+     */
+    var envInterfaceProgram = System.getenv("FP_SDK_FPGA_INTERFACE_PROGRAM");
 
-      /**
-       * FP_SDK_FPGA_INTERFACE_PROGRAM is only relevant when the user has specified board = fpga and
-       * flash = true. So only print the info message if that is the case.
-       */
-      var platform = config.get(PlatformProperty.INSTANCE);
-      var interfaceProgramRelevant =
-          platform.board().value() != null
-              && platform.board().value().equals("fpga")
-              && platform.flash().value() == true;
+    /**
+     * FP_SDK_FPGA_INTERFACE_PROGRAM is only relevant when the user has specified board = fpga and
+     * flash = true. So only print the info message if that is the case.
+     */
+    var platform = config.get(PlatformProperty.INSTANCE);
+    var interfaceProgramRelevant =
+        platform.board().value() != null
+            && platform.board().value().equals("fpga")
+            && platform.flash().value() == true;
 
-      if (interfaceProgramRelevant && envInterfaceProgram != null) {
+    if (interfaceProgramRelevant && envInterfaceProgram != null) {
+      messageReporter
+          .nowhere()
+          .info("FP_SDK_FPGA_INTERFACE_PROGRAM not supported by LF. It will have no effect.");
+    }
+
+    if (flash != null) {
+      /** Set the variable to nothing for the flash command, which will avoid the [Y/n] prompt */
+      flash.setEnvironmentVariable("FP_SDK_FPGA_INTERFACE_PROGRAM", "");
+
+      int ret = flash.run(context.getCancelIndicator());
+      if (ret != 0) {
         messageReporter
             .nowhere()
-            .info("FP_SDK_FPGA_INTERFACE_PROGRAM not supported by LF. It will have no effect.");
+            .error(
+                "Command " + cmd + " failed with exit code: " + ret + ". Is the target connected?");
       }
-
-      if (flash != null) {
-        /** Set the variable to nothing for the flash command, which will avoid the [Y/n] prompt */
-        flash.setEnvironmentVariable("FP_SDK_FPGA_INTERFACE_PROGRAM", "");
-
-        int ret = flash.run(context.getCancelIndicator());
-        if (ret != 0) {
-          messageReporter.nowhere().error("Command " + cmd + " failed with exit code: " + ret);
-          throw new IOException("Could not flash");
-        }
-      }
-    } catch (IOException e) {
-      Exceptions.sneakyThrow(e);
-    } catch (NullPointerException e) {
-      Exceptions.sneakyThrow(e);
     }
   }
 }
