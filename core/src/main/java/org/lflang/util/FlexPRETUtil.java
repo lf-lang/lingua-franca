55972052d020f3747c4e9616cfb2c792e4c5d922
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-05-22 00:05:30 +0200

    Remove use of FP_SDK_FPGA_INTERFACE_PROGRAM

--- core/src/main/java/org/lflang/util/FlexPRETUtil.java
+++ core/src/main/java/org/lflang/util/FlexPRETUtil.java
@@ -6,7 +6,6 @@ import org.lflang.MessageReporter;
 import org.lflang.generator.GeneratorCommandFactory;
 import org.lflang.generator.LFGeneratorContext;
 import org.lflang.target.TargetConfig;
-import org.lflang.target.property.PlatformProperty;
 
 public class FlexPRETUtil {
   private LFGeneratorContext context;
@@ -32,37 +31,7 @@ public class FlexPRETUtil {
 
     LFCommand flash = commandFactory.createCommand(cmd, args);
 
-    /**
-     * Check if environment variable FP_SDK_FPGA_INTERFACE_PROGRAM is set. If it is set, we should
-     * let the user know it is not supported by LF.
-     *
-     * <p>We cannot support it because FP_SDK_FPGA_INTERFACE_PROGRAM will lead to a [Y/n] prompt,
-     * and the LFCommands do not have a way of getting the prompt response.
-     *
-     * <p>When FP_SDK_FPGA_INTERFACE_PROGRAM is not set, there is no prompt.
-     */
-    var envInterfaceProgram = System.getenv("FP_SDK_FPGA_INTERFACE_PROGRAM");
-
-    /**
-     * FP_SDK_FPGA_INTERFACE_PROGRAM is only relevant when the user has specified board = fpga and
-     * flash = true. So only print the info message if that is the case.
-     */
-    var platform = config.get(PlatformProperty.INSTANCE);
-    var interfaceProgramRelevant =
-        platform.board().value() != null
-            && platform.board().value().equals("fpga")
-            && platform.flash().value() == true;
-
-    if (interfaceProgramRelevant && envInterfaceProgram != null) {
-      messageReporter
-          .nowhere()
-          .info("FP_SDK_FPGA_INTERFACE_PROGRAM not supported by LF. It will have no effect.");
-    }
-
     if (flash != null) {
-      /** Set the variable to nothing for the flash command, which will avoid the [Y/n] prompt */
-      flash.setEnvironmentVariable("FP_SDK_FPGA_INTERFACE_PROGRAM", "");
-
       int ret = flash.run(context.getCancelIndicator());
       if (ret != 0) {
         messageReporter
