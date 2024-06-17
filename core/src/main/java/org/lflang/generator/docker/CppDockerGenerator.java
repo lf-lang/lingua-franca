b58903ffe5d8353d72f66906a12ecbdbd73a2d7c
Marten Lohstroh <marten@berkeley.edu>
2024-06-16 18:08:44 -0700

    Added FIXME

--- core/src/main/java/org/lflang/generator/docker/CppDockerGenerator.java
+++ core/src/main/java/org/lflang/generator/docker/CppDockerGenerator.java
@@ -1,15 +1,8 @@
 package org.lflang.generator.docker;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
-
 import org.lflang.generator.LFGeneratorContext;
-import org.lflang.generator.c.CCompiler;
-import org.lflang.generator.c.CFileConfig;
-import org.lflang.generator.cpp.CppGenerator;
 import org.lflang.generator.cpp.CppPlatformGenerator;
-import org.lflang.target.Target;
 
 /**
  * Generates the docker file related code for the Typescript target.
@@ -28,21 +21,7 @@ public class CppDockerGenerator extends CDockerGenerator {
 
   @Override
   protected List<String> defaultBuildCommands() {
-    try {
-      return List.of(
-          "mkdir -p bin",
-          String.format(
-              "%s -DCMAKE_INSTALL_BINDIR=./bin -S src-gen -B bin", platformGenerator),
-          "cd bin",
-          "make all",
-          "cd ..");
-    } catch (IOException e) {
-      context
-          .getErrorReporter()
-          .nowhere()
-          .error("Unable to create file configuration for Docker container");
-      throw new RuntimeException(e);
-    }
+    // FIXME
+    return List.of();
   }
-
 }
