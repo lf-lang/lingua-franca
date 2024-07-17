8390155a96efe0418ab856f2e65d67ba0843f45d
Chadlia Jerad <chadlia.jerad@gmail.com>
2024-07-17 12:22:23 +0100

    Fix code printing of eroDelayCycleNetworkUpstreamFeds

--- core/src/main/java/org/lflang/federated/extensions/CExtensionUtils.java
+++ core/src/main/java/org/lflang/federated/extensions/CExtensionUtils.java
@@ -77,13 +77,15 @@ public class CExtensionUtils {
         code.pr(
             trigger + ".source_id = " + federate.networkMessageSourceFederate.get(i).id + "; \\");
         if (federate.zeroDelayCycleNetworkMessageActions.contains(action)) {
-          var upstream = federate.zeroDelayCycleNetworkUpstreamFeds.get(i);
-          code.pr(
-              "_lf_zero_delay_cycle_upstream_ids["
-                  + zeroDelayActionTableCount
-                  + "] = "
-                  + upstream.id
-                  + "; \\");
+          for (int j = 0; j < federate.zeroDelayCycleNetworkUpstreamFeds.size(); ++j) {
+            var upstream = federate.zeroDelayCycleNetworkUpstreamFeds.get(j);
+            code.pr(
+                "_lf_zero_delay_cycle_upstream_ids["
+                    + zeroDelayActionTableCount
+                    + "] = "
+                    + upstream.id
+                    + "; \\");
+          }
           if (federate.isTransient) {
             // Transient federates are assumed to be initially disconnected.
             code.pr(
