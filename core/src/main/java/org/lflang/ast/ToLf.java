c25b2f3d7ed5cdc40cb924f0ad73a33de9fb819e
vinzbarbuto <vinz.barbuto@gmail.com>
2024-09-10 13:50:23 -0700

    Fix bug in ToLf.java; Added test for new import statement

--- core/src/main/java/org/lflang/ast/ToLf.java
+++ core/src/main/java/org/lflang/ast/ToLf.java
@@ -397,10 +397,8 @@ public class ToLf extends LfSwitch<MalleableString> {
         .append("import ")
         // TODO: This is a place where we can use conditional parentheses.
         .append(list(", ", "", "", false, true, true, object.getReactorClasses()))
-        .append(" from \"")
-        // TODO: Check if angular brackets (< >) are required
-        .append(object.getImportURI() != null ? object.getImportURI() : object.getImportPackage())
-        .append("\"")
+        .append(" from ")
+        .append(object.getImportURI() != null ? "\"" + object.getImportURI() + "\"" : object.getImportPackage())
         .get();
   }
 
