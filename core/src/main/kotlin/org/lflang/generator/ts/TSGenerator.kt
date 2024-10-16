54612a25dcf3736abad421fe5ca0e5663a504ba8
Shulu Li <65802727+Depetrol@users.noreply.github.com>
2024-10-16 14:41:10 -0700

    Remove unused import
    Co-authored-by: Marten Lohstroh <marten@berkeley.edu>

--- core/src/main/kotlin/org/lflang/generator/ts/TSGenerator.kt
+++ core/src/main/kotlin/org/lflang/generator/ts/TSGenerator.kt
@@ -47,7 +47,6 @@ import org.lflang.util.FileUtil
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
-import kotlin.io.path.extension
 
 private const val NO_NPM_MESSAGE = "The TypeScript target requires npm >= 6.14.4. " +
         "For installation instructions, see: https://www.npmjs.com/get-npm. \n" +
