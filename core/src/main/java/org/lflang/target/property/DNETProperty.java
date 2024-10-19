32bbd70118c426854fdcd2eb9516b95f2819ecc4
Byeonggil Jun <byeonggil@asu.edu>
2024-10-19 16:03:21 -0700

    Apply spotless

--- core/src/main/java/org/lflang/target/property/DNETProperty.java
+++ core/src/main/java/org/lflang/target/property/DNETProperty.java
@@ -2,10 +2,9 @@ package org.lflang.target.property;
 
 /**
  * @brief Target property turning on or off the DNET signal optimization.
- * 
- * If this target property is true, the RTI sends DNET (downstream next event tag) signals to an upstream federate
- * to tell the federate that sending LTC and NET signals with tags less than the specified value is unnecessary.
- * The default is true.
+ *     <p>If this target property is true, the RTI sends DNET (downstream next event tag) signals to
+ *     an upstream federate to tell the federate that sending LTC and NET signals with tags less
+ *     than the specified value is unnecessary. The default is true.
  */
 public final class DNETProperty extends BooleanProperty {
 
