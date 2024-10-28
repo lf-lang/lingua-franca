c1d483718c3376c7f9c34c28e2052a27d5df550c
Omer Majeed <omer.majeed@magnition.io>
2024-10-29 01:18:44 +0500

    fqn function exposed to LFScope

--- core/src/main/resources/lib/cpp/lfutil.hh
+++ core/src/main/resources/lib/cpp/lfutil.hh
@@ -60,6 +60,7 @@ public:
   reactor::Duration get_elapsed_logical_time() const { return reactor->get_elapsed_logical_time(); }
   reactor::Duration get_elapsed_physical_time() const { return reactor->get_elapsed_physical_time(); }
   reactor::Environment* environment() const { return reactor->environment(); }
+  const std::string& fqn() const { return reactor->fqn(); }
   void request_stop() const { return environment()->sync_shutdown(); }
 };
 
