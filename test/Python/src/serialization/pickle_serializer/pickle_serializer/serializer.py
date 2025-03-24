6471a8d134531f6d728cd6d44085b508eb324826
MoezBHH <moez.benhajhmida@gmail.com>
2025-03-24 16:54:26 +0100

    clean up

--- test/Python/src/serialization/pickle_serializer/pickle_serializer/serializer.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-class Serializer():
-    def serialize(self, obj)->bytes:
-        return pickle.dumps(obj)
-    def deserialize(self, message:bytes):
-        return pickle.loads(message)
