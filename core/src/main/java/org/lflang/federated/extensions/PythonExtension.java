a7f272507cd3f1a1d5009d921b1e0fcf5df2c7a0
Edward A. Lee <eal@berkeley.edu>
2026-03-13 00:37:05 -0700

    Use lf_py_get_parameter_as_long to set sender_index

--- core/src/main/java/org/lflang/federated/extensions/PythonExtension.java
+++ core/src/main/java/org/lflang/federated/extensions/PythonExtension.java
@@ -79,7 +79,14 @@ public class PythonExtension extends CExtension {
            extern reaction_t* port_absent_reaction[];
            void lf_enqueue_port_absent_reactions(environment_t*);
            LF_PRINT_DEBUG("Adding network port absent reaction to table.");
-           port_absent_reaction[self->sender_index] = &self->_lf__reaction_2;
+           static long _lf_sender_idx = -1;
+           if (_lf_sender_idx < 0) {
+             _lf_sender_idx = lf_py_get_parameter_as_long("__main__", self->_lf_name, 0, "sender_index");
+             if (_lf_sender_idx < 0) {
+               lf_print_error_and_exit("Failed to read sender_index parameter for network sender.");
+             }
+           }
+           port_absent_reaction[_lf_sender_idx] = &self->_lf__reaction_2;
            LF_PRINT_DEBUG("Added network output control reaction to table. Enqueueing it...");
            lf_enqueue_port_absent_reactions(self->base.environment);
            """;
@@ -88,10 +95,7 @@ public class PythonExtension extends CExtension {
   @Override
   public void addSenderIndexParameter(Reactor sender) {
     var senderIndexParameter = LfFactory.eINSTANCE.createParameter();
-    var senderIndexParameterType = LfFactory.eINSTANCE.createType();
     senderIndexParameter.setName("sender_index");
-    senderIndexParameterType.setId("int");
-    senderIndexParameter.setType(senderIndexParameterType);
     var senderIndexParameterInit = LfFactory.eINSTANCE.createInitializer();
     var senderIndexParameterInitExpr = LfFactory.eINSTANCE.createLiteral();
     senderIndexParameterInitExpr.setLiteral("0");
