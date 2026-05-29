53b91abc533e7563c79176718e2b2326860cec13
Edward A. Lee <eal@berkeley.edu>
2026-05-28 17:53:27 -0700

    Spotless and align reactor-c

--- core/src/main/java/org/lflang/generator/python/PythonGenerator.java
+++ core/src/main/java/org/lflang/generator/python/PythonGenerator.java
@@ -179,7 +179,8 @@ public class PythonGenerator extends CGenerator implements CCmakeGenerator.SetUp
         "# so that these names will be assumed present without having to compile and install.",
         "# pylint: disable=no-name-in-module, import-error",
         "from " + pyModuleName + " import (",
-        "    Tag, action_capsule_t, port_capsule, get_fed_maxwait, request_stop, set_fed_maxwait, start",
+        "    Tag, action_capsule_t, port_capsule, get_fed_maxwait, request_stop, set_fed_maxwait,"
+            + " start",
         ")",
         "# pylint: disable=c-extension-no-member",
         "import " + pyModuleName + " as lf",
