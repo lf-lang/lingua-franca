4104e6b04e28210ffed76c63627cef8b26878ba5
Hokeun Kim <hokeunkim@berkeley.edu>
2026-05-28 19:44:47 -0700

    Run `./gradlew :core:spotlessApply`

--- core/src/main/java/org/lflang/federated/serialization/FedProtoPythonSerialization.java
+++ core/src/main/java/org/lflang/federated/serialization/FedProtoPythonSerialization.java
@@ -87,8 +87,7 @@ public class FedProtoPythonSerialization implements FedSerialization {
         "PyObject* _lf_descriptor = PyObject_GetAttrString(" + varName + ", \"DESCRIPTOR\");\n");
     code.append("if (_lf_descriptor == NULL) {\n");
     code.append("    if (PyErr_Occurred()) PyErr_Print();\n");
-    code.append(
-        "    lf_print_error_and_exit(\"Failed to get DESCRIPTOR from proto object.\");\n");
+    code.append("    lf_print_error_and_exit(\"Failed to get DESCRIPTOR from proto object.\");\n");
     code.append("}\n");
     code.append(
         "PyObject* _lf_full_name_obj = PyObject_GetAttrString(_lf_descriptor, \"full_name\");\n");
@@ -100,16 +99,8 @@ public class FedProtoPythonSerialization implements FedSerialization {
     code.append("const char* _lf_full_name = PyUnicode_AsUTF8(_lf_full_name_obj);\n");
     code.append("size_t _lf_name_len = strlen(_lf_full_name);\n");
     // Allocate and fill the wire buffer.
-    code.append(
-        "size_t "
-            + LEN_VAR
-            + " = 4 + _lf_name_len + (size_t)_lf_proto_raw_len;\n");
-    code.append(
-        "unsigned char* "
-            + BUF_VAR
-            + " = (unsigned char*)malloc("
-            + LEN_VAR
-            + ");\n");
+    code.append("size_t " + LEN_VAR + " = 4 + _lf_name_len + (size_t)_lf_proto_raw_len;\n");
+    code.append("unsigned char* " + BUF_VAR + " = (unsigned char*)malloc(" + LEN_VAR + ");\n");
     code.append("if (" + BUF_VAR + " == NULL) {\n");
     code.append(
         "    lf_print_error_and_exit(\"Failed to allocate buffer for proto serialization.\");\n");
@@ -120,9 +111,7 @@ public class FedProtoPythonSerialization implements FedSerialization {
     code.append(BUF_VAR + "[3] = (unsigned char)(_lf_name_len & 0xFF);\n");
     code.append("memcpy(" + BUF_VAR + " + 4, _lf_full_name, _lf_name_len);\n");
     code.append(
-        "memcpy("
-            + BUF_VAR
-            + " + 4 + _lf_name_len, _lf_proto_raw, (size_t)_lf_proto_raw_len);\n");
+        "memcpy(" + BUF_VAR + " + 4 + _lf_name_len, _lf_proto_raw, (size_t)_lf_proto_raw_len);\n");
     code.append("Py_XDECREF(_lf_full_name_obj);\n");
     code.append("Py_XDECREF(_lf_proto_bytes);\n");
     return code;
@@ -142,11 +131,8 @@ public class FedProtoPythonSerialization implements FedSerialization {
     StringBuilder code = new StringBuilder();
     // Read wire format.
     code.append(
-        "const unsigned char* _lf_wire = (const unsigned char*)"
-            + varName
-            + "->token->value;\n");
-    code.append(
-        "size_t _lf_wire_len = (size_t)" + varName + "->token->length;\n");
+        "const unsigned char* _lf_wire = (const unsigned char*)" + varName + "->token->value;\n");
+    code.append("size_t _lf_wire_len = (size_t)" + varName + "->token->length;\n");
     code.append("if (_lf_wire_len < 4) {\n");
     code.append("    lf_print_error_and_exit(\"Proto wire format too short.\");\n");
     code.append("}\n");
@@ -157,10 +143,8 @@ public class FedProtoPythonSerialization implements FedSerialization {
     code.append(
         "    lf_print_error_and_exit(\"Proto wire format: name length exceeds buffer.\");\n");
     code.append("}\n");
-    code.append(
-        "const unsigned char* _lf_proto_data = _lf_wire + 4 + _lf_full_name_len;\n");
-    code.append(
-        "size_t _lf_proto_data_len = _lf_wire_len - 4 - _lf_full_name_len;\n");
+    code.append("const unsigned char* _lf_proto_data = _lf_wire + 4 + _lf_full_name_len;\n");
+    code.append("size_t _lf_proto_data_len = _lf_wire_len - 4 - _lf_full_name_len;\n");
     // Look up the proto class using Python's symbol database.
     code.append(
         "PyObject* _lf_sym_db_mod ="
@@ -171,8 +155,7 @@ public class FedProtoPythonSerialization implements FedSerialization {
         "    lf_print_error_and_exit(\"Failed to import"
             + " google.protobuf.symbol_database.\");\n");
     code.append("}\n");
-    code.append(
-        "PyObject* _lf_sym_db = PyObject_CallMethod(_lf_sym_db_mod, \"Default\", NULL);\n");
+    code.append("PyObject* _lf_sym_db = PyObject_CallMethod(_lf_sym_db_mod, \"Default\", NULL);\n");
     code.append("Py_XDECREF(_lf_sym_db_mod);\n");
     code.append("if (_lf_sym_db == NULL) {\n");
     code.append("    if (PyErr_Occurred()) PyErr_Print();\n");
@@ -208,8 +191,7 @@ public class FedProtoPythonSerialization implements FedSerialization {
     code.append("Py_XDECREF(_lf_proto_bytes_obj);\n");
     code.append("if (_lf_parse_result == NULL) {\n");
     code.append("    if (PyErr_Occurred()) PyErr_Print();\n");
-    code.append(
-        "    lf_print_error_and_exit(\"Failed to parse proto message from bytes.\");\n");
+    code.append("    lf_print_error_and_exit(\"Failed to parse proto message from bytes.\");\n");
     code.append("}\n");
     code.append("Py_XDECREF(_lf_parse_result);\n");
     return code;
