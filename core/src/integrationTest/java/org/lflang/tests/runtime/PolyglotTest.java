a24895747a867f9b0f1c15d641ce1ae7c56f5a1c
Hokeun Kim <hokeunkim@berkeley.edu>
2026-05-29 11:05:58 -0700

    Add CI tests for Polyglot target.

---
+++ core/src/integrationTest/java/org/lflang/tests/runtime/PolyglotTest.java
@@ -0,0 +1,47 @@
+package org.lflang.tests.runtime;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.lflang.target.Target;
+import org.lflang.tests.RuntimeTest;
+
+/**
+ * Collection of tests for the Polyglot target.
+ *
+ * <p>Even though all tests are implemented in the base class, we override them here so that each
+ * test can be easily invoked individually from IDEs with JUnit support like Eclipse and IntelliJ.
+ * This is typically done by right-clicking on the name of the test method and then clicking "Run".
+ *
+ * @author Hokeun Kim
+ * @ingroup Tests
+ */
+public class PolyglotTest extends RuntimeTest {
+
+  public PolyglotTest() {
+    super(Target.Polyglot);
+  }
+
+  @Override
+  protected boolean supportsFederatedExecution() {
+    return true;
+  }
+
+  @Test
+  @Override
+  public void runBasicTests() {
+    super.runBasicTests();
+  }
+
+  @Test
+  @Override
+  public void runTargetSpecificTests() {
+    super.runTargetSpecificTests();
+  }
+
+  @Test
+  @Override
+  public void runFederatedTests() {
+    Assumptions.assumeFalse(isWindows(), Message.NO_WINDOWS_SUPPORT);
+    super.runFederatedTests();
+  }
+}
